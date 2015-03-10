488117bc6962972c1925ed994a595f43369b968f
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-03-10 23:42:16 +0000

    Travis deployed 'master' - Tue Mar 10 23:42:16 UTC 2015

--- katas/es5/mocha+assert/assert-api.js
+++ katas/es5/mocha+assert/assert-api.js
@@ -1,8 +1,33 @@
 describe('mocha + assert', function() {
-  it('assert.equal', function() {
-    assert.equal('equal', 'not equals');
+  it('simple values equal?', function() {
+    assert.equal('one', 'one');
   });
-  it('to compare complex types use deepEqual', function() {
+  it('simple values doesnt equal?', function() {
+    assert.notEqual('one', 'two');
+  });
+  
+  it('value is truthy', function() {
+    assert.ok(true);
+  });
+  
+  it('complex types equal?', function() {
     assert.deepEqual([1,2,3], [1,2,3]);
+  });
+  it('complex types dont equal?', function() {
+    assert.notDeepEqual([1,2,3], ['one', 'two']);
+  });
+  
+  it('value and type equal?', function() {
+    assert.strictEqual('1', '1');
+  });
+  it('complex types dont equal?', function() {
+    assert.notStrictEqual(1, '1');
+  });
+  
+  it('throws?', function() {
+    assert.throws(function(){ throw new Error(); });
+  });
+  it('does not throw?', function() {
+    assert.doesNotThrow(function() {});
   });
 });
