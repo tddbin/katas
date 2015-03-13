d49bfe5dbfff187cfcf0140f059346bb0037a37e
Wolfram Kriesing <wk@uxebu.com>
2015-03-13 09:58:21 +0100

    ES6 multiline string kata.

---
+++ katas/es6/language/template-strings/multiline.js
@@ -0,0 +1,20 @@
+// template strings - multiline
+// To do: make all tests pass
+
+describe('template string, can be multiline', function() {
+  
+  it('count the lines properly', function() {
+    var multiline = `line 1
+    line 2
+    line 3`;
+    assert.equal(multiline.split('\n').length, 4);
+  });
+  
+  it('with variable place holders and spaces matter', function() {
+    var x = 42;
+    var multiline = `line 1
+        ${x}`;
+    assert.equal(multiline, '???');
+  });
+  
+});
