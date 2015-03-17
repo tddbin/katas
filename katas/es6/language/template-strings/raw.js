f7fd0ddfac32bafddc9dd126bf7de4edeb43eb79
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-03-17 14:28:08 +0000

    Travis deployed 'master' - Tue Mar 17 14:28:08 UTC 2015

---
+++ katas/es6/language/template-strings/raw.js
@@ -0,0 +1,40 @@
+// template strings - String.raw
+// To do: make all tests pass, leave the asserts unchanged!
+
+describe('on tagged template strings you can use String.raw', function() {
+  
+  it('the `raw` property accesses the string as it was entered', function() {
+    function firstChar(strings) {
+      return strings;
+    }
+    assert.equal(firstChar`\n`, '\\n');
+  });
+
+  it('`raw` can access the backslash of a line-break', function() {
+    function firstCharEntered(strings) {
+      var lineBreak = strings.raw[0];
+      return lineBreak;
+    }
+    assert.equal(firstCharEntered`\n`, '\\');
+  });
+
+  describe('`String.raw` as a static function', function(){
+    
+    it('concats the raw strings', function() {
+      var expected = '\n';
+      assert.equal(String.raw`\n`, expected);
+    });
+    
+    it('two raw-templates-string-backslashes equal two escaped backslashes', function() {
+      const A_BACKSLASH = '\\';
+      assert.equal(String.raw`\\`, A_BACKSLASH);
+    });
+    
+    it('works on unicodes too', function() {
+      var smilie = '\u{1F600}';
+      var actual = String.raw`\u{1F600}`;
+      assert.equal(actual, smilie);
+    });
+    
+  });
+});
