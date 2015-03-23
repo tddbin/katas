c4f7588f665d3bb369ea5ebd065c334105d2bf49
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-03-23 21:37:02 +0000

    Travis deployed 'master' - Mon Mar 23 21:37:02 UTC 2015

---
+++ katas/es6/language/block-scoping/const.js
@@ -0,0 +1,44 @@
+// block scope - const
+// To do: make all tests pass, leave the asserts unchanged!
+
+describe('`const` is like `let` plus read-only', () => {
+
+  describe('scalar values are immutable', () => {
+
+    it('number', () => {
+      const constNum = 0;
+      constNum = 1;
+      assert.equal(constNum, 0);
+    });
+
+    it('string', () => {
+      const constString = 'I am a const';
+      constString = 'Cant change you?';
+      assert.equal(constString, 'I am a const');
+    });
+
+  });
+  
+  const notChangeable = 23;
+
+  it('consts scope', () => {
+    assert.equal(notChangeable, 23);
+  });
+  
+  describe('complex types are NOT immutable', () => {
+
+    it('array', () => {
+      const arr = [42, 23];
+      arr[0] = 0;
+      assert.equal(arr[0], 42);
+    });
+    
+    it('object', () => {
+      const obj = {x: 1};
+      obj.x = 2;
+      assert.equal(obj.x, 3);
+    });
+    
+  });
+
+});
