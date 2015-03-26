8e8bb246455d2ffaf4f0b3bbedb14828d7b2b512
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-03-26 08:50:16 +0000

    Travis deployed 'master' - Thu Mar 26 08:50:16 UTC 2015

--- katas/es6/language/destructuring/array.js
+++ katas/es6/language/destructuring/array.js
@@ -3,7 +3,7 @@
 
 describe('destructuring arrays makes shorter code', () => {
 
-  it('to an array', () => {
+  it('extract value from array', () => {
     let firstValue = [1];
     assert.strictEqual(firstValue, 1);
   });
@@ -17,10 +17,10 @@ describe('destructuring arrays makes shorter code', () => {
   it('leading commas', () => {
     const all = ['ax', 'why', 'zet'];
     const [,z] = all;
-    assert.deepEqual(z, 'zet');
+    assert.equal(z, 'zet');
   });
   
-  it('use nested arrays', () => {
+  it('extract from nested arrays', () => {
     const user = [['Some', 'One'], 23];
     const [firstName, surname, age] = user;
     
