d0c08ec3d1bd509086c322d0b53847e637074fdc
Wolfram Kriesing <wk@uxebu.com>
2015-03-29 18:48:18 +0200

    Make them fail, so it is a real kata now :).

--- katas/es6/language/destructuring/object.js
+++ katas/es6/language/destructuring/object.js
@@ -4,34 +4,36 @@
 describe('destructuring objects', () => {
 
   it('is simple', () => {
-    const {x} = {x: 1};
+    const x = {x: 1};
     assert.equal(x, 1);
   });
 
   describe('nested', () => {
     it('multiple objects', () => {
       const magic = {first: 23, second: 42};
-      const {magic: {second}} = {magic};
+      const {magic: [second]} = {magic};
       assert.equal(second, 42);
     });
     it('object and array', () => {
-      const {z:[,x]} = {z: [23, 42]};
+      const {z:[x]} = {z: [23, 42]};
       assert.equal(x, 42);
     });
     it('array and object', () => {
-      const [,[{lang}]] = [null, [{env: 'browser', lang: 'ES6'}]];
+      const [,{lang}] = [null, [{env: 'browser', lang: 'ES6'}]];
       assert.equal(lang, 'ES6');
     });
   });
   
-  it('missing => undefined', () => {
-    const {z} = {x: 1};
-    assert.equal(z, void 0);
-  });
-
-  it('destructure from builtins (string)', () => {
-    const {substr} = '';
-    assert.equal(substr, String.prototype.substr);
+  describe('interesting', () => {
+    it('missing refs become undefined', () => {
+      const {z} = {x: 1, z: 2};
+      assert.equal(z, void 0);
+    });
+  
+    it('destructure from builtins (string)', () => {
+      const {substr} = 1;
+      assert.equal(substr, String.prototype.substr);
+    });
   });
 
 });
