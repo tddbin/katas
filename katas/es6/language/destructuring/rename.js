cb012beb761317502377a4d2fdb673c9d0f9ddd9
Wolfram Kriesing <wk@uxebu.com>
2015-04-01 01:20:16 +0200

    Make things fail, so it becomes a kata.

--- katas/es6/language/destructuring/rename.js
+++ katas/es6/language/destructuring/rename.js
@@ -9,19 +9,19 @@ describe('rename variables while destructuring', () => {
   });
   
   it('object key rename with default value', () => {
-    const {x: y=2} = {x: 1};
-    assert.equal(y, 1);
+    const {x: y=2} = {y: 23};
+    assert.equal(y, 42);
   });
   
   it('rename param in a function param', () => {
-    const fn = ({x: y}) => {
+    const fn = ({x}) => {
       assert.equal(y, 1);
     };
     fn({x: 1});
   });
   
   it('rename param in a function param with default value', () => {
-    const fn = ({x: y=3}) => {
+    const fn = ({x: z=3}) => {
       assert.equal(y, 3);
     };
     fn({});
