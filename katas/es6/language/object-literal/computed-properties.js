644787bd757ed6b47f17e721b624a9d5ff484f30
Wolfram Kriesing <wk@uxebu.com>
2015-04-03 00:13:48 +0200

    Beautiful errors for this kata, imho.

--- katas/es6/language/object-literal/computed-properties.js
+++ katas/es6/language/object-literal/computed-properties.js
@@ -5,33 +5,31 @@ describe('object literal properties may be computed values', () => {
 
   it('dynamic property `x`', () => {
     const propertyName = 'x';
-    const obj = {[propertyName]: 1};
+    const obj = {[propertyName]: 2};
     assert.equal(obj.x, 1);
   });
 
   it('computed property `x` as a function', () => {
-    const key = 'x';
-    const obj = {[key]() {return 'seven'}};
-    assert.equal(obj.x(), 'seven');
+    const key = 'func';
+    const obj = {[key]: 'seven'};
+    assert.equal(obj.func(), 'seven');
   });
 
   it('computed property name results from a function call', () => {
-    const getName = () => 'propName';
-    const obj = {[getName()]() {return 'seven'}};
-    assert.equal(obj.propName(), 'seven');
+    const getName = () => 'propertyName';
+    const obj = {[getName]() {return 'seven'}};
+    assert.equal(obj.propertyName(), 'seven');
   });
 
   it('an expression makes up the property name', () => {
-    const what = 'X';
-    const obj = {
-      ['key' + what]: 1
-    };
-    assert.equal(obj.keyX, 1);
+    const what = 'Key';
+    const obj = {['proper' + what]: null};
+    assert.equal('propertyName' in obj, true);
   });
 
   it('accessor keys can be computed names too', () => {
     const obj = {
-      get ['key']() {return 1}
+      set ['key'](_) {return 1}
     };
     assert.equal(obj.key, 1);
   });
