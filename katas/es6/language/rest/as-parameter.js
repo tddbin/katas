1c73efd09295fe325ca97ac44d0d19a6f0e0e8c0
Wolfram Kriesing <wk@uxebu.com>
2015-04-07 10:01:37 +0200

    Rest parameter kata.

---
+++ katas/es6/language/rest/as-parameter.js
@@ -0,0 +1,33 @@
+// 18: rest - as-parameter
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('rest in function params', () => {
+    
+  it('must be the last parameter', () => {
+    const fn = (...rest, veryLast) => {
+      assert.deepEqual([1, 2], rest);
+    };
+    fn(1, 2);
+  });
+  
+  it('can be used to get all other parameters', () => {
+    const fn = (firstParam, secondParam, rest) => {
+      assert.deepEqual([3,4], rest);
+    };
+    fn(null, 2, 3, 4);
+  });
+  
+  it('makes `arguments` obsolete', () => {
+    const fn = () => {
+      assert.deepEqual([42, 'twenty three', 'win'], args);
+    };
+    fn(42, 'twenty three', 'win');
+  });
+    
+  it('fun with function args and destructuring', () => {
+    const fn = (...args) => args.slice(1);
+    const [firstArg, ...rest] = fn(1, 2, 3);
+    assert.deepEqual([2, 3], rest);
+  });
+    
+});
