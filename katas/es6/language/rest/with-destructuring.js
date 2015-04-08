887073f8e29867680ca8db3f02885e81af853450
Wolfram Kriesing <wk@uxebu.com>
2015-04-08 09:46:37 +0200

    A kata, all fails :).

--- katas/es6/language/rest/with-destructuring.js
+++ katas/es6/language/rest/with-destructuring.js
@@ -4,23 +4,24 @@
 describe('rest with destrcturing', () => {
     
   it('rest parameter must be last', () => {
-    const [...all] = [1, 2, 3, 4];
+    const [all] = [1, 2, 3, 4];
     assert.deepEqual(all, [1, 2, 3, 4]);
   });
   
   it('assign rest of an array to a variable', () => {
-    const [, ...all] = [1, 2, 3, 4];
+    const [...all] = [1, 2, 3, 4];
     assert.deepEqual(all, [2, 3, 4]);
   });
   
   it('concat differently', () => {
     const theEnd = [3, 4];
-    const allInOne = [1, 2, ...theEnd];
+    const allInOne = [1, 2, ...[theEnd]];
     assert.deepEqual(allInOne, [1, 2, 3, 4]);
   });
   
   it('`apply` made simple, even for constructors', () => {
-    const date = new Date(...[2015, 1, 1]);
+    const theDate = [2015, 1, 1];
+    const date = new Date(theDate);
     assert.deepEqual(new Date(2015, 1, 1), date);
   });
   
