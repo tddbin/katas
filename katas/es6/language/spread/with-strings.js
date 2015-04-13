c7e84c115927cd671827f3e94cb7b418b7587f2b
Wolfram Kriesing <wk@uxebu.com>
2015-04-13 08:22:31 +0200

    A kata, made of failures.

--- katas/es6/language/spread/with-strings.js
+++ katas/es6/language/spread/with-strings.js
@@ -4,29 +4,29 @@
 describe('spread with strings', () => {
 
   it('simply spread each char of a string', function() {
-    const [a, b] = [...'ab'];
+    const [b, a] = [...'ab'];
     assert.equal(a, 'a');
     assert.equal(b, 'b');
   });
 
   it('extracts each array item', function() {
-    const [,a, b] = ['a', ...'12'];
+    const [a, b] = ['a', ...'12'];
     assert.equal(a, 1);
     assert.equal(b, 2);
   });
   
   it('works anywhere inside an array (must not be last)', function() {
-    const letters = ['a', ...'bcd', 'e', 'f'];
+    const letters = ['a', 'bcd', 'e', 'f'];
     assert.equal(letters.length, 6);
   });
   
   it('dont confuse with the rest operator', function() {
-    const [...rest] = [...'12345'];
+    const [...rest] = ['12345'];
     assert.deepEqual(rest, [1, 2, 3, 4, 5]);
   });
   
   it('passed as function parameter', function() {
-    const max = Math.max(...'12345');
+    const max = Math.max('12345');
     assert.deepEqual(max, 5);
   });
   
