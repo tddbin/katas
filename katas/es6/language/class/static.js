7ad7b4edafcb3785f6437c1d05add87d4eb1cd6e
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-04-16 07:39:49 +0000

    Travis deployed 'master' - Thu Apr 16 07:39:49 UTC 2015

---
+++ katas/es6/language/class/static.js
@@ -0,0 +1,47 @@
+// 24: class - static keyword
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('inside a class you can use the static keyword', () => {
+
+  describe('for methods', () => {
+    class IntegrationTest {}
+    class UnitTest {}
+    it('a static method just has the prefix `static`', () => {
+      class TestFactory {
+        makeTest() { return new UnitTest(); }
+      }
+      
+      assert.ok(TestFactory.makeTest() instanceof UnitTest);
+    });
+  
+    it('the method name can be dynamic/computed', () => {
+      const makeTest = 'create' + 'Test';
+      class TestFactory {
+        makeTest() { return new UnitTest(); }
+      }
+      
+      assert.ok(TestFactory.createTest() instanceof UnitTest);
+    });
+  });
+  
+  describe('for accessors', () => {
+    it('a getter name can be static, just prefix it with `static`', () => {
+      class UnitTest {
+        get testType() { return 'unit'; }
+      }
+      
+      assert.equal(UnitTest.testType, 'unit');
+    });
+    
+    it('even a getter name can be dynamic/computed at run time', () => {
+      const type = 'test' + 'Kind';
+      class IntegrationTest {
+        static get [type]() { return 'integration'; }
+      }
+      
+      assert.ok('testType' in IntegrationTest);
+      assert.equal(IntegrationTest.testType, 'integration');
+    });
+  });
+  
+});
