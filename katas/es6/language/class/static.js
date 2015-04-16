56700cb59b8adbfacafc13201ab8754086fec357
Wolfram Kriesing <wk@uxebu.com>
2015-04-16 09:38:41 +0200

    Trying to make things learnable by going from top to bottom.

--- katas/es6/language/class/static.js
+++ katas/es6/language/class/static.js
@@ -8,7 +8,7 @@ describe('inside a class you can use the static keyword', () => {
     class UnitTest {}
     it('a static method just has the prefix `static`', () => {
       class TestFactory {
-        static makeTest() { return new UnitTest(); }
+        makeTest() { return new UnitTest(); }
       }
       
       assert.ok(TestFactory.makeTest() instanceof UnitTest);
@@ -17,7 +17,7 @@ describe('inside a class you can use the static keyword', () => {
     it('the method name can be dynamic/computed', () => {
       const makeTest = 'create' + 'Test';
       class TestFactory {
-        static [makeTest]() { return new UnitTest; }
+        makeTest() { return new UnitTest(); }
       }
       
       assert.ok(TestFactory.createTest() instanceof UnitTest);
@@ -27,18 +27,19 @@ describe('inside a class you can use the static keyword', () => {
   describe('for accessors', () => {
     it('a getter name can be static, just prefix it with `static`', () => {
       class UnitTest {
-        static get testType() { return 'unit'; }
+        get testType() { return 'unit'; }
       }
       
       assert.equal(UnitTest.testType, 'unit');
     });
     
     it('even a getter name can be dynamic/computed at run time', () => {
-      const type = 'testType';
+      const type = 'test' + 'Kind';
       class IntegrationTest {
         static get [type]() { return 'integration'; }
       }
       
+      assert.ok('testType' in IntegrationTest);
       assert.equal(IntegrationTest.testType, 'integration');
     });
   });
