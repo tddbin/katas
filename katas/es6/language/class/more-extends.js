9b944212a19c25ef03268e6a143594cbdd67c562
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-04-17 08:56:08 +0000

    Travis deployed 'master' - Fri Apr 17 08:56:08 UTC 2015

---
+++ katas/es6/language/class/more-extends.js
@@ -0,0 +1,43 @@
+// 26: class - more-extends
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('class can inherit from another', () => {
+
+  it('extend an `old style` "class", a function, still works', () => {
+    let A;
+    class B extends A {}
+    
+    assert.equal(new B() instanceof A, true);
+  });
+  
+  describe('prototypes are as you know them', () => {
+    class A {}
+    class B extends A {}
+    it('A is the prototype of B', () => {
+      const isIt = A.isPrototypeOf(null);
+      assert.equal(isIt, true);
+    });
+    it('A`s prototype is also B`s prototype', () => {
+      const proto = B;
+      // Remember: don't touch the assert!!! :)
+      assert.equal(A.prototype.isPrototypeOf(proto), true);
+    });
+  });
+
+  describe('`extends` using an expression', () => {
+    it('like the inline assignment of the parent class', () => {
+      let A;
+      class B extends (A = {}) {}
+      
+      assert.equal(new B() instanceof A, true);
+    });
+    
+    it('or calling a function that returns the parent class', () => {
+      const returnParent = (classOrNot) => classOrNot ? class {} : null;
+      class B extends (returnParent) {}
+      
+      assert.equal(Object.getPrototypeOf(B.prototype), null);
+    });
+  });
+  
+});
