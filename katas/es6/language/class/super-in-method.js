a2fc297952a312a697c2692efd106d7d975adfe6
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-04-21 07:17:09 +0000

    Travis deployed 'master' - Tue Apr 21 07:17:09 UTC 2015

---
+++ katas/es6/language/class/super-in-method.js
@@ -0,0 +1,45 @@
+// 27: class - super
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('inside a class use `super` to access parent methods', () => {
+
+  it('use of `super` without `extends` fails (already when transpiling)', () => {
+    class A {hasSuper() { return super; }}
+    
+    assert.equal(new A().hasSuper(), false);
+  });
+  
+  it('`super` with `extends` calls the method of the given name of the parent class', () => {
+    class A {hasSuper() { return true; }}
+    class B extends A {hasSuper() { return super.hasSuper; }}
+    
+    assert.equal(new B().hasSuper(), true);
+  });
+  
+  it('when overridden a method does NOT automatically call its super method', () => {
+    class A {hasSuper() { return true; }}
+    class B extends A {hasSuper() { return 'nothing'; }}
+    
+    assert.equal(new B().hasSuper(), void 0);
+  });
+  
+  it('`super` works across any number of levels of inheritance', () => {
+    class A {iAmSuper() { return this.youAreSuper; }}
+    class B extends A {constructor() { this.youAreSuper = true; } }
+    class C extends B {
+      iAmSuper() { 
+        return this.iAmSuper(); 
+      }
+    }
+    
+    assert.equal(new C().iAmSuper(), true);
+  });
+  
+  it('accessing an undefined member of the parent class returns `undefined`', () => {
+    class A {}
+    class B extends A {getMethod() { return super.constructor; }}
+    
+    assert.equal(new B().getMethod(), void 0);
+  });
+  
+});
