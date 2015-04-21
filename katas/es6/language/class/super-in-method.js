abb6b45554b511affff394d54f36354c548fc6b7
Wolfram Kriesing <wk@uxebu.com>
2015-04-21 08:40:58 +0200

    All tests passing for kata 27.

---
+++ katas/es6/language/class/super-in-method.js
@@ -0,0 +1,42 @@
+// 27: class - super
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('inside a class use `super` to access parent methods', () => {
+
+  it('use of `super` without `extends` fails (already when transpiling)', () => {
+    class A {hasSuper() { return false; }}
+    
+    assert.equal(new A().hasSuper(), false);
+  });
+  
+  it('`super` with `extends` calls the method of the given name of the parent class', () => {
+    class A {hasSuper() { return true; }}
+    class B extends A {hasSuper() { return super.hasSuper(); }}
+    
+    assert.equal(new B().hasSuper(), true);
+  });
+  
+  it('when overridden a method does NOT automatically call its super method', () => {
+    class A {hasSuper() { return true; }}
+    class B extends A {hasSuper() {}}
+    
+    assert.equal(new B().hasSuper(), void 0);
+  });
+  
+  it('`super` works across any number of levels of inheritance', () => {
+    class A {iAmSuper() { return true; }}
+    class B extends A {}
+    class C extends B {iAmSuper() { return super.iAmSuper(); }}
+    
+    assert.equal(new C().iAmSuper(), true);
+  });
+  
+  it('accessing an undefined member of the parent class returns `undefined`', () => {
+    class A {}
+    class B extends A {getMethod() { return super.iAmSuper; }}
+    
+    assert.equal(new B().getMethod(), void 0);
+  });
+  
+});
+
