bfe9fcf0f3318be05a1916337a4e27659f202267
Wolfram Kriesing <wk@uxebu.com>
2015-04-22 09:37:12 +0200

    Some super in constructor challenges.

---
+++ katas/es6/language/class/super-in-constructor.js
@@ -0,0 +1,55 @@
+// 28: class - super in constructor
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('class', () => {
+
+  it('use `super()` to call the parent constructor', () => {
+    class A {constructor() { this.levels = 1; }}
+    class B extends A {
+      constructor() { 
+        super();
+        this.levels++; 
+      }
+    }
+    
+    assert.equal(new B().levels, 2);
+  });
+
+  it('`super()` may also take params', () => {
+    class A {constructor(startValue=1, addTo=1) { this.counter = startValue + addTo; }}
+    class B extends A {
+      constructor(...args) { 
+        super(...args);
+        this.counter++; 
+      }
+    }
+    
+    assert.equal(new B(42, 2).counter, 45);
+  });
+  
+  it('it is important where you place your `super()` call!', () => {
+    class A {constructor() { this.countUp = 1; }}
+    class B extends A {
+      constructor() { 
+        this.countUp = 2; 
+        super();
+      }
+    }
+    
+    assert.equal(new B().countUp, 1);
+  });
+
+  it('you can dynamically check for a parent constructor', () => {
+    class A extends null {
+      constructor() {
+        this.isTop = false;
+        try {
+          this.isTop = !!super.constructor;
+        } catch (e) {}
+      }
+    }
+
+    assert.equal(new A().isTop, false);
+  });
+  
+});
