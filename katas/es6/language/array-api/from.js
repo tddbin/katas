e46bc3d8e93d4abc4ac60e331b435ab03e86c0c7
Wolfram Kriesing <wk@uxebu.com>
2015-04-23 10:28:48 +0200

    Now it's a real kata.

--- katas/es6/language/array-api/from.js
+++ katas/es6/language/array-api/from.js
@@ -1,24 +1,26 @@
-// 29: array - 
+// 29: array - `Array.from` static method
 // To do: make all tests pass, leave the assert lines unchanged!
 
-describe('Array.from converts an array-like object or list into an Array', () => {
+describe('`Array.from` converts an array-like object or list into an Array', () => {
 
-  it('from an object, requires the length property', function() {
-    const arr = Array.from({0: 1, 1: 2, length: 2});
+  const arrayLike = {0: 'one', 1: 'two', length: 2};
+  
+  it('call `Array.from` with an array-like object', function() {
+    const arr = arrayLike;
     
-    assert.deepEqual(arr, [1, 2]);
+    assert.deepEqual(arr, ['one', 'two']);
   });
   
   it('a DOM node`s classList object can be converted', function() {
     document.body.classList.add('some');
     document.body.classList.add('other');
-    const classList = Array.from(document.body.classList);
+    const classList = document.body.classList;
     
     assert.deepEqual(classList, ['some', 'other']);
   });
   
-  it('convert a NodeList to an Array and filter works on it', function() {
-    const nodeList = Array.from(document.querySelectorAll('body'));
+  it('convert a NodeList to an Array and `filter()` works on it', function() {
+    const nodeList = document.querySelectorAll('body');
     const bodies = nodeList.filter((node) => node === document.body);
     
     assert.deepEqual(bodies, [document.body]);
@@ -26,16 +28,14 @@ describe('Array.from converts an array-like object or list into an Array', () =>
   
   describe('custom conversion using a map function as second param', () => {
     it('we can modify the value before putting it in the array', function() {
-      const arr = Array.from({0: 1, 1: 2, length: 2}, (value) => value - 1);
-      assert.deepEqual(arr, [0, 1]);
+      const arr = Array.from(arrayLike, (value) => value);
+      assert.deepEqual(arr, ['ONE', 'TWO']);
     });
     
-    it('and we also get the object`s key', function() {
-      const arrLike = {0: 'one', 1: 'two', length: 2};
-      const arr = Array.from(arrLike, (value, key) => `${key}=${value}`);
+    it('and we also get the object`s key as second parameter', function() {
+      const arr = Array.from(arrayLike, (value) => `${key}=${value}`);
       assert.deepEqual(arr, ['0=one', '1=two']);
     });
   });
-  
   
 });
