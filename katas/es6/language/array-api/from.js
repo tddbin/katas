ac285a4a72a4bf69bc4e74006e8a650baf625138
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-04-23 08:30:26 +0000

    Travis deployed 'master' - Thu Apr 23 08:30:26 UTC 2015

---
+++ katas/es6/language/array-api/from.js
@@ -0,0 +1,41 @@
+// 29: array - `Array.from` static method
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('`Array.from` converts an array-like object or list into an Array', () => {
+
+  const arrayLike = {0: 'one', 1: 'two', length: 2};
+  
+  it('call `Array.from` with an array-like object', function() {
+    const arr = arrayLike;
+    
+    assert.deepEqual(arr, ['one', 'two']);
+  });
+  
+  it('a DOM node`s classList object can be converted', function() {
+    document.body.classList.add('some');
+    document.body.classList.add('other');
+    const classList = document.body.classList;
+    
+    assert.deepEqual(classList, ['some', 'other']);
+  });
+  
+  it('convert a NodeList to an Array and `filter()` works on it', function() {
+    const nodeList = document.querySelectorAll('body');
+    const bodies = nodeList.filter((node) => node === document.body);
+    
+    assert.deepEqual(bodies, [document.body]);
+  });
+  
+  describe('custom conversion using a map function as second param', () => {
+    it('we can modify the value before putting it in the array', function() {
+      const arr = Array.from(arrayLike, (value) => value);
+      assert.deepEqual(arr, ['ONE', 'TWO']);
+    });
+    
+    it('and we also get the object`s key as second parameter', function() {
+      const arr = Array.from(arrayLike, (value) => `${key}=${value}`);
+      assert.deepEqual(arr, ['0=one', '1=two']);
+    });
+  });
+  
+});
