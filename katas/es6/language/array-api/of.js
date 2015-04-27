565f92b0d46f88e1049b07af3533d9999937e0fe
Wolfram Kriesing <wk@uxebu.com>
2015-04-27 10:07:20 +0200

    A kata, fix it!

--- katas/es6/language/array-api/of.js
+++ katas/es6/language/array-api/of.js
@@ -4,19 +4,21 @@
 describe('`Array.of` creates an array with the given arguments as elements', () => {
   
   it('dont mix it up with `Array(10)`, where the argument is the array length', () => {
-    const arr = Array.of(10);
+    const arr = Array(10);
     
     assert.deepEqual(arr, [10]);
   });
   
   it('puts all arguments into array elements', () => {
-    const arr = Array.of(1, 2);
+    const arr = Array.of();
     
     assert.deepEqual(arr, [1, 2]);
   });
   
   it('takes any kind and number of arguments', () => {
-    const arr = Array.of([1, 2], 3, '4');
+    const starter = [1, 2];
+    const end = [3, '4'];
+    const arr = Array.of(...starter, ...end);
     
     assert.deepEqual(arr, [[1, 2], 3, '4']);
   });
