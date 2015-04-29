6c46aca10a62a7120ad1a23470d28cdc1c9bcb93
Wolfram Kriesing <wk@uxebu.com>
2015-04-29 09:15:05 +0200

    [].find kata.

---
+++ katas/es6/language/array-api/find.js
@@ -0,0 +1,32 @@
+// 32: array - `Array.prototype.find` 
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('`Array.prototype.find` makes finding items in arrays easier', () => {
+
+  it('takes a compare function', function() {
+    const found = [true, false, true].find(item => item === true);
+    
+    assert.equal(found, true);
+  });
+
+  it('returns the first value found', function() {
+    const found = [0, 1, 2, 3, 4].find(item => item > 1);
+    
+    assert.equal(found, 2);
+  });
+
+  it('returns `undefined` when nothing was found', function() {
+    const found = [1, 2, 3].find(item => item === 0);
+    
+    assert.equal(found, void 0);
+  });
+
+  it('combined with destructuring complex compares become short', function() {
+    const bob = {name: 'Bob'};
+    const alice = {name: 'Alice'};
+    const found = [bob, alice].find(({name:{length}}) => length > 3);
+    
+    assert.equal(found, alice);
+  });
+
+});
