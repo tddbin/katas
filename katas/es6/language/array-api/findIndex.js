0025b736376743adcdea936cffb462afdae947f2
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-05-04 07:49:58 +0000

    Travis deployed 'master' - Mon May  4 07:49:58 UTC 2015

---
+++ katas/es6/language/array-api/findIndex.js
@@ -0,0 +1,44 @@
+// 33: array - `Array.prototype.findIndex` 
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('`Array.prototype.findIndex` makes finding items in arrays easier', () => {
+
+  it('takes a compare function', function() {
+    const foundAt = [false, true].findIndex(item);
+    
+    assert.equal(foundAt, 1);
+  });
+
+  it('returns the first position it was found at', function() {
+    const foundAt = [0, 1, 1, 1].findIndex(item => item = 1);
+    
+    assert.equal(foundAt, 1);
+  });
+
+  it('returns `-1` when nothing was found', function() {
+    const foundAt = [1, 2, 3].findIndex(item => item > 3);
+    
+    assert.equal(foundAt, -1);
+  });
+
+  it('the findIndex callback gets the item, index and array arguments', function() {
+    const three = 3;
+    const containsThree = arr => arr.indexOf(three) > -1;
+    function theSecondThree(index, arr) {
+      const preceedingItems = arr.slice(0, index);
+      return containsThree(preceedingItems);
+    }
+    const foundAt = [1, 1, 2, 3, 3, 3].findIndex(theSecondThree);
+    
+    assert.equal(foundAt, 4);
+  });
+
+  it('combined with destructuring complex compares become short', function() {
+    const bob = {name: 'Bob'};
+    const alice = {name: 'Alice'};
+    const foundAt = [bob, alice].findIndex(({name:{length:l}}) => length > 3);
+    
+    assert.equal(foundAt, 1);
+  });
+
+});
