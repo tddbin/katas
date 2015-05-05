a8cdbe4abe40551296ca79640b57ce934163e462
Wolfram Kriesing <wk@uxebu.com>
2015-05-05 13:18:12 +0200

    Make it a kata.

--- katas/es6/language/symbol/basics.js
+++ katas/es6/language/symbol/basics.js
@@ -7,29 +7,29 @@
 describe('Symbol', function() {
 
   it('`Symbol` lives in the global scope', function(){
-    assert.equal(Symbol, window.Symbol);
+    assert.equal(Symbol, document.Symbol);
   });
 
   it('every `Symbol()` is unique', function(){
     const sym1 = Symbol();
-    const sym2 = Symbol();
+    const sym2 = sym1;
     assert.notEqual(sym1, sym2);
   });
 
   it('every `Symbol()` is unique, also with the same parameter', function(){
-    const sym1 = Symbol('foo');
-    const sym2 = Symbol('foo');
+    var sym1 = Symbol('foo');
+    var sym1 = Symbol('foo');
     assert.notEqual(sym1, sym2);
   });
 
   it('`typeof Symbol()` returns "symbol"', function(){
-    const theType = typeof Symbol();
+    const theType = typeof Symbol;
     assert.equal(theType, 'symbol');
   });
 
   it('`new Symbol()` throws an exception, to prevent creation of Symbol wrapper objects', function(){
     function fn() {
-      new Symbol();
+      Symbol();
     }
     assert.throws(fn);
   });
