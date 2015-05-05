d086e733948ead1b23c2e8630f77206b3e0e9559
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-05-05 11:19:06 +0000

    Travis deployed 'master' - Tue May  5 11:19:06 UTC 2015

---
+++ katas/es6/language/symbol/basics.js
@@ -0,0 +1,37 @@
+// 34: symbol
+// A symbol is a unique and immutable data type and may be used as an identifier for object properties
+// read more at https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol
+
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('Symbol', function() {
+
+  it('`Symbol` lives in the global scope', function(){
+    assert.equal(Symbol, document.Symbol);
+  });
+
+  it('every `Symbol()` is unique', function(){
+    const sym1 = Symbol();
+    const sym2 = sym1;
+    assert.notEqual(sym1, sym2);
+  });
+
+  it('every `Symbol()` is unique, also with the same parameter', function(){
+    var sym1 = Symbol('foo');
+    var sym1 = Symbol('foo');
+    assert.notEqual(sym1, sym2);
+  });
+
+  it('`typeof Symbol()` returns "symbol"', function(){
+    const theType = typeof Symbol;
+    assert.equal(theType, 'symbol');
+  });
+
+  it('`new Symbol()` throws an exception, to prevent creation of Symbol wrapper objects', function(){
+    function fn() {
+      Symbol();
+    }
+    assert.throws(fn);
+  });
+
+});
