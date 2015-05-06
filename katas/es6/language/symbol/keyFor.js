cfd4ae8c4cb66b66070f05880e5fafbdcf521f60
Wolfram Kriesing <wk@uxebu.com>
2015-05-06 10:13:37 +0200

    Make it a kata, all tests fail.

--- katas/es6/language/symbol/keyFor.js
+++ katas/es6/language/symbol/keyFor.js
@@ -3,29 +3,31 @@
 
 describe('`Symbol.keyFor` gets the symbol key for a given symbol', function() {
   
+  const sym = Symbol.for('foo');
+  
   it('pass the symbol to `keyFor()` and you get it`s key', function() {
-    const sym = Symbol.for('foo');
-    const key = Symbol.keyFor(sym);
+    const key = Symbol.____(sym);
     
     assert.equal(key, 'foo');
   });
   
   it('local symbols are not in the runtime-wide registry', function() {
-    const localSymbol = Symbol('foo');
+    // hint: `Symbol()` creates a local symbol!
+    const localSymbol = Symbol.for('foo');
     const key = Symbol.keyFor(localSymbol);
     
     assert.equal(key, void 0);
   });
   
   it('well-known symbols are not in the runtime-wide registry either', function() {
-    const key = Symbol.keyFor(Symbol.iterator);
+    const key = Symbol.keyFor(Symbol.iteraTor);
     
     assert.equal(key, void 0);
   });
   
   it('for non-Symbols throws an error', function() {
     function fn() {
-      Symbol.keyFor(null);
+      Symbol.keyFor(sym);
     }
     
     assert.throws(fn);
