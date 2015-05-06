991b1e41b22bd9a36edb900c58561183486ed6ea
Wolfram Kriesing <wk@uxebu.com>
2015-05-06 08:52:17 +0200

    Made it a kata.

--- katas/es6/language/symbol/for.js
+++ katas/es6/language/symbol/for.js
@@ -3,21 +3,23 @@
 
 describe('`Symbol.for` for registering Symbols globally', function() {
   
-  it('creates a new symbol', function() {
-    const sym = Symbol.for('symbol name');
-    assert.equal(typeof sym, 'symbol');
+  it('creates a new symbol (check via `typeof`)', function() {
+    const symbolType = Symbol.for('symbol name');
+    assert.equal(symbolType, 'symbol');
   });
   
   it('stores the symbol in a runtime-wide registry and retreives it from it', function() {
     const sym = Symbol.for('new symbol');
-    const sym1 = Symbol.for('new symbol');
+    const sym1 = Symbol.for('new symbol1');
+    
     assert.equal(sym, sym1);
   });
   
   it('is different to `Symbol()` which creates a symbol every time and does not store it', function() {
-    const symFor = Symbol.for('new symbol');
-    const sym = Symbol('new symbol');
-    assert.notEqual(symFor, sym);
+    var globalSymbol = Symbol.for('new symbol');
+    var globalSymbol = Symbol('new symbol');
+    
+    assert.notEqual(globalSymbol, dynamicSymbol);
   });
   
   describe('`.toString()` on a Symbol', function() {
@@ -26,14 +28,14 @@ describe('`Symbol.for` for registering Symbols globally', function() {
     const symbolFromRegistry = Symbol.for('new symbol');
     
     it('also contains the key given to `Symbol.for()`', function() {
-      const description = newSymbol.toString();
+      const description = newSymbol.toString;
       assert.equal(description, 'Symbol(new symbol)');
     });
   
     describe('NOTE: the description of two different symbols', function() {
       it('might be the same', function() {
         const newDescription = newSymbol.toString();
-        const fromRegistryDescription = symbolFromRegistry.toString();
+        const fromRegistryDescription = ''+symbolFromRegistry;
         
         assert.equal(newDescription, fromRegistryDescription);
       });
