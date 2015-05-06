d07271a441296d487194ad66b4e9bab278c80618
Wolfram Kriesing <wk@uxebu.com>
2015-05-06 09:20:34 +0200

    Symbol.for() is done, now keyFor is next.

--- katas/es6/language/symbol/for-and-keyFor.js
+++
@@ -1,5 +0,0 @@
-// 35: Symbol.for/keyFor
-// To do: make all tests pass, leave the assert lines unchanged!
-
-describe('`Symbol.for/keyFor` for registering Symbols globally', function() {
-});
---
+++ katas/es6/language/symbol/keyFor.js
@@ -0,0 +1,5 @@
+// 36: Symbol.keyFor
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('`Symbol.keyFor` for registering Symbols globally', function() {
+});
