4fdd0754f150766185bb19ef3a43025294f80d00
Wolfram Kriesing <wk@uxebu.com>
2015-05-08 00:30:30 +0200

    Prevent deploying pull requests, which travis would.

--- scripts/for-travis/build-and-deploy.sh
+++ scripts/for-travis/build-and-deploy.sh
@@ -3,5 +3,9 @@
 # connect with `&&` so we only run the next on success
 # otherwise we `echo` and `exit 1`
 
-npm run build && ./scripts/for-travis/deploy-master-branch-only.sh ||
-{ echo "Ooops, ERROR cant deploy, see error(s) above"; exit 1; }
+if [ -z $TRAVIS_PULL_REQUEST ]; then
+  npm run build && ./scripts/for-travis/deploy-master-branch-only.sh ||
+  { echo "Ooops, ERROR cant deploy, see error(s) above"; exit 1; }
+else
+  echo "deploy: pull requests don't get deployed"
+fi
