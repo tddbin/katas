e2ba61dea1e2e1e496cd20595f28f612412376fa
Wolfram Kriesing <wk@uxebu.com>
2015-05-13 22:30:32 +0200

    All katas in one structure.

---
+++ katas/es6/language/katasInOrder.js
@@ -0,0 +1,209 @@
+const ES6_LEVEL = {TBD, BEGINNER, INTERMEDIATE, ADVANCED, EXPERT};
+
+const obj = {
+  name: 'ES6 Katas',
+  items: {
+    1: {
+      path: 'template-strings/basics',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    2: {
+      path: 'template-strings/multiline',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    3: {
+      path: 'template-strings/tagged',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    4: {
+      path: 'template-strings/raw',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    5: {
+      path: 'arrow-functions/basics',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    6: {
+      path: 'arrow-functions/binding',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    7: {
+      path: 'block-scope/let',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    8: {
+      path: 'block-scope/const',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    9: {
+      path: 'object-literal/basics',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    10: {
+      path: 'destructuring/array',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    11: {
+      path: 'destructuring/string',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    12: {
+      path: 'destructuring/object',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    13: {
+      path: 'destructuring/defaults',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    14: {
+      path: 'destructuring/parameters',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    15: {
+      path: 'destructuring/rename',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    16: {
+      path: 'object-literal/computed-properties',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    17: {
+      path: 'unicode/in-strings',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    18: {
+      path: 'rest/as-parameter',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    19: {
+      path: 'rest/with-destructuring',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    20: {
+      path: 'spread/with-arrays',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    21: {
+      path: 'spread/with-strings',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    22: {
+      path: 'class/creation',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    23: {
+      path: 'class/accessors',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    24: {
+      path: 'class/static-keyword',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    25: {
+      path: 'class/extends',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    26: {
+      path: 'class/more-extends',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    27: {
+      path: 'class/super-in-method',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    28: {
+      path: 'class/super-in-constructor',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    29: {
+      path: 'array-api/from',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    30: {
+      path: 'array-api/of',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    31: {
+      path: 'array-api/fill',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    32: {
+      path: 'array-api/find',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    33: {
+      path: 'array-api/findIndex',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    34: {
+      path: 'symbol/basics',
+      level: ES6_LEVEL.INTERMEDIATE,
+      requiresKnowledgeFrom: []
+    },
+    35: {
+      path: 'symbol/for',
+      level: ES6_LEVEL.INTERMEDIATE,
+      requiresKnowledgeFrom: []
+    },
+    36: {
+      path: 'symbol/keyFor',
+      level: ES6_LEVEL.INTERMEDIATE,
+      requiresKnowledgeFrom: [35]
+    },
+    37: {
+      path: 'iterator/array',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: []
+    },
+    38: {
+      path: 'iterator/string',
+      level: ES6_LEVEL.TBD,
+      requiresKnowledgeFrom: [37]
+    },
+    39: {
+      path: 'iterator/protocol',
+      level: ES6_LEVEL.EXPERT,
+      requiresKnowledgeFrom: []
+    },
+    40: {
+      path: 'iterator/usages',
+      level: ES6_LEVEL.EXPERT,
+      requiresKnowledgeFrom: [34, 39]
+    }
+  }
+};
+
+export obj;
