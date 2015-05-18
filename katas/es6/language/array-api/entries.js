61367d47fbecb1037fc1a877c893c2d3f90f3f82
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-05-18 08:01:02 +0000

    Travis deployed 'master' - Mon May 18 08:01:02 UTC 2015

---
+++ katas/es6/language/array-api/entries.js
@@ -0,0 +1,32 @@
+// 41: array - entries
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('`Array.prototype.entries` returns an iterator object with all entries', function() {
+  
+  it('returns key+value for each element', function() {
+    const arr = ['a', 'b', 'c'];
+    const entriesAsArray = Array.from(arr.___());
+    
+    assert.deepEqual(entriesAsArray, [[0,"a"], [1,"b"], [2,"c"]]);
+  });
+  
+  it('empty elements contain the value `undefined`', function() {
+    const arr = ['one'];
+    arr[2] = 'three';
+    const secondValue = arr.entries();
+    
+    assert.deepEqual(secondValue, [1, void 0]);
+  });
+
+  describe('returns an iterable', function() {
+    
+    it('has `next()` to iterate', function() {
+      const arr = ['one'];
+      const value = arr;
+      
+      assert.deepEqual(value, [0, 'one']);
+    });
+    
+  });
+  
+});
