4746b20eb053e3e5da605c079e86aae4c81ebe50
Wolfram Kriesing <wk@uxebu.com>
2015-05-19 09:24:21 +0200

    Simple but effective kata, I think.

--- katas/es6/language/array-api/keys.js
+++ katas/es6/language/array-api/keys.js
@@ -8,19 +8,18 @@ describe('`Array.prototype.keys` returns an iterator for all keys in the array',
     const iterator = arr.keys();
     
     assert.deepEqual(iterator.next(), {value: 0, done: false});
-    assert.deepEqual(iterator.next(), {value: 1, done: false});
     assert.deepEqual(iterator.next(), {value: void 0, done: true});
   });
   
   it('gets all keys', function() {
-    const arr = [1, 2, 3];
+    const arr = [1, 2];
     const keys = Array.from(arr.keys());
     
     assert.deepEqual(keys, [0, 1, 2]);
   });
   
   it('empty array contains no keys', function() {
-    const arr = [];
+    const arr = ['empty me'];
     const keys = [...arr.keys()];
     
     assert.equal(keys.length, 0);
@@ -28,14 +27,14 @@ describe('`Array.prototype.keys` returns an iterator for all keys in the array',
   
   it('a sparse array without real values has keys though', function() {
     const arr = [,,];
-    const keys = [...arr.keys()];
+    const keys = [...arr.___()];
     
     assert.deepEqual(keys, [0, 1]);
   });
 
   it('also includes holes in sparse arrays', function() {
     const arr = ['a', , 'c'];
-    const keys = Array.from(arr.keys());
+    const keys = arr.keys;
     
     assert.deepEqual(keys, [0, 1, 2]);
   });
