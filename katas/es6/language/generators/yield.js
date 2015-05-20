774e423dbec137489f57a92662ef0b5464869dd0
Jonathan Prince <jonathan.prince@gmail.com>
2015-05-20 09:52:09 +0200

    add kata for generator yield statements

---
+++ katas/es6/language/generators/yield.js
@@ -0,0 +1,38 @@
+// Generators - Yield Statements
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('generator instance', () => {
+  
+  function* generator() {
+    yield 'hello';
+    yield 'world';
+  }
+  
+  // create instance
+  let genInstance = generator();
+  
+  it('should have an undefined value property after initialisation', () => {
+    let value = genInstance.value;
+    assert.equal(value, undefined);
+  });
+  
+  it('should have value of "hello" after first next() call', () => {
+    let thisStep = genInstance.next();
+    let [value, done] = [thisStep.value, thisStep.done];
+    assert.equal(value, 'hello');
+    assert.equal(done, false);
+  });
+  
+  it('should have value of "world" after second next() call', () => {
+    let thisStep = genInstance.next();
+    let [value, done] = [thisStep.value, thisStep.done];  
+    assert.equal(value, 'world');
+    assert.equal(done, false);
+  });
+  
+  it('done property = true after stepping past all yield statements', () => {
+    let done = genInstance.next().done;
+    assert.equal(done, true);
+  });
+
+});
