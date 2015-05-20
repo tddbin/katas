adbb3f4526d853a861d91da71e41b84e97851740
Wolfram Kriesing <wk@uxebu.com>
2015-05-20 09:06:40 +0200

    Trying to underline the speciality of values() returning an iterator.

--- katas/es6/language/array-api/values.js
+++ katas/es6/language/array-api/values.js
@@ -4,43 +4,38 @@
 describe('`Array.prototype.values` returns an iterator for all values in the array', () => {
 
   it('`values()` returns an iterator', function() {
-    const arr = ['a', 'b'];
+    const arr = ['k', 'e', 'y'];
     const iterator = arr.values();
     
-    assert.deepEqual(iterator.next(), {value: 'a', done: false});
-    assert.deepEqual(iterator.next(), {value: 'b', done: false});
     assert.deepEqual(iterator.next(), {value: void 0, done: true});
   });
   
-  it('gets all keys', function() {
+  it('use iterator to drop first key', function() {
     const arr = ['keys', 'values', 'entries'];
-    const values = [];
-    for (let value of arr.values()) {
-      values.push(value);
-    }
-    
-    assert.deepEqual(values, ['keys', 'values', 'entries']);
+    const iterator = arr.values();
+    iterator.___();
+
+    assert.deepEqual([...iterator], ['values', 'entries']);
   });
   
   it('empty array contains no values', function() {
-    const arr = [];
+    const arr = [...[...[...[...'1']]]];
     const values = [...arr.values()];
     
     assert.equal(values.length, 0);
   });
   
   it('a sparse array without real values has values though', function() {
-    const arr = [,,];
+    const arr = [, 0];
     const keys = [...arr.values()];
     
     assert.deepEqual(keys, [void 0, void 0]);
   });
   
   it('also includes holes in sparse arrays', function() {
-    const arr = ['a', , 'c'];
-    const keys = [...arr.values()];
-    
-    assert.deepEqual(keys, ['a', void 0, 'c']);
+    const arr = ['a'];
+
+    assert.deepEqual([...arr.values()], ['a', void 0, 'c']);
   });
   
 });
