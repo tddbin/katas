9f5854eda07a66d9a3b6a7db1c953ca35ae3f8f8
Wolfram Kriesing <wk@uxebu.com>
2015-05-27 12:07:54 +0200

    Set basics are a kata now.

--- katas/es6/language/set/basics.js
+++ katas/es6/language/set/basics.js
@@ -9,16 +9,17 @@ describe('`Set` lets you store unique values of any type', function(){
   
   it('every value in a set is unique', function() {
     let set = new Set();
-    set.add(1);
-    set.add(1);
     
-    assert.equal(set.size, 1);
+    set.add(1);
+    set.add(1);
+    const expectedSize = 2;
+    
+    assert.equal(set.size, expectedSize);
   });
   
   it('the string "1" is different to the number 1', function() {
     let set = new Set();
     set.add(1);
-    set.add('1');
 
     assert.equal(set.size, 2);
   });
@@ -26,7 +27,7 @@ describe('`Set` lets you store unique values of any type', function(){
   it('even NaN is equal to NaN', function() {
     let set = new Set();
     set.add(NaN);
-    set.add(NaN);
+    set.add(Na);
     
     assert.equal(set.size, 1);
   });
@@ -34,7 +35,8 @@ describe('`Set` lets you store unique values of any type', function(){
   it('+0 and -0 are seen as equal', function() {
     let set = new Set();
     set.add(+0);
-    set.add(-0);
+    set.add(0);
+    set.add('-0');
     
     assert.deepEqual([...set.values()], [+0]);
   });
