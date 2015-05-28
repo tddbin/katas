7ee04698d19589281d2146be92ebb3bab4ec0232
Wolfram Kriesing <wk@uxebu.com>
2015-05-28 23:55:11 +0300

    Restructure the meta data, to make them easier maintainable.

--- katas/es6/language/__all__.js
+++ katas/es6/language/__all__.js
@@ -14,10 +14,12 @@ const ARROW_FUNCTION_BASICS = 5;
 const ARROW_FUNCTION_BINDING = 6;
 const LET = 7;
 const CONST = 8;
+const OBJECT_LITERAL_BASICS = 9;
 const DESTRUCTURING_ARRAY = 10;
 
 const DESTRUCTURING_STRING = 11;
 const DESTRUCTURING_OBJECT = 12;
+const OBJECT_LITERAL_COMPUTED_PROPERTIES = 16;
 const ITERATOR_PROTOCOL = 39;
 
 const ARRAY_FROM = 29;
@@ -27,224 +29,307 @@ const SPREAD_WITH_ARRAYS = 20;
 const ARRAY_ENTRIES = 41;
 const ARRAY_KEYS = 42;
 const ARRAY_VALUES = 43;
+const MAP_BASICS = 44;
+const MAP_GET = 45;
+const MAP_SET = 46;
+const SET_BASICS = 47;
 
+const SPREAD_WITH_STRINGS = 21;
 export const all = {
   name: 'ES6 Katas',
-  items: {
-    [TEMPLATE_STRING_BASICS]: {
-      path: 'template-strings/basics',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
+  groups: {
+    'Template strings': {
+      items: {
+        [TEMPLATE_STRING_BASICS]: {
+          name: 'Basics',
+          description: '',
+          path: 'template-strings/basics',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        [TEMPLATE_STRING_MULTILINE]: {
+          path: 'template-strings/multiline',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        [TEMPLATE_STRING_TAGGED]: {
+          path: 'template-strings/tagged',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        [TEMPLATE_STRING_RAW]: {
+          path: 'template-strings/raw',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        }
+      }
     },
-    [TEMPLATE_STRING_MULTILINE]: {
-      path: 'template-strings/multiline',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
+    'Arrow functions': {
+      items: {
+        [ARROW_FUNCTION_BASICS]: {
+          path: 'arrow-functions/basics',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        [ARROW_FUNCTION_BINDING]: {
+          path: 'arrow-functions/binding',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        }
+      }
     },
-    [TEMPLATE_STRING_TAGGED]: {
-      path: 'template-strings/tagged',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
+    'Block scope': {
+      items: {
+        [LET]: {
+          path: 'block-scope/let',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        [CONST]: {
+          path: 'block-scope/const',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        }
+      }
     },
-    [TEMPLATE_STRING_RAW]: {
-      path: 'template-strings/raw',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
+    'Object literal': {
+      items: {
+        [OBJECT_LITERAL_BASICS]: {
+          path: 'object-literal/basics',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: [ARROW_FUNCTION_BASICS, CONST]
+        },
+        [OBJECT_LITERAL_COMPUTED_PROPERTIES]: {
+          path: 'object-literal/computed-properties',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        }
+      }
     },
-    [ARROW_FUNCTION_BASICS]: {
-      path: 'arrow-functions/basics',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
+    'Destructuring': {
+      items: {
+        [DESTRUCTURING_ARRAY]: {
+          path: 'destructuring/array',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        [DESTRUCTURING_STRING]: {
+          path: 'destructuring/string',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        [DESTRUCTURING_OBJECT]: {
+          path: 'destructuring/object',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        13: {
+          path: 'destructuring/defaults',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        14: {
+          path: 'destructuring/parameters',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        15: {
+          path: 'destructuring/rename',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+      }
     },
-    [ARROW_FUNCTION_BINDING]: {
-      path: 'arrow-functions/binding',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
+    'Unicode': {
+      items: {
+        17: {
+          path: 'unicode/in-strings',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+      }
     },
-    [LET]: {
-      path: 'block-scope/let',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
+    'Rest operator': {
+      items: {
+        18: {
+          path: 'rest/as-parameter',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        19: {
+          path: 'rest/with-destructuring',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+      }
     },
-    [CONST]: {
-      path: 'block-scope/const',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
+    'Spread operator': {
+      items: {
+        [SPREAD_WITH_ARRAYS]: {
+          path: 'spread/with-arrays',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        [SPREAD_WITH_STRINGS]: {
+          path: 'spread/with-strings',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+      }
     },
-    9: {
-      path: 'object-literal/basics',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: [ARROW_FUNCTION_BASICS, CONST]
+    'Class': {
+      items: {
+        22: {
+          path: 'class/creation',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        23: {
+          path: 'class/accessors',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        24: {
+          path: 'class/static-keyword',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        25: {
+          path: 'class/extends',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        26: {
+          path: 'class/more-extends',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        27: {
+          path: 'class/super-in-method',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        28: {
+          path: 'class/super-in-constructor',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+      }
     },
-    [DESTRUCTURING_ARRAY]: {
-      path: 'destructuring/array',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
+    'Array API': {
+      items: {
+        [ARRAY_FROM]: {
+          path: 'array-api/from',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        30: {
+          path: 'array-api/of',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        31: {
+          path: 'array-api/fill',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        32: {
+          path: 'array-api/find',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        33: {
+          path: 'array-api/findIndex',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        [ARRAY_ENTRIES]: {
+          path: 'array-api/entries',
+          level: SKILL_LEVEL.INTERMEDIATE,
+          requiresKnowledgeFrom: [CONST, ARRAY_FROM, ITERATOR_PROTOCOL]
+        },
+        [ARRAY_KEYS]: {
+          path: 'array-api/keys',
+          level: SKILL_LEVEL.INTERMEDIATE,
+          requiresKnowledgeFrom: [CONST, DESTRUCTURING_ARRAY, ARRAY_FROM, ITERATOR_PROTOCOL]
+        },
+        [ARRAY_VALUES]: {
+          path: 'array-api/values',
+          level: SKILL_LEVEL.INTERMEDIATE,
+          requiresKnowledgeFrom: [CONST, DESTRUCTURING_ARRAY, ITERATOR_PROTOCOL]
+        }
+      }
     },
-    [DESTRUCTURING_STRING]: {
-      path: 'destructuring/string',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
+    'Symbol': {
+      items: {
+        [SYMBOL_BASICS]: {
+          path: 'symbol/basics',
+          level: SKILL_LEVEL.INTERMEDIATE,
+          requiresKnowledgeFrom: []
+        },
+        35: {
+          path: 'symbol/for',
+          level: SKILL_LEVEL.INTERMEDIATE,
+          requiresKnowledgeFrom: []
+        },
+        36: {
+          path: 'symbol/keyFor',
+          level: SKILL_LEVEL.INTERMEDIATE,
+          requiresKnowledgeFrom: [35]
+        },
+      }
     },
-    [DESTRUCTURING_OBJECT]: {
-      path: 'destructuring/object',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
+    'Iterator': {
+      items: {
+        37: {
+          path: 'iterator/array',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        38: {
+          path: 'iterator/string',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: [37]
+        },
+        [ITERATOR_PROTOCOL]: {
+          path: 'iterator/protocol',
+          level: SKILL_LEVEL.EXPERT,
+          requiresKnowledgeFrom: []
+        },
+        40: {
+          path: 'iterator/usages',
+          level: SKILL_LEVEL.EXPERT,
+          requiresKnowledgeFrom: [LET, DESTRUCTURING_ARRAY, DESTRUCTURING_OBJECT, SYMBOL_BASICS, ITERATOR_PROTOCOL, SPREAD_WITH_ARRAYS]
+        },
+      }
     },
-    13: {
-      path: 'destructuring/defaults',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
+    'Map': {
+      items: {
+        [MAP_BASICS]: {
+          path: 'map/basics',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+        [MAP_GET]: {
+          path: 'map/get',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: [37]
+        },
+        [MAP_SET]: {
+          path: 'map/set',
+          level: SKILL_LEVEL.EXPERT,
+          requiresKnowledgeFrom: []
+        },
+      }
     },
-    14: {
-      path: 'destructuring/parameters',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
+    'Set': {
+      items: {
+        [SET_BASICS]: {
+          path: 'set/basics',
+          level: SKILL_LEVEL.TBD,
+          requiresKnowledgeFrom: []
+        },
+      }
     },
-    15: {
-      path: 'destructuring/rename',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    16: {
-      path: 'object-literal/computed-properties',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    17: {
-      path: 'unicode/in-strings',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    18: {
-      path: 'rest/as-parameter',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    19: {
-      path: 'rest/with-destructuring',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    [SPREAD_WITH_ARRAYS]: {
-      path: 'spread/with-arrays',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    21: {
-      path: 'spread/with-strings',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    22: {
-      path: 'class/creation',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    23: {
-      path: 'class/accessors',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    24: {
-      path: 'class/static-keyword',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    25: {
-      path: 'class/extends',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    26: {
-      path: 'class/more-extends',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    27: {
-      path: 'class/super-in-method',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    28: {
-      path: 'class/super-in-constructor',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    [ARRAY_FROM]: {
-      path: 'array-api/from',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    30: {
-      path: 'array-api/of',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    31: {
-      path: 'array-api/fill',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    32: {
-      path: 'array-api/find',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    33: {
-      path: 'array-api/findIndex',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    [SYMBOL_BASICS]: {
-      path: 'symbol/basics',
-      level: SKILL_LEVEL.INTERMEDIATE,
-      requiresKnowledgeFrom: []
-    },
-    35: {
-      path: 'symbol/for',
-      level: SKILL_LEVEL.INTERMEDIATE,
-      requiresKnowledgeFrom: []
-    },
-    36: {
-      path: 'symbol/keyFor',
-      level: SKILL_LEVEL.INTERMEDIATE,
-      requiresKnowledgeFrom: [35]
-    },
-    37: {
-      path: 'iterator/array',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: []
-    },
-    38: {
-      path: 'iterator/string',
-      level: SKILL_LEVEL.TBD,
-      requiresKnowledgeFrom: [37]
-    },
-    [ITERATOR_PROTOCOL]: {
-      path: 'iterator/protocol',
-      level: SKILL_LEVEL.EXPERT,
-      requiresKnowledgeFrom: []
-    },
-    40: {
-      path: 'iterator/usages',
-      level: SKILL_LEVEL.EXPERT,
-      requiresKnowledgeFrom: [LET, DESTRUCTURING_ARRAY, DESTRUCTURING_OBJECT, SYMBOL_BASICS, ITERATOR_PROTOCOL, SPREAD_WITH_ARRAYS]
-    },
-    [ARRAY_ENTRIES]: {
-      path: 'array-api/entries',
-      level: SKILL_LEVEL.INTERMEDIATE,
-      requiresKnowledgeFrom: [CONST, ARRAY_FROM, ITERATOR_PROTOCOL]
-    },
-    [ARRAY_KEYS]: {
-      path: 'array-api/keys',
-      level: SKILL_LEVEL.INTERMEDIATE,
-      requiresKnowledgeFrom: [CONST, DESTRUCTURING_ARRAY, ARRAY_FROM, ITERATOR_PROTOCOL]
-    },
-    [ARRAY_VALUES]: {
-      path: 'array-api/values',
-      level: SKILL_LEVEL.INTERMEDIATE,
-      requiresKnowledgeFrom: [CONST, DESTRUCTURING_ARRAY, ITERATOR_PROTOCOL]
-    }
   }
 };
--- src/build-meta-data.js
+++ src/build-meta-data.js
@@ -1,11 +1,38 @@
 export function buildMetaData(all) {
+  const items = buildItems(extractAllItems(all));
+  return {items: items};
+}
+
+function extractAllItems(all) {
+  const groups = all.groups;
+  let extractedItems = {};
+  for (let groupName in groups) {
+    extractItemsFromGroup(groups, groupName, extractedItems);
+  }
+  return extractedItems;
+}
+
+function extractItemsFromGroup(groups, groupName, addItemsTo) {
+  const items = groups[groupName].items;
+  for (let key in items) {
+    const item = cloneObject(items[key]);
+    item.groupName = groupName;
+    addItemsTo[key] = item;
+  }
+}
+
+function buildItems(flattenedItems) {
   const items = [];
-  const ids = Object.keys(all.items);
+  const ids = Object.keys(flattenedItems);
   const sortedIds = ids.sort((id1, id2) => parseInt(id1) - parseInt(id2));
   for (let id of sortedIds) {
-    let item = JSON.parse(JSON.stringify(all.items[id]));
+    let item = flattenedItems[id];
     item.id = id;
     items.push(item);
   }
-  return {items: items};
+  return items;
+}
+
+function cloneObject(obj) {
+  return JSON.parse(JSON.stringify(obj));
 }
