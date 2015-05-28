657001ab9b1a4b18ae5221fe922fd1ac4851b9bc
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-05-28 22:01:53 +0000

    Travis deployed 'master' - Thu May 28 22:01:53 UTC 2015

---
+++ katas/es6/language/set/add.js
@@ -0,0 +1,38 @@
+// 48: Set - add
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('`add()` appends a new element to the end of a Set object.', function(){
+
+  let set;
+  beforeEach(() => set = new Set());
+
+  it('adds every value, of any type, only ones', function() {
+    const fn = () => {};
+    
+    set.add(1);
+    set.add(1);
+    set.add(fn);
+    set.add({fn});
+    
+    assert.equal(set.size, 2);
+  });
+  
+  it('is chainable', function() {
+    set.add.add;
+
+    assert.equal(set.has(2), true);
+  });
+  
+  it('call without params adds undefined', function() {
+    set.add
+
+    assert.equal(set.has(void 0), true);
+  });
+  
+  it('0, -0 and +0 are equal', function() {
+    set.add();
+    set.add();
+
+    assert.equal(set.has(+0), true);
+  });
+});
