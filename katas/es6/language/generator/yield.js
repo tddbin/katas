a2852110f8033fa42b5c0abc6dae46c5d2df285e
Wolfram Kriesing <wk@uxebu.com>
2015-06-03 09:26:46 +0200

    Just tiny text fixes.

--- katas/es6/language/generator/yield.js
+++ katas/es6/language/generator/yield.js
@@ -1,17 +1,17 @@
-// 51: Generators - Yield Expressions
+// 51: Generator - Yield Expressions
 // To do: make all tests pass, leave the assert lines unchanged!
 
-describe('generator - yield keyword is used to pause and resume a generator function', () => {
+describe('generator - `yield` is used to pause and resume a generator function', () => {
   
-  function* generator() {
+  function* generatorFunction() {
     yield 'hello';
     yield 'world';
   }
   
-  let genInstance;
+  let generator;
 
   beforeEach(function() {
-    genInstance = generator();
+    generator = generatorFunction();
   });
   
   it('converting a generator to an array resumes the generator until all values are received', () => {
@@ -19,24 +19,24 @@ describe('generator - yield keyword is used to pause and resume a generator func
     assert.deepEqual(values, ['hello', 'world']);
   });
   
-  it('should have value of "hello" after first next() call', () => {
-    let thisStep = genInstance;
+  it('value is "hello" after first `next()` call', () => {
+    let thisStep = generator;
     let [value, done] = [thisStep.value, thisStep.done];
     assert.equal(value, 'hello');
     assert.equal(done, false);
   });
   
-  it('should have value of "world" after second next() call', () => {
-    let thisStep = genInstance.next();
+  it('value is "world" after second `next()` call', () => {
+    let thisStep = generator.next();
     let [value, done] = [thisStep.value, thisStep.done];  
     assert.equal(value, 'world');
     assert.equal(done, false);
   });
   
-  it('done property = true after stepping past all yield statements', () => {
-    genInstance.next();
-    genInstance.next();
-    let done = genInstance.done;
+  it('done property equals `true` after stepping past all yield statements', () => {
+    generator.next();
+    generator.next();
+    let done = generator.done;
     assert.equal(done, true);
   });
 
