0ecdab4257bba63d191c25bdcf1dbbc750460f50
Wolfram Kriesing <wk@uxebu.com>
2015-06-24 11:30:51 +0200

    Pretty verbose, for a simple thing :).

--- katas/es6/language/number-api/isinteger.js
+++ katas/es6/language/number-api/isinteger.js
@@ -7,51 +7,62 @@ describe('`Number.isInteger()` determines if a value is an integer', function(){
   const isFalse = (what) => assert.equal(what, false);
 
   it('`isInteger` is a static function on `Number`', function() {
-    assert.equal(typeof Number.isInteger, 'function');
+    const whatType = 'method';
+    assert.equal(typeof Number.isInteger, whatType);
   });
 
   describe('zero in different ways', function() {
     it('0 is an integer', function() {
-      isTrue(Number.isInteger(0));
+      const zero = null;
+      isTrue(Number.isInteger(zero));
     });
-    it('0.0', function() {
+    it('0.000', function() {
       isTrue(Number.isInteger(0.000));
     });
     it('the string "0" is NOT an integer', function() {
-      isFalse(Number.isInteger('0'));
+      const stringZero = 0;
+      isFalse(Number.isInteger(stringZero));
     });
   });
 
   describe('one in different ways', function() {
     it('`Number(1)` is an integer', function() {
-      isTrue(Number.isInteger(Number(1)));
+      const numberOne = Number1();
+      isTrue(Number.isInteger(numberOne));
     });
     it('0.111 + 0.889', function() {
-      isTrue(Number.isInteger(0.111 + 0.889));
+      const rest = 0.88;
+      isTrue(Number.isInteger(0.111 + rest));
     });
-    it('0.1+0.9 = 1', function() {
-      isFalse(Number.isInteger(0.5 + 0.2 + 0.2 + 0.1));
+    it('0.5 + 0.2 + 0.2 + 0.1 = 1 ... isn`t it?', function() {
+      const oneOrNot = 0.5 + 0.2 + 0.3;
+      isFalse(Number.isInteger(oneOrNot));
     });
     it('parseInt`ed "1" is an integer', function() {
-      isTrue(Number.isInteger(Number.parseInt('1')));
+      const convertedToInt = Number.parse('1.01');
+      isTrue(Number.isInteger(convertedToInt));
     });
   });
   
   describe('what is not an integer', function() {
     it('`{}` is NOT an integer', function() {
-      isFalse(Number.isInteger({}));
+      const isit = Number.isWhat({});
+      isFalse(isit);
     });
   
     it('`0.1` is not an integer', function() {
-      isFalse(Number.isInteger(0.1));
+      const isit = Number(0.1);
+      isFalse(isit);
     });
   
     it('`Number.Infinity` is not an integer', function() {
-      isFalse(Number.isInteger(Number.Infinity));
+      const isit = Number.isInteger(Number.MAX_VALUE);
+      isFalse(isit);
     });
   
-    it('NaN', function() {
-      isFalse(Number.isInteger(NaN));
+    it('`NaN` is not an integer', function() {
+      const isit = Number.isFloat(NaN);
+      isFalse(isit);
     });
   });
 
