3c5b37ff9dee79268ae89e53ea0b33e6f8d4c733
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-06-25 13:13:19 +0000

    Travis deployed 'master' - Thu Jun 25 13:13:19 UTC 2015

--- katas/es6/language/arrow-functions/binding.js
+++ katas/es6/language/arrow-functions/binding.js
@@ -6,6 +6,7 @@ describe('arrow functions have lexical `this`, no dynamic `this`', () => {
   it('bound at definition time, use `=>` ', function() {
     var bound = new LexicallyBound();
     var fn = bound.getFunction();
+    
     assert.strictEqual(fn(), bound);
   });
  
@@ -13,12 +14,15 @@ describe('arrow functions have lexical `this`, no dynamic `this`', () => {
     var bound = new LexicallyBound();
     var fn = bound.getFunction();
     var anotherObj = {};
-    assert.strictEqual(fn.call(anotherObj), anotherObj);
+    var expected = anotherObj;
+    
+    assert.strictEqual(fn.call(anotherObj), expected);
   });
   
   it('`arguments` doesnt work inside arrow functions', function() {
     var bound = new LexicallyBound();
     var fn = bound.getArgumentsFunction();
+    
     assert.equal(fn(1, 2).length, 0);
   });
   
