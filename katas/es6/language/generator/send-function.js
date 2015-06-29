3379555919c1ecb28c2ca6314b6de0467ac3d8d6
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-06-29 11:48:12 +0000

    Travis deployed 'master' - Mon Jun 29 11:48:12 UTC 2015

---
+++ katas/es6/language/generator/send-function.js
@@ -0,0 +1,39 @@
+// 56: Generator - Send function to a generator
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('pass a function to a generator', () => {
+
+  it('the generator can receive a function as a value', function() {
+    let fn = function() {};
+    
+    function* generatorFunction() {
+      assert.equal(yield null, fn);
+    }
+
+    let iterator = generatorFunction();
+    iterator.next();
+    iterator.next();
+  });
+
+  it('pass a function to the iterator, which calls it', function() {
+    function* generatorFunction() {
+      yield (yield 1)();
+    }
+
+    var iterator = generatorFunction();
+    var iteratedOver = [iterator.next().value, iterator.next().value];
+    
+    assert.deepEqual([1, 2], iteratedOver);
+  });
+
+  it('nesting yielded function calls', function() {
+    function* generatorFunction() {
+      yield (yield (yield 1)())();
+    }
+
+    var iteratedOver = [];
+    
+    assert.deepEqual([1, 2, 3], iteratedOver);
+  });
+
+});
