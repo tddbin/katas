05cffea57e23d6fddd30b4e6b01920bcb4b6a379
Wolfram Kriesing <wk@uxebu.com>
2015-07-08 10:03:36 +0200

    Made it a kata, tests failing.

--- katas/es6/language/reflect/getprototypeof.js
+++ katas/es6/language/reflect/getprototypeof.js
@@ -5,37 +5,41 @@ describe('`Reflect.getPrototypeOf` returns the prototype', function() {
 
   it('works like `Object.getPrototypeOf`', function() {
     const viaObject = Object.getPrototypeOf({});
-    const viaReflect = Object.getPrototypeOf({});
+    const viaReflect = Reflect.getPrototypeOf();
     
     assert.strictEqual(viaObject, viaReflect);
   });
 
   it('throw TypeError for a non-object', function() {
-    assert.throws(() => Reflect.getPrototypeOf(null), TypeError);
+    let fn = () => { Reflect.getPrototypeOf({}) };
+    assert.throws(fn, TypeError);
   });
 
-  it('a `Number(1)` has a prototype', function() {
-    const number = {};
+  it('a `new Set()` has a prototype', function() {
+    const aSet = Set;
     
-    assert.equal(Reflect.getPrototypeOf(number), Object.prototype);
+    assert.equal(Reflect.getPrototypeOf(aSet), Set.prototype);
   });
 
   it('for a class, it is `Klass.prototype`', function() {
     class Klass {}
+    const proto = new Klass();
     
-    assert.equal(Reflect.getPrototypeOf(new Klass()), Klass.prototype);
+    assert.equal(proto, Klass.prototype);
   });
 
   it('for a old-style class, works too', function() {
     function Klass() {}
+    const proto = Reflect.getPrototypeOf();
     
-    assert.equal(Reflect.getPrototypeOf(new Klass()), Klass.prototype);
+    assert.equal(proto, Klass.prototype);
   });
 
-  it('coerce to object', function() {
+  it('coerce to object (?)', function() {
     let arr = [];
+    const expectedProto = Array;
     
-    assert.equal(Reflect.getPrototypeOf(arr), Array.prototype);
+    assert.equal(Reflect.getPrototypeOf(arr), expectedProto);
   });
 
   // TODO
@@ -45,7 +49,4 @@ describe('`Reflect.getPrototypeOf` returns the prototype', function() {
   //   PRs welcome
   //   assert.equal(Reflect.getPrototypeOf(namespace exotic object), null);
   // });
-
-  
-
 });
