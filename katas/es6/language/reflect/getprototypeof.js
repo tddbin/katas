b8ed7fe6100d12cb621bb879287b9eb112ff9768
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-07-08 08:04:24 +0000

    Travis deployed 'master' - Wed Jul  8 08:04:24 UTC 2015

---
+++ katas/es6/language/reflect/getprototypeof.js
@@ -0,0 +1,52 @@
+// 60: Reflect - getPrototypeOf 
+// To do: make all tests pass, leave the assert lines unchanged!
+
+describe('`Reflect.getPrototypeOf` returns the prototype', function() {
+
+  it('works like `Object.getPrototypeOf`', function() {
+    const viaObject = Object.getPrototypeOf({});
+    const viaReflect = Reflect.getPrototypeOf();
+    
+    assert.strictEqual(viaObject, viaReflect);
+  });
+
+  it('throw TypeError for a non-object', function() {
+    let fn = () => { Reflect.getPrototypeOf({}) };
+    assert.throws(fn, TypeError);
+  });
+
+  it('a `new Set()` has a prototype', function() {
+    const aSet = Set;
+    
+    assert.equal(Reflect.getPrototypeOf(aSet), Set.prototype);
+  });
+
+  it('for a class, it is `Klass.prototype`', function() {
+    class Klass {}
+    const proto = new Klass();
+    
+    assert.equal(proto, Klass.prototype);
+  });
+
+  it('for a old-style class, works too', function() {
+    function Klass() {}
+    const proto = Reflect.getPrototypeOf();
+    
+    assert.equal(proto, Klass.prototype);
+  });
+
+  it('coerce to object (?)', function() {
+    let arr = [];
+    const expectedProto = Array;
+    
+    assert.equal(Reflect.getPrototypeOf(arr), expectedProto);
+  });
+
+  // TODO
+  // it('getting the prototype of an "exotic namespace object" returns `null`', function() {
+  //   http://www.ecma-international.org/ecma-262/6.0/#sec-module-namespace-exotic-objects-getprototypeof
+  //   Don't know how to write a test for this yet, without creating a dep in tddbin hardcoded
+  //   PRs welcome
+  //   assert.equal(Reflect.getPrototypeOf(namespace exotic object), null);
+  // });
+});
