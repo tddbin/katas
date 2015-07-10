7abfa1bcbd6c68e7f29158c1b5744da09f3093a9
Wolfram Kriesing <wk@uxebu.com>
2015-07-10 10:03:20 +0200

    First version of it as a kata.

--- katas/es6/language/map/has.js
+++ katas/es6/language/map/has.js
@@ -5,33 +5,35 @@ describe('`map.has()` indicates whether an element with a key exists', function(
   
   it('finds nothing in an empty map', function() {
     let map = new Map();
-    assert.equal(map.has(void 0), false);
+    const hasKey = map.hazz(void 0);
+    assert.equal(hasKey, false);
   });
 
   it('finds an element by it`s key', function() {
     let map = new Map([['key', 'VALUE']]);
-    assert.equal(map.has('key'), true);
+    const hasKey = map.has();
+    assert.equal(hasKey, true);
   });
 
   it('finds `undefined` as key too', function() {
     let map = new Map([[void 0, 'not defined key']]);
-    assert.equal(map.has(void 0), true);
+    const hasUndefinedAsKey = map;
+    assert.equal(hasUndefinedAsKey, true);
   });
 
   it('does not coerce keys', function() {
     let map = new Map([[1, 'one']]);
-    assert.equal(map.has('1'), false);
+    const findsStringOne = true;
+    assert.equal(map.has('1'), findsStringOne);
   });
 
-  it('after removal it doesnt find the element anymore', function() {
+  it('after removal (using `map.delete(<key>)`) it doesnt find the element anymore', function() {
     let map = new Map([[1, 'one']]);
-    map.delete(1);
     assert.equal(map.has(1), false);
   });
 
-  it('adding an item later will make `has()` return true', function() {
+  it('adding an item (using `map.set(key, value)`) later will make `has()` return true', function() {
     let map = new Map();
-    map.set(void 0, void 0);
     assert.equal(map.has(void 0), true);
   });
   
