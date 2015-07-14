0a6fd0c38032101f1f925531c9bfe2c307748a6f
Wolfram Kriesing <wk@uxebu.com>
2015-07-14 22:37:35 +0200

    Fix comment.

--- scripts/build.sh
+++ scripts/build.sh
@@ -24,7 +24,7 @@ cp $ORIGIN_ROOT/CNAME $DIST_ROOT/CNAME;
 cp $ORIGIN_ROOT/.nojekyll $DIST_ROOT/;
 
 cp $ORIGIN_ROOT/html/proxy.html $DIST_ROOT/;
-# replace place holder KATAS_SERVICE_DOMAIN with env var, so it can be different in dev/prod mode
+# replace place holder TDDBIN_ROOT_DOMAIN with env var, so it can be different in dev/prod mode
 if [[ $OSTYPE == darwin* ]]; then 
   sed -i '' "s/\${TDDBIN_ROOT_DOMAIN}/$TDDBIN_ROOT_DOMAIN/g" $DIST_ROOT/proxy.html
 else
