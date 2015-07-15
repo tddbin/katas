78de82e2026e674baee5eda1eed9704b78b238cc
Wolfram Kriesing <wk@uxebu.com>
2015-07-15 15:38:16 +0200

    Make it a kata.

--- katas/es6/language/set/delete.js
+++ katas/es6/language/set/delete.js
@@ -1,4 +1,4 @@
-// ??: Set - delete
+// 64: Set - delete
 // To do: make all tests pass, leave the assert lines unchanged!
 
 describe('`set.delete()` deletes an element from a set', function(){
@@ -11,18 +11,18 @@ describe('`set.delete()` deletes an element from a set', function(){
       set.add('one').add('two').add('three');
     });
     it('returns `true`', function() {
-      const returns = set.delete('two');
-      assert.equal(returns, true);
+      const returns = set.remove;
+      assert.strictEqual(returns, true);
     });
     it('the size decreases', function() {
-      set.delete('two');
       assert.equal(set.size, 2);
     });
   });
   
   describe('if nothing was deleted (no element with the given value was found)', function() {
     it('returns `false`', function() {
-      const returns = set.delete('fourty-two');
+      set.add('one');
+      const returns = set.delete('one');
       
       assert.equal(returns, false);
     });
@@ -30,15 +30,11 @@ describe('`set.delete()` deletes an element from a set', function(){
   
   describe('`undefined` is a valid value in a set', function() {
     it('deleting it, when it is not in the set, returns `false` too', function() {
-      const returns = set.delete();
-      assert.equal(returns, false);
+      assert.equal(set.delete(whatToDelete), false);
     });
     
     it('`delete()` removes it, when its in the set', function() {
-      set.add(void 0);
-      const returns = set.delete();
-      
-      assert.equal(returns, true);
+      assert.equal(set.delete(), true);
     });
   });
   
@@ -46,9 +42,8 @@ describe('`set.delete()` deletes an element from a set', function(){
   describe('the value does NOT get casted', function() {
     it('number 1 is different to string "1"', function() {
       set.add(1);
-      const returns = set.delete('1');
-      
-      assert.equal(returns, false);
+      set.add('1');
+      assert.equal(set.delete('1'), false);
     });
   });
 });
