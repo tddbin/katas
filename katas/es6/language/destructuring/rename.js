d7e74fca3241fe841d7b7419b4cc4482921cac27
Wolfram Kriesing <wk@uxebu.com>
2015-07-28 12:32:29 +0200

    Renaming as discussed in #14.

--- katas/es6/language/destructuring/rename.js
+++ katas/es6/language/destructuring/rename.js
@@ -1,26 +1,26 @@
-// 15: destructuring - rename
+// 15: destructuring - assign
 // To do: make all tests pass, leave the assert lines unchanged!
 
-describe('rename variables while destructuring', () => {
+describe('assign variables to a different name while destructuring', () => {
 
-  it('rename object key', () => {
+  it('assign object key', () => {
     const {x: newName} = {x: 1};
     assert.equal(y, 1);
   });
   
-  it('object key rename with default value', () => {
+  it('object key assign with default value', () => {
     const {x: y=2} = {y: 23};
     assert.equal(y, 42);
   });
   
-  it('rename param in a function param', () => {
+  it('assign param in a function param', () => {
     const fn = ({x}) => {
       assert.equal(y, 1);
     };
     fn({x: 1});
   });
   
-  it('rename param in a function param with default value', () => {
+  it('assign param in a function param with default value', () => {
     const fn = ({x: z=3}) => {
       assert.equal(y, 3);
     };
