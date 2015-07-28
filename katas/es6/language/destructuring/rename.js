30adf3e0cf5fa3ae0b5e58f1da55994a6b0e76d6
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-07-28 10:38:55 +0000

    Travis deployed 'master' - Tue Jul 28 10:38:55 UTC 2015

--- katas/es6/language/destructuring/rename.js
+++ katas/es6/language/destructuring/rename.js
@@ -1,30 +1,34 @@
-// 15: destructuring - rename
+// 15: destructuring - assign
 // To do: make all tests pass, leave the assert lines unchanged!
 
-describe('rename variables while destructuring', () => {
+describe('assign variables to a different name while destructuring', () => {
 
-  it('rename object key', () => {
-    const {x: newName} = {x: 1};
-    assert.equal(y, 1);
-  });
-  
-  it('object key rename with default value', () => {
-    const {x: y=2} = {y: 23};
-    assert.equal(y, 42);
-  });
-  
-  it('rename param in a function param', () => {
-    const fn = ({x}) => {
+  describe('for simple objects', function() {
+    it('use a colon after the original name, like so `originalName: newName`', () => {
+      const {x: newName} = {x: 1};
       assert.equal(y, 1);
-    };
-    fn({x: 1});
+    });
+    
+    it('assign a new name and give it a default value using `= <default value>`', () => {
+      const {x: y=2} = {y: 23};
+      assert.equal(y, 42);
+    });
   });
-  
-  it('rename param in a function param with default value', () => {
-    const fn = ({x: z=3}) => {
-      assert.equal(y, 3);
-    };
-    fn({});
+
+  describe('for function parameter names', function() {
+    it('do it the same way, with a colon behind it', () => {
+      const fn = ({x}) => {
+        assert.equal(y, 1);
+      };
+      fn({x: 1});
+    });
+    
+    it('giving it a default value is possible too, like above', () => {
+      const fn = ({x: z=3}) => {
+        assert.equal(y, 3);
+      };
+      fn({});
+    });
   });
   
 });
