61ee965196e8939df7fe727da8d8f732ba7b3418
Wolfram Kriesing <wk@uxebu.com>
2015-07-28 12:37:01 +0200

    Improving the description and grouping it a bit.

--- katas/es6/language/destructuring/rename.js
+++ katas/es6/language/destructuring/rename.js
@@ -3,28 +3,30 @@
 
 describe('assign variables to a different name while destructuring', () => {
 
-  it('assign object key', () => {
+  it('use a colon after the original name, like so `originalName: newName`', () => {
     const {x: newName} = {x: 1};
     assert.equal(y, 1);
   });
   
-  it('object key assign with default value', () => {
+  it('assign a new name and give it a default value using `= <default value>`', () => {
     const {x: y=2} = {y: 23};
     assert.equal(y, 42);
   });
-  
-  it('assign param in a function param', () => {
-    const fn = ({x}) => {
-      assert.equal(y, 1);
-    };
-    fn({x: 1});
-  });
-  
-  it('assign param in a function param with default value', () => {
-    const fn = ({x: z=3}) => {
-      assert.equal(y, 3);
-    };
-    fn({});
+
+  describe('for function parameter names', function() {
+    it('do it the same way, with a colon behind it', () => {
+      const fn = ({x}) => {
+        assert.equal(y, 1);
+      };
+      fn({x: 1});
+    });
+    
+    it('giving it a default value is possible too, like above', () => {
+      const fn = ({x: z=3}) => {
+        assert.equal(y, 3);
+      };
+      fn({});
+    });
   });
   
 });
