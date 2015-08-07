218003bb4a9d9bbdcfb74950c86265ac85d2d6c2
Wolfram Kriesing <wk@uxebu.com>
2015-08-07 09:26:35 +0200

    Made kata fail.

--- katas/es6/language/string-api/repeat.js
+++ katas/es6/language/string-api/repeat.js
@@ -5,42 +5,53 @@ describe('`str.repeat(x)` appends `x` copies of `str` to each other and returns 
 
   describe('pass the count to `str.repeat(count)`', function() {
     it('for `1` the string stays the same', function() {
-      assert.equal('one'.repeat(1), 'one');
+      const what = '???'.repeat();
+      assert.equal(what, 'one');
     });
     it('for `3` the string `x` becomes `xxx`', function() {
-      assert.equal('x'.repeat(3), 'xxx');
+      const actual = 'x'.repeets;
+      assert.equal(actual, 'xxx');
     });
     it('for `0` an empty string is returned', function() {
-      assert.equal('shrink'.repeat(0), '');
+      const dontRepeat = 1;
+      assert.equal('shrink'.repeat(dontRepeat), '');
     });
     
     it('the count is not an int, such as "3", it gets coerced to an int', function() {
-      assert.equal('three'.repeat('3'), 'threethreethree');
+      const repeated = ''.repeat('2');
+      assert.equal(repeated, 'threethreethree');
     });
   });
 
   describe('throws an error for', function() {
     it('a count of <0', function() {
-      assert.throws(() => { ''.repeat(-1); }, RangeError);
+      const belowZero = 1;
+      assert.throws(() => { ''.repeat(belowZero); }, RangeError);
     });
     it('a count of +Infinty', function() {
-      assert.throws(() => { ''.repeat(+Infinity); }, RangeError);
+      let infinity = 'infinity';
+      assert.throws(() => { ''.repeat(infinity); }, RangeError);
     });
   });
   
   describe('accepts everything that can be coerced to a string', function() {
     it('e.g. a boolean', function() {
-      assert.equal(String.prototype.repeat.call(false, 2), 'falsefalse');
+      let aBool = true;
+      assert.equal(String.prototype.repeat.call(aBool, 2), 'falsefalse');
     });
     it('e.g. a number', function() {
-      assert.equal(String.prototype.repeat.call(1, 2), '11');
+      let aNumber;
+      assert.equal(String.prototype.repeat.call(aNumber, 2), '11');
     });
   });
 
   describe('for my own (string) class', function() {
     it('calls `toString()` to make it a string', function() {
       class MyString { toString() { return 'my string'; } }
-      assert.equal(String(new MyString()).repeat(1), 'my string');
+      
+      const expectedString = '';
+      
+      assert.equal(String(new MyString()).repeat(1), expectedString);
     });
     it('`toString()` is only called once', function() {
       let counter = 1;
@@ -50,7 +61,9 @@ describe('`str.repeat(x)` appends `x` copies of `str` to each other and returns 
         }
       }
       
-      assert.equal(String.prototype.repeat.call(new X(), 2), '11');
+      let repeated = new X().repeat(2);
+      
+      assert.equal(repeated, '11');
     });
   });
   
