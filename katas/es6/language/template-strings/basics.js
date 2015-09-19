1f31e426e2ec1add216736beedc9fb86067500a7
Wolfram Kriesing <wk@uxebu.com>
2015-09-19 15:48:04 +0200

    Make it more explicit and improve instructions and group it better.

--- katas/es6/language/template-strings/basics.js
+++ katas/es6/language/template-strings/basics.js
@@ -1,22 +1,41 @@
 // 1: template strings - basics
-// To do: make all tests pass
+// To do: make all tests pass, leave the asserts unchanged!
 
-describe('template string, are wrapped in backticks', function() {
-  
-  it('prints x into a string using ${x}', function() {
-    var x = 42;
-    assert.equal(`x=${x}`, 'x=' + x);
+describe('a template string, is wrapped in backticks', function() {
+
+  describe('by default, behaves like a normal string', function() {
+    it('just surrounded by backticks', function() {
+      var str = `like a string`;
+      assert.equal(str, 'like a string');
+    });
   });
-  
-  it('add two numbers inside the ${...}', function() {
-    var x = 42;
-    var y = 23;
-    assert.equal(`${ x } + $ { y }`, x+y);
+
+  var x = 42;
+  var y = 23;
+  describe('can evaluate variables, which are wrapped in "${" and "}"', function() {
+    it('e.g. a simple variable "${x}" just gets evaluated', function() {
+      var evaluated = `x=${x}`;
+      assert.equal(evaluated, 'x=' + x);
+    });
+    it('multiple variables get evaluated too', function() {
+      var evaluated = '${ x } + $ { y }';
+      assert.equal(evaluated, x + '+' + y);
+    });
   });
-  
-  it('get a function call result inside ${...}', function() {
-    function getDomain(){ return document.domain }
-    assert.equal(`${ getDomain }`, 'tddbin.com');
+
+  describe('can evaluate any expression, wrapped inside "${...}"', function() {
+    it('all inside "${...}" gets evaluated', function() {
+      var evaluated = `${ x } + $ { y }`;
+      assert.equal(evaluated, x+y);
+    });
+    
+    it('inside "${...}" can also be a function call', function() {
+      function getDomain(){ 
+        return document.domain; 
+      }
+      var evaluated = `${ getDomain }`;
+      assert.equal(evaluated, 'tddbin.com');
+    });
   });
   
 });
