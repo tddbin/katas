7490311f18795914e3cf3c79dbe33b6718624fb2
Wolfram Kriesing <wk@uxebu.com>
2015-09-21 16:00:01 +0200

    Just new lines to be consistent.

--- katas/es6/language/template-strings/basics.js
+++ katas/es6/language/template-strings/basics.js
@@ -4,26 +4,33 @@
 describe('a template string, is wrapped in ` (backticks) instead of \' or "', function() {
 
   describe('by default, behaves like a normal string', function() {
+    
     it('just surrounded by backticks', function() {
       var str = ``;
       assert.equal(str, 'like a string');
     });
+    
   });
 
   var x = 42;
   var y = 23;
+  
   describe('can evaluate variables, which are wrapped in "${" and "}"', function() {
+    
     it('e.g. a simple variable "${x}" just gets evaluated', function() {
       var evaluated = `x=#x`;
       assert.equal(evaluated, 'x=' + x);
     });
+    
     it('multiple variables get evaluated too', function() {
       var evaluated = '${ x } + $ { y }';
       assert.equal(evaluated, x + '+' + y);
     });
+    
   });
 
   describe('can evaluate any expression, wrapped inside "${...}"', function() {
+    
     it('all inside "${...}" gets evaluated', function() {
       var evaluated = `${ x } + ${ y }`;
       assert.equal(evaluated, x+y);
@@ -36,6 +43,7 @@ describe('a template string, is wrapped in ` (backticks) instead of \' or "', fu
       var evaluated = `${ getDomain }`;
       assert.equal(evaluated, 'tddbin.com');
     });
+    
   });
   
 });
