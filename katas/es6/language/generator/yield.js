8b5700d74a130b0a97db1196f0b00cea6f277897
Wolfram Kriesing <wk@uxebu.com>
2015-09-24 11:40:24 +0200

    Some newlines for more readability.

--- katas/es6/language/generator/yield.js
+++ katas/es6/language/generator/yield.js
@@ -20,25 +20,31 @@ describe('generator - `yield` is used to pause and resume a generator function',
   });
 
   describe('after the first `generator.next()` call', function() {
+    
     it('the value is "hello"', function() {
       const {value} = generator.next;
       assert.equal(value, 'hello');
     });
+    
     it('and `done` is false', function() {
       const {done} = generator;
       assert.equal(done, false);
     });
+    
   });
 
   describe('after the second `next()` call', function() {
+    
     let secondItem;
     beforeEach(function() {
       secondItem = generator.next();
     });
+    
     it('`value` is "world"', function() {
       let {value} = secondItem;
       assert.equal(value, 'world');
     });
+    
     it('and `done` is still false', function() {
       const done = secondItem;
       assert.equal(done, false);
@@ -46,12 +52,14 @@ describe('generator - `yield` is used to pause and resume a generator function',
   });
 
   describe('after stepping past the last element, calling `next()` that often', function() {
+    
     it('`done` property equals true, since there is nothing more to iterator over', function() {
       generator.next();
       generator.next();
       let done = generator.done;
       assert.equal(done, true);
     });
+    
   });
 
 });
