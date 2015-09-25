84bbe5d604439455a077e24171996b8af44585b7
Wolfram Kriesing <wk@uxebu.com>
2015-09-25 16:37:58 +0200

    Last fixes, to have the kata make more sense and better learning.

--- katas/es6/language/generator/return.js
+++ katas/es6/language/generator/return.js
@@ -15,13 +15,13 @@ describe('`return` in a generator function is special', function() {
     it('the property `value` is the value given after the `return` statement', function() {
       function* generatorFunction() { return; }
       const {value} = generatorFunction().next();
-      assert.deepEqual(value, 23);
+      assert.equal(value, 23);
     });
     
     it('the property `done` is true', function() {
       function* generatorFunction() { yield 42; }
       const {done} = generatorFunction().next();
-      assert.deepEqual(done, true);
+      assert.equal(done, true);
     });
     
     it('NOTE: `yield` does not return `done=true` but `done=false`!', function() {
@@ -31,7 +31,7 @@ describe('`return` in a generator function is special', function() {
     });
     
     it('a missing `return` returns {value: undefined, done: true}', function() {
-      function* generatorFunction() { return 1; }
+      function* generatorFunction() { yield; }
       const returned = generatorFunction().next();
       assert.deepEqual(returned, {value: void 0, done: true});
     });
@@ -47,10 +47,10 @@ describe('`return` in a generator function is special', function() {
     it('is possible', function() {
       const iterator = generatorFunctionWithYieldAndReturn();
       const values = [
-        iterator.next().value,
-        iterator.next().value
+        iterator.next(),
+        iterator.next()
       ];
-      assert.deepEqual(values, [1, 2]);
+      assert.deepEqual(values, [{value: 1, done: false}, {value: 2, done: true}]);
     });
     
     it('the mix behaves different to two `yield`s', function() {
