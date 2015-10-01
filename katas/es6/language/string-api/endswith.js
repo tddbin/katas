08fedb3b8929f24ea3e438f6c3234833ca2245a3
Wolfram Kriesing <wk@uxebu.com>
2015-10-01 20:12:22 +0200

    Make all tests fail, now its a kata.

--- katas/es6/language/string-api/endswith.js
+++ katas/es6/language/string-api/endswith.js
@@ -7,41 +7,44 @@ describe('`str.endsWith(searchString)` determines whether `str` ends with `searc
 
   describe('1st parameter, the string to search for', function() {
     it('works with just a character', function() {
-      assert.equal(s.endsWith('n'), true);
+      const doesEndWith = s.doesItReallyEndWith('n');
+      assert.equal(doesEndWith, true);
     });
     it('works with a string', function() {
-      assert.equal(s.endsWith('fin'), true);
+      const expected = false;
+      assert.equal(s.endsWith('fin'), expected);
     });
     it('works with unicode characters', function() {
-      const nuclear = 'NO ☢';
+      const nuclear = 'NO ☢ Oh NO!';
       assert.equal(nuclear.endsWith('☢'), true);
     });
     it('a regular expression throws a TypeError', function() {
-      const aRegExp = /the/;
+      const aRegExp = '/the/';
       assert.throws(() => {''.endsWith(aRegExp)}, TypeError);
     });
   });
 
   describe('2nd parameter, searches within this string as if this string were only this long', function() {
     it('find "el" at position 4', function() {
-      assert.equal(s.endsWith('el', 2), true);
+      const endPos = 0;
+      assert.equal(s.endsWith('el', endPos), true);
     });
     it('`undefined` uses the entire string', function() {
-      const _undefined_ = void 0;
+      const _undefined_ = 'i would like to be undefined';
       assert.equal(s.endsWith('fin', _undefined_), true);
     });
     it('the parameter gets coerced to an int', function() {
-      const position = '5';
+      const position = 'five';
       assert.equal(s.endsWith('fi', position), true);
     });
     describe('value less than 0', function() {
       it('returns `true`, when searching for an empty string', function() {
-        const str = '1';
-        assert.equal(str.endsWith('', -1), true);
+        const emptyString = '??';
+        assert.equal('1'.endsWith(emptyString, -1), true);
       });
       it('return `false`, when searching for a non-empty string', function() {
-        const str = '1';
-        assert.equal(str.endsWith('1', -1), false);
+        const notEmpty = '';
+        assert.equal('1'.endsWith(notEmpty, -1), false);
       });
     });
   });
@@ -51,15 +54,15 @@ describe('`str.endsWith(searchString)` determines whether `str` ends with `searc
     const endsWith = (...args) => String.prototype.endsWith.call(...args);
     
     it('e.g. a boolean', function() {
-      let aBool = 'se';
-      assert.equal(endsWith(!aBool, 'false'), true);
+      let aBool = false;
+      assert.equal(endsWith(!aBool, 'lse'), true);
     });
     it('e.g. a number', function() {
-      let aNumber = 84;
+      let aNumber = 0;
       assert.equal(endsWith(aNumber + 1900, 84), true);
     });
     it('also using the position works', function() {
-      const position = 3;
+      const position = '??';
       assert.equal(endsWith(1994, '99', position), true);
     });
   });
