7301a37c1e455d27a8cf49eb63139ec0dce30059
Wolfram Kriesing <wk@uxebu.com>
2015-10-09 10:13:32 +0200

    Make it a real kata, changed the sync tests to be async too, since that was just fake before :).

--- katas/es6/language/promise/basics.js
+++ katas/es6/language/promise/basics.js
@@ -4,59 +4,60 @@
 describe('a Promise represents an operation that hasn`t completed yet, but is expected in the future', function() {
 
   it('`Promise` is a global function', function() {
-    assert.equal(typeof Promise, 'function');
+    const expectedType = '???';
+    assert.equal(typeof Promise, expectedType);
   });
 
   describe('the constructor', function() {
   
     it('instantiating it without params throws', function() {
-      assert.throws(() => { new Promise(); });
+      const fn = () => { Promise }
+      assert.throws(fn);
     });  
     
     it('expects a function', function() {
-      assert.doesNotThrow(() => { new Promise(() => {}); });
+      const param = null;
+      assert.doesNotThrow(() => { new Promise(param); });
     });  
     
   });
 
-  describe('a synchronous promise', function() {
+  describe('simplest promises', function() {
   
-    it('resolve a promise by calling the `resolve` function given as first parameter to the constructor', function() {
+    it('resolve a promise by calling the `resolve` function given as first parameter', function(done) {
+      let promise = new Promise((resolve) => {
+      });
+      
+      promise
+        .then(() => done())
+        .catch(() => done(new Error('The promise is expected to resolve.')));
+    });
+  
+    it('the `resolve` function can return a value, that is consumed by the `promise.then()` callback', function(done) {
       let promise = new Promise((resolve) => {
         resolve();
       });
       
       promise
-        .then(() => assert.ok(true))
-        .catch(() => assert.ok(false, 'The promise is expected to resolve.'));
+        .then(value => {assert.equal(value, 42); done(); })
+        .catch(() => done(new Error('The promise is expected to resolve.')));
     });
   
-    it('the `resolve` function can return a value, that is consumed by the `promise.then()` callback', function() {
-      let promise = new Promise((resolve) => {
-        resolve(42);
+    it('rejecting a promise is done by calling the callback given as 2nd parameter', function(done) {
+      let promise = new Promise(() => {
       });
       
       promise
-        .then((value) => assert.equal(value, 42))
-        .catch(() => assert.ok(false, 'The promise is expected to resolve.'));
-    });
-  
-    it('rejecting a promise is done by calling the callback given as 2nd parameter to the constructor', function() {
-      let promise = new Promise((resolve, reject) => {
-        reject();
-      });
-      
-      promise
-        .then(() => assert.ok(false, 'The promise is expected to be rejected.'))
-        .catch(() => assert.ok(true));
+        .then(() => done(new Error('The promise is expected to be rejected.')))
+        .catch(() => done());
     });
 
   });
 
   describe('an asynchronous promise', function() {
   
-    it('can resolve later, also by calling the first callback given to the constructor', function(done) {
-      let promise = new Promise((resolve) => {
+    it('can resolve later, also by calling the first callback', function(done) {
+      let promise = new Promise(() => {
         setTimeout(() => resolve(), 100);
       });
       
@@ -65,8 +66,8 @@ describe('a Promise represents an operation that hasn`t completed yet, but is ex
         .catch(() => done(new Error('The promise is expected to resolve.')));
     });
   
-    it('reject it at some later point in time, calling the 2nd callback given to the constructor', function(done) {
-      let promise = new Promise((resolve, reject) => {
+    it('reject it at some later point in time, calling the 2nd callback', function(done) {
+      let promise = new Promise((reject) => {
         setTimeout(() => reject(), 100);
       });
       
@@ -80,8 +81,8 @@ describe('a Promise represents an operation that hasn`t completed yet, but is ex
   describe('test library (mocha here) support for promises', function() {
     
     it('just returning the promise makes the test library check that the promise resolves', function() {
-      let promise = new Promise((resolve) => {
-        setTimeout(() => resolve(), 100);
+      let promise = new Promise((reject, resolve) => {
+        resolve();
       });
       
       // return the promise to mocha, it has the checking for promise resolving built in, when it receives a promise
