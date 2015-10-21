b6549f48ff0ef1374ab294e4a7be410479ddb517
Wolfram Kriesing <wk@uxebu.com>
2015-10-21 11:54:34 +0200

    Made all tests fail.

--- katas/javascript/language/array/sort-basics.js
+++ katas/javascript/language/array/sort-basics.js
@@ -4,25 +4,33 @@
 describe('`[].sort()` sorts an array using each value as a string', function() {
 
   it('is a function on the array prototype', function() {
-    assert.equal(typeof [].sort, 'function');
+    const theType = '???';
+    assert.equal(typeof [].sort, theType);
   });
 
   describe('sorts characters', function() {
   
     it('in alphabetical order', function() {
-      assert.deepEqual(['b', 'a'].sort(), ['a', 'b']);
+      const sorted = ['b', 'a'].sort;
+      assert.deepEqual(sorted, ['a', 'b']);
     });
   
     it('upper case characters come first', function() {
-      assert.deepEqual(['B', '\u{61}', 'C'].sort(), ['B', 'C', 'a']);
+      const sorted = ['a', 'B', 'C'];
+      assert.deepEqual(['B', '\u{61}', 'C'].sort(), sorted);
     });
   
     it('depending on their position in the unicode table', function() {
-      assert.deepEqual(['+', '*', '(', ')'].sort(), ['(', ')', '*', '+']);
+      const sorted = ['+', '*', '(', ')'];
+      assert.deepEqual(sorted, ['(', ')', '*', '+']);
     });
   
     it('unicode characters depending on their code point', function() {
-      assert.deepEqual(['\u{1F602}', '\u{1F603}', '\u{1F601}'].sort(), ['\u{1F601}', '\u{1F602}', '\u{1F603}']);
+      const grinningFace = '\u{1F601}';
+      const grinningEyes = '\u{1F602}';
+      const withTears = '\u{1F603}';
+      const smilies = [grinningEyes];
+      assert.deepEqual(smilies.sort(), [grinningFace, '\u{1F602}', withTears]);
     });
     
   });
@@ -30,11 +38,13 @@ describe('`[].sort()` sorts an array using each value as a string', function() {
   describe('sorts strings', function() {
     
     it('considering the string from start to end', function() {
-      assert.deepEqual(['aa', 'Ba'].sort(), ['Ba', 'aa']);
+      const sortedResult = ['????'];
+      assert.deepEqual(['aa', 'Ba'].sort(), sortedResult);
     });
     
     it('shorter strings go to front', function() {
-      assert.deepEqual(['Balls', 'Ball'].sort(), ['Ball', 'Balls']);
+      const balls = ['Ball', 'Ball s', ' bald'].sort();
+      assert.deepEqual(balls, ['Ball', 'Balls', 'bald']);
     });
     
   });
@@ -42,11 +52,13 @@ describe('`[].sort()` sorts an array using each value as a string', function() {
   describe('sorts numbers as if they were strings', function() {
     
     it('`1` and `2` are sorted as expected', function() {
-      assert.deepEqual([1, 2].sort(), [1, 2]);
+      const numbers = [3, 4, 2, 1, 5];
+      assert.deepEqual(numbers.sort(), [1, 2, 5]);
     });
     
     it('see multi digit numbers as strings!', function() {
-      assert.deepEqual([1, 2, 11].sort(), [1, 11, 2]);
+      const sortedNumbers = [___];
+      assert.deepEqual([1, 2, 11].sort(), sortedNumbers);
     });
     
   });
