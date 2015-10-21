d3f4c5a56cfb4e86a26423baec1bed062ac93bdf
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-10-21 09:59:03 +0000

    Travis deployed 'master' - Wed Oct 21 09:59:03 UTC 2015

---
+++ katas/javascript/language/__raw-metadata__.js
@@ -0,0 +1,41 @@
+import {SKILL_LEVEL} from '../../skill-levels';
+import * as tag from '../../tags';
+import {toUtcDate}  from '../../date';
+import * as date from '../../date';
+
+const ARRAY_SORT_BASICS = 1;
+
+export const all = {
+  name: 'JavaScript Katas',
+  groups: {
+    'Array': {
+      items: {
+        [ARRAY_SORT_BASICS]: {
+          name: '`[].sort()` basics',
+          description: '.',
+          path: 'array/sort-basics',
+          level: SKILL_LEVEL.BEGINNER,
+          requiresKnowledgeFrom: [],
+          publishDateUTC: new Date(Date.UTC(2015, date.OCTOBER)),
+          links: [
+            {
+              url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort',
+              comment: 'Very detailed description of how sort() works.',
+              tags: [tag.MDN, tag.DOCS]
+            },
+            {
+              url: 'https://en.wikipedia.org/wiki/List_of_Unicode_characters#Basic_Latin',
+              comment: 'All the basic latin characters (close to ASCII).',
+              tags: [tag.WIKIPEDIA, tag.DOCS]
+            },
+            {
+              url: 'https://en.wikipedia.org/wiki/Emoji#Blocks',
+              comment: 'Some emoji icons and their unicode data.',
+              tags: [tag.WIKIPEDIA, tag.DOCS]
+            }
+          ]
+        }
+      }
+    }
+  }
+};
