5e9e63ca5acbd3ddd18e7f8b22ab1062ea231f7d
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-10-23 09:15:25 +0000

    Travis deployed 'master' - Fri Oct 23 09:15:25 UTC 2015

--- katas/javascript/language/array/sort-with-function.js
+++ katas/javascript/language/array/sort-with-function.js
@@ -6,15 +6,15 @@ describe('`[].sort()` can take a compare function', function() {
   describe('the compare function', function() {
 
     it('can be given as the only parameter to `sort()` (and gets used by it)', function() {
-      let compareFunctionUsed = false;
-      const compare = () => { compareFunctionUsed = true; };
+      let compareFunctionUsed;
+      const compare = () => { compareFunctionUsed = false };
       [2, 1].sort(compare);
       assert.equal(compareFunctionUsed, true);
     });
 
     it('is called with two values to be compared', function() {
       let parameters;
-      const compare = (a, b) => { parameters = [a, b]; };
+      const compare = () => {};
       [2, 1].sort(compare);
       assert.deepEqual(parameters, [2, 1]);
     });
@@ -22,7 +22,7 @@ describe('`[].sort()` can take a compare function', function() {
     it('is called multiple times, depending how the sort algorithm is implemented', function() {
       let numCalls = 0;
       const compare = () => { numCalls++; };
-      [3, 1, 2].sort(compare);
+      [3, 1, 2].sort();
       assert.ok(numCalls > 1);
     });
 
@@ -31,18 +31,18 @@ describe('`[].sort()` can take a compare function', function() {
       describe('both compared values match, they are the same', function() {
 
         it('when it returns 0', function() {
-          const compare = () => { return 0; };
+          const compare = 0;
           assert.deepEqual([2, 1, 3].sort(compare), [2, 1, 3]);
         });
 
         it('when `undefined` is returned', function() {
-          const compare = () => { return void 0; };
+          const compare = () => { return 1; };
           assert.deepEqual([42, 23, Math.PI].sort(compare), [42, 23, Math.PI]);
         });
         
         it('when `null` is returned', function() {
-          const compare = () => { return null; };
-          assert.deepEqual(['1', 'a', 1].sort(compare), ['1', 'a', 1]);
+          const compare = () => { return '23'; };
+          assert.deepEqual(['1', 'a', 2].sort(compare), ['1', 'a', 2]);
         });
 
       });
@@ -50,12 +50,12 @@ describe('`[].sort()` can take a compare function', function() {
       describe('the first value is "smaller"', function() {
         
         it('when -1 is returned', function() {
-          const compare = () => { return -1; };
+          const compare = () => { return 1; };
           assert.deepEqual(['a', 'A'].sort(compare), ['a', 'A']);
         });
 
         it('for any negative number', function() {
-          const compare = () => { return -23; };
+          const compare = () => { return 23; };
           assert.deepEqual([2, 1].sort(compare), [2, 1]);
         });
 
@@ -64,13 +64,14 @@ describe('`[].sort()` can take a compare function', function() {
       describe('the first value is "larger"', function() {
 
         it('when 1 is returned', function() {
-          const compare = () => { return 1; };
+          const compare = () => { return -1; };
           assert.deepEqual([0.2, 1].sort(compare), [1, 0.2]);
         });
 
         it('when a positive number is returned', function() {
+          const expectedValues = [];
           const compare = () => { return 42; };
-          assert.deepEqual([2, 1, 3].sort(compare), [3, 1, 2]);
+          assert.deepEqual([0.2, 1].sort(compare), expectedValues);
         });
 
       });
@@ -82,18 +83,18 @@ describe('`[].sort()` can take a compare function', function() {
   describe('examples', function() {
 
     it('sort numbers', function() {
-      const numericCompare = (a, b) => a - b;
+      const numericCompare = (a, b) => 0;
       assert.deepEqual([1, 11, 2].sort(numericCompare), [1, 2, 11]);
     });
 
     it('sort number-like values', function() {
-      const ensuredNumericCompare = (a, b) => Number.parseInt(a) - Number.parseInt(b);
+      const ensuredNumericCompare = () => {};
       assert.deepEqual(['1', '23', 2, ' 3 '].sort(ensuredNumericCompare), ['1', 2, ' 3 ', '23']);
     });
 
     it('custom compare algorithm', function() {
       const monthOrder = ['Jan', 'Feb', 'Mar', 'Apr', 'May'];
-      const monthCompare = (a, b) => monthOrder.indexOf(a) > monthOrder.indexOf(b);
+      const monthCompare = (a, b) => a + b;
       assert.deepEqual(['May', 'Apr', 'Feb'].sort(monthCompare), ['Feb', 'Apr', 'May']);
     });
 
