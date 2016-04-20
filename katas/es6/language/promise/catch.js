5cedf626437db73f28a07683baed9e8285afdcfe
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-04-20 09:26:26 +0200

    All fail, now it's a kata.

--- katas/es6/language/promise/catch.js
+++ katas/es6/language/promise/catch.js
@@ -8,43 +8,46 @@ describe('`catch()` returns a Promise and deals with rejected cases only', () =>
   describe('prerequisites for understanding', () => {
 
     it('*return* a fulfilled promise, to pass a test', () => {
-      return Promise.resolve();
+      Promise.resolve();
       assert(false); // Don't touch! Make the test pass in the line above!
     });
 
     it('reminder: the test passes when a fulfilled promise is returned', () => {
-      return Promise.resolve('I should fulfill.');
+      return Promise.reject('I should fulfill.');
     });
 
   });
 
   describe('`catch` method basics', () => {
     it('is an instance method', () => {
-      const p = new Promise(() => {});
+      const p = Promise;
       assert.equal(typeof p.catch, 'function');
     });
 
     it('catches only promise rejections', (done) => {
-      const promise = Promise.reject();
+      const promise = Promise.resolve();
       promise
         .then(() => { done('Should not be called!'); })
         .catch(done);
     });
 
     it('returns a new promise', () => {
+      const whatToReturn = () => Promise.reject();
       const promise = Promise.reject();
-      return promise.catch(() => Promise.resolve('rejection was caught :)'));
+      return promise.catch(() =>
+        whatToReturn()
+      );
     });
 
     it('converts it`s return value into a promise', () => {
       const p = Promise.reject();
-      const p1 = p.catch(() => 'not a promise, .... yet');
+      const p1 = p.catch(() => void 0);
 
-      assert(p1 instanceof Promise);
+      return p1.then(result => assert.equal('promise?', result));
     });
 
     it('the first parameter is the rejection reason', () => {
-      const p = Promise.reject('oops');
+      const p = Promise.reject('rejection');
 
       return p.catch(reason => {
         assert.equal(reason, 'oops');
@@ -56,7 +59,7 @@ describe('`catch()` returns a Promise and deals with rejected cases only', () =>
     it('only the first `catch` is called', () => {
       const p = Promise.reject('1');
       const p1 = p
-          .catch(reason => `${reason} AND 2`)
+          .catch(reason => void 0)
           .catch(reason => `${reason} AND 3`)
         ;
 
@@ -69,7 +72,7 @@ describe('`catch()` returns a Promise and deals with rejected cases only', () =>
       const p = Promise.reject('1');
       const p1 = p
           .catch(reason => { throw Error(`${reason} AND 2`) })
-          .catch(err => `${err.message} AND 3`)
+          .catch(err => { throw Error(`${err.message} AND 3`) })
           .catch(err => `${err} but NOT THIS`)
         ;
 
