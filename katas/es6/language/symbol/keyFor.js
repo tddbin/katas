0dc80fc6af1bba9bc9adaf778cd1e3386b04ca09
Bohdan Hontarenko <tuoefil@gmail.com>
2016-07-25 10:21:42 +0300

    Update keyFor.js

--- katas/es6/language/symbol/keyFor.js
+++ katas/es6/language/symbol/keyFor.js
@@ -19,6 +19,12 @@ describe('`Symbol.keyFor()` gets the symbol key for a given symbol', function() 
     assert.equal(key, void 0);
   });
   
+  it('predefined symbols are not in the runtime-wide registry either', function() {
+    const key = Symbol.keyFor(Symbol.iteraTor);
+ 
+    assert.equal(key, void 0);
+  });
+  
   it('for non-Symbols throws an error', function() {
     function fn() {
       Symbol.keyFor(sym);
