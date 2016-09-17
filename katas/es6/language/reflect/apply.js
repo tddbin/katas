a5ea2c53478ce0144547e21671dada9696239162
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-09-17 12:35:04 +0200

    A bit more compact formatting, the new style :).

--- katas/es6/language/reflect/apply.js
+++ katas/es6/language/reflect/apply.js
@@ -5,22 +5,18 @@ describe('`Reflect.apply` calls a target function', function() {
 
   it('it is a static method', function() {
     const expectedType = '???';
-
     assert.equal(typeof Reflect.apply, expectedType)
   });
 
   describe('the 1st parameter', () => {
     it('is a callable, e.g. a function', () => {
       let fn;
-
       assert.equal(Reflect.apply(fn, void 0, []), 42);
     });
-
     it('passing it a non-callable throws a TypeError', function() {
       let applyOnUncallable = () => {
         Reflect.apply(Array);
       };
-
       assert.throws(applyOnUncallable, TypeError);
     });
   });
@@ -31,9 +27,7 @@ describe('`Reflect.apply` calls a target function', function() {
       fn() {return this.value}
     }
     let instance = new FourtyTwo();
-
     const fourtyTwo = Reflect.apply(instance.fn);
-
     assert.deepEqual(fourtyTwo, 42);
   });
 
@@ -42,10 +36,8 @@ describe('`Reflect.apply` calls a target function', function() {
       const thirdParam = 'should be array-like';
       assert.doesNotThrow(() => Reflect.apply(() => void 0, null, thirdParam));
     });
-
     it('is an array of parameters passed to the call', function() {
       let emptyArrayWithFiveElements = Reflect.apply(Array);
-
       assert.deepEqual(emptyArrayWithFiveElements.fill(42), [42, 42, 42, 42, 42]);
     });
   });
