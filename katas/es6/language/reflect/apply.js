b442c82ca6e1cfc3c66f43c0039a8e78ff010cc8
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2016-09-17 10:38:50 +0000

    Travis deployed 'master' - Sat Sep 17 10:38:50 UTC 2016

--- katas/es6/language/reflect/apply.js
+++ katas/es6/language/reflect/apply.js
@@ -5,40 +5,61 @@ describe('`Reflect.apply` calls a target function', function() {
 
   it('it is a static method', function() {
     const expectedType = '???';
-    
     assert.equal(typeof Reflect.apply, expectedType)
   });
-  
-  it('it calls a callable, e.g. a function', function() {
-    let fn;
-    
-    assert.equal(Reflect.apply(fn,undefined,[]), 42);
+
+  describe('the 1st parameter', () => {
+    it('is a callable, e.g. a function', () => {
+      let fn;
+      assert.equal(Reflect.apply(fn, void 0, []), 42);
+    });
+    it('passing it a non-callable throws a TypeError', function() {
+      let applyOnUncallable = () => {
+        Reflect.apply(Array);
+      };
+      assert.throws(applyOnUncallable, TypeError);
+    });
   });
-  
-  it('passing it a non-callable throws a TypeError', function() {
-    let applyOnUncallable = () => { 
-      Reflect.apply(Array); 
-    };
-    
-    assert.throws(applyOnUncallable, TypeError);
-  });
-  
-  it('the second argument is the scope (or the `this`)', function() {
+
+  it('the 2nd parameter is the scope (or the `this`)', function() {
     class FourtyTwo {
       constructor() { this.value = 42}
       fn() {return this.value}
     }
     let instance = new FourtyTwo();
-    
     const fourtyTwo = Reflect.apply(instance.fn);
-    
     assert.deepEqual(fourtyTwo, 42);
   });
-  
-  it('the 3rd argument is an array of parameters passed to the call', function() {
-    let emptyArrayWithFiveElements = Reflect.apply(Array);
-    
-    assert.deepEqual(emptyArrayWithFiveElements.fill(42), [42, 42, 42, 42, 42]);
+
+  describe('the 3rd parameter', () => {
+    it('must be an array (or array-like)', () => {
+      const thirdParam = 'should be array-like';
+      assert.doesNotThrow(() => Reflect.apply(() => void 0, null, thirdParam));
+    });
+    it('is an array of parameters passed to the call', function() {
+      let emptyArrayWithFiveElements = Reflect.apply(Array);
+      assert.deepEqual(emptyArrayWithFiveElements.fill(42), [42, 42, 42, 42, 42]);
+    });
   });
-  
+
+  describe('example usages', () => {
+    it('simple function call', () => {
+      const fn = () => ':(';
+      assert.equal(Reflect.apply(fn, void 0, []), 'the return value');
+    });
+    it('call a function on an array', () => {
+      const fn = [].join;
+      assert.deepEqual(Reflect.apply(fn, [0, 23, 42], [1]), [23, 42]);
+    });
+    it('pass in the `this` that the function to call needs', () => {
+      class Bob {
+        constructor() { this._name = 'Bob'; }
+        name() { return this._name; }
+      }
+      const bob = new Bob();
+      const scope = Bob;
+      assert.equal(Reflect.apply(bob.name, scope, []), 'Bob');
+    });
+  });
+
 });
