c05f2bd476e4c817ca0129d9b9ec21f1dd616820
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-09-17 12:20:04 +0200

    Better structuring, focus on the params.

--- katas/es6/language/reflect/apply.js
+++ katas/es6/language/reflect/apply.js
@@ -5,33 +5,35 @@ describe('`Reflect.apply` calls a target function', function() {
 
   it('it is a static method', function() {
     const expectedType = '???';
-    
+
     assert.equal(typeof Reflect.apply, expectedType)
   });
-  
-  it('it calls a callable, e.g. a function', function() {
-    let fn;
-    
-    assert.equal(Reflect.apply(fn, void 0, []), 42);
+
+  describe('the 1st parameter', () => {
+    it('is a callable, e.g. a function', () => {
+      let fn;
+
+      assert.equal(Reflect.apply(fn, void 0, []), 42);
+    });
+
+    it('passing it a non-callable throws a TypeError', function() {
+      let applyOnUncallable = () => {
+        Reflect.apply(Array);
+      };
+
+      assert.throws(applyOnUncallable, TypeError);
+    });
   });
-  
-  it('passing it a non-callable throws a TypeError', function() {
-    let applyOnUncallable = () => { 
-      Reflect.apply(Array); 
-    };
-    
-    assert.throws(applyOnUncallable, TypeError);
-  });
-  
-  it('the second argument is the scope (or the `this`)', function() {
+
+  it('the 2nd parameter is the scope (or the `this`)', function() {
     class FourtyTwo {
       constructor() { this.value = 42}
       fn() {return this.value}
     }
     let instance = new FourtyTwo();
-    
+
     const fourtyTwo = Reflect.apply(instance.fn);
-    
+
     assert.deepEqual(fourtyTwo, 42);
   });
 
