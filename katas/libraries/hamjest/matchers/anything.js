d303bc710396a44610645df0190ce48ea7d3106c
Wolfram Kriesing <wolfram+github@kriesing.de>
2017-08-22 12:27:31 +0200

    Write all tests for `anything()`.

--- katas/libraries/hamjest/matchers/anything.js
+++ katas/libraries/hamjest/matchers/anything.js
@@ -1,10 +1,34 @@
 import {
-  assertThat, equalTo, not,
+  assertThat, equalTo,
+  anything,
+  Matcher,
 } from 'hamjest';
 
-describe('`anything()`, matches any value', () => {
-  it('', () => {
+describe('`anything()`, matches any value and never fails', () => {
+  describe('for example', () => {
+    it('`undefined`', () => {
+      assertThat(undefined, anything());
+    });
+    it('any number', () => {
+      assertThat(42, anything());
+    });
+    it('an object', () => {
+      assertThat({any: 'object'}, anything());
+    });
+    it('an `Array`', () => {
+      assertThat(Array, anything());
+    });
+    it('just anything', () => {
+      assertThat(Math.PI, anything());
+      assertThat(Number.MAX_SAFE_INTEGER, anything());
+      assertThat(0, anything());
+      assertThat('', anything());
+    });
+  });
 
+  it('and returns a hamjest matcher', () => {
+    const value = anything();
+    assertThat('Expected to get a hamjest Matcher.', Matcher.isMatcher(value), equalTo(true));
   });
 });
 
