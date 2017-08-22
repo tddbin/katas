be0d8f9b41387977050d35a83569cc65a3db7e16
Wolfram Kriesing <wolfram+github@kriesing.de>
2017-08-22 12:33:07 +0200

    Implement `defined()` tests.

--- katas/libraries/hamjest/matchers/defined.js
+++ katas/libraries/hamjest/matchers/defined.js
@@ -1,8 +1,36 @@
 import {
   assertThat, equalTo, not,
+  defined,
+  Matcher,
 } from 'hamjest';
 
-// defined()
-// Matches everything except undefined:
+describe('The matcher `defined()`, matches everything but `undefined`', () => {
+  describe('for example', () => {
+    it('any number', () => {
+      assertThat(42, defined());
+    });
+    it('an object', () => {
+      assertThat({any: 'object'}, defined());
+    });
+    it('an `Array`', () => {
+      assertThat(Array, defined());
+    });
+    it('just about anything', () => {
+      assertThat(Math.PI, defined());
+      assertThat(Number.MAX_SAFE_INTEGER, defined());
+      assertThat(0, defined());
+      assertThat('', defined());
+    });
+  });
+  describe('but not', () => {
+    it('`undefined`', () => {
+      assertThat(undefined, not(defined()));
+    });
+  });
 
+  it('and returns a hamjest matcher', () => {
+    const value = defined();
+    assertThat('Expected to get a hamjest Matcher.', Matcher.isMatcher(value), equalTo(true));
+  });
+});
 
