bd6bd0a41129007f13e51dbccc6df911fd118574
Wolfram Kriesing <wolfram+github@kriesing.de>
2017-08-27 19:39:05 +0200

    Started with `not()`.

--- katas/libraries/hamjest/matchers/not.js
+++ katas/libraries/hamjest/matchers/not.js
@@ -1,7 +1,42 @@
 import {
-  assertThat, equalTo, not,
+  assertThat, equalTo, not, strictlyEqualTo,
+  Matcher,
 } from 'hamjest';
 
-// not(valueOrMatcher)
-// Matches if the given matcher fails and vice versa. As usual, if the given value is not a matcher, it is wrapped with equalTo.
+describe('The function `not()`, works like an inverted `equalTo()`', () => {
+  describe('when given a value', () => {
+    describe('for example', () => {
+      it('the (integer) numbers 0 and 1', () => {
+        assertThat(0, not(1));
+        assertThat(0, not(equalTo(1)));
+      });
+      it('two (decimal) numbers', () => {
+        assertThat(1.23, not(3.21));
+        assertThat(1.23, not(equalTo(3.21)));
+      });
+      it('the letters `a` and `A`', () => {
+        assertThat('a', not('A'));
+        assertThat('a', not(equalTo('A')));
+      });
+      it('two arrays with different content', () => {
+        assertThat([1], not([2]));
+        assertThat([1], not(equalTo([2])));
+      });
+      it('but NOT for two arrays with same content', () => {
+        assertThat([1], not(not([1]))); // NOTE the `not(not())`
+        assertThat([1], not(not(equalTo([1]))));
+      });
+    });
+  });
+  describe('when given a matcher', () => {
+    it('', () => {
+
+    });
+  });
+  
+  it('and returns a hamjest matcher', () => {
+    const value = not();
+    assertThat('Expected to get a hamjest Matcher.', Matcher.isMatcher(value), equalTo(true));
+  });
+});
 
