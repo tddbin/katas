d75ad10da318bc8ca2758905a5c64c2d05c132f7
Wolfram Kriesing <wolfram+github@kriesing.de>
2017-08-28 22:48:06 +0200

    Try to make a kata out of each test.

--- katas/libraries/hamjest/assertThat.js
+++ katas/libraries/hamjest/assertThat.js
@@ -14,11 +14,13 @@ describe('The core function, `assertThat()`', () => {
   });
   describe('requires at least two params', () => {
     it('1st: the actual value', () => {
+      //// const actual = undefined;
       const actual = 'actual';
       const expected = equalTo('actual');
       assertThat(actual, expected);
     });
     it('2nd: a matcher for the expected value', () => {
+      ////
       const matcher = equalTo('expected');
       assertThat('expected', matcher);
     });
@@ -27,6 +29,7 @@ describe('The core function, `assertThat()`', () => {
         let caughtError;
         const reason = 'This is the reason, the first `assertThat()` throws as part of its message.';
         try {
+          //// assertThat(reason, true, equalTo(true));
           assertThat(reason, true, equalTo(false));
         } catch (e) {
           caughtError = e;
@@ -38,10 +41,12 @@ describe('The core function, `assertThat()`', () => {
 
   describe('under the hood, it does', () => {
     it('nothing, WHEN actual and expected match (using the given matcher)', () => {
+      //// const passingTest = () => assertThat(true, equalTo(false));
       const passingTest = () => assertThat(true, equalTo(true));
       assertThat(passingTest, returns());
     });
     it('throw an assertion, WHEN actual and expected don`t match', () => {
+      //// const failingTest = () => 'use assertThat() to write a failing test';
       const failingTest = () => assertThat(false, equalTo(true));
       assertThat(failingTest, throws());
     });
