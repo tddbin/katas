feb5fa731e7cb47e51c32c3bcb28a6f4c8e5059a
Wolfram Kriesing <wolfram+github@kriesing.de>
2017-09-07 00:23:41 +0200

    Start with kataify.

---
+++ src/kataifier.spec.js
@@ -0,0 +1,20 @@
+import { assertThat } from 'hamjest';
+import { buildFunctionSpy, wasCalledWith } from 'hamjest-spy';
+
+const kataify = (dependencies) => {
+  dependencies.writeFile('');
+};
+
+describe('Kataify a file', () => {
+  it('an empty file stays as is', () => {
+    const emptyFile = '';
+    const writeFile = buildFunctionSpy();
+    const dependencies = {
+      readFile: () => Promise.resolve(emptyFile),
+      writeFile,
+    };
+    kataify(dependencies);
+
+    assertThat(writeFile, wasCalledWith(emptyFile));
+  });
+});
