0de5c3afc00b0bf9418a0ad0b37ba19109ce883e
Wolfram Kriesing <wolfram+github@kriesing.de>
2017-09-10 22:53:42 +0200

    Make sure it works async.

--- src/deploy.spec.js
+++ src/deploy.spec.js
@@ -5,8 +5,9 @@ import {
 import { buildFunctionSpy, wasCalledWith } from 'hamjest-spy';
 
 const FilterKatasDir = (dependencies) => {
+  const readFiles = dependencies.readFiles;
   return {
-    forMetadataFiles: () => []
+    forMetadataFiles: () => readFiles().then(() => [])
   };
 };
 
@@ -16,17 +17,22 @@ describe('Filter katas-dir', () => {
   //    - exclude metadata files AND
   //      all JS files on the root of `katas` dir
   describe('for metadata files', () => {
-    it('find none when there are no files at all', () => {
+    it('find none when there are no files at all', async () => {
       const noFiles = [];
       const readFiles = () => Promise.resolve(noFiles);
-      const metadataFiles = FilterKatasDir({readFiles}).forMetadataFiles();
+      const metadataFiles = await FilterKatasDir({readFiles}).forMetadataFiles();
       assertThat(metadataFiles, equalTo(noFiles));
     });
-    it('find none when its on the root dir', () => {
+    it('find none when its on the root dir', async () => {
       const oneFile = ['__raw-metadata__.js'];
       const readFiles = () => Promise.resolve(oneFile);
-      const metadataFiles = FilterKatasDir({readFiles}).forMetadataFiles();
+      const metadataFiles = await FilterKatasDir({readFiles}).forMetadataFiles();
       assertThat(metadataFiles, equalTo([]));
+    });
+    it('works async', () => {
+      const noFiles = [];
+      const readFiles = () => Promise.resolve(noFiles);
+      return promiseThat(FilterKatasDir({readFiles}).forMetadataFiles(), fulfilled());
     });
   });
 
