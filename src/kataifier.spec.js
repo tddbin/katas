1d32f0927a70d6cbb513b7629b26af7bc95962b9
Wolfram Kriesing <wolfram+github@kriesing.de>
2017-10-03 21:24:19 +0200

    Read+write once for one file.

--- src/kataifier.spec.js
+++ src/kataifier.spec.js
@@ -3,7 +3,7 @@ import {
   assertThat
 } from 'hamjest';
 import {
-  buildFunctionSpy, wasNotCalled
+  buildFunctionSpy, wasNotCalled, callCountWas,
 } from 'hamjest-spy';
 
 type Filename = string;
@@ -13,25 +13,38 @@ type KataifyableFile = {
 };
 type KataifyableFileList = Array<KataifyableFile>;
 
-const kataify = (param, deps) => {
-
+const kataify = (kataifyableList, deps) => {
+  if (kataifyableList.length) {
+    return deps.readFile()
+      .then(deps.writeFile)
+    ;
+  }
 };
 
 describe('Kataify files', () => {
-  it('WHEN no files given, dont read any file', async () => {
-    const deps = {
-      readFile: buildFunctionSpy(),
-      writeFile: buildFunctionSpy(),
+  const buildDeps = () => {
+    return {
+      readFile: buildFunctionSpy({ returnValue: Promise.resolve() }),
+      writeFile: buildFunctionSpy({ returnValue: Promise.resolve() }),
     };
+  };
+
+  it('WHEN no files given, dont read any file', async () => {
+    const deps = buildDeps();
     await kataify([], deps);
     assertThat(deps.readFile, wasNotCalled());
   });
-  it('WHEN one kataifyable files given, read once and write once', async () => {
-    const deps = {
-      readFile: buildFunctionSpy(),
-      writeFile: buildFunctionSpy(),
+  it('WHEN one kataifyable file given, read once and write once', async () => {
+    const deps = buildDeps();
+    const oneFile = {
+      sourceFileName: '/src/file.js',
+      destinationFilename: '/dest/file.js',
     };
-    await kataify([], deps);
-    assertThat(deps.readFile, wasCalledWith());
+    await kataify([oneFile], deps);
+    assertThat(deps.readFile, callCountWas(1));
+    assertThat(deps.writeFile, callCountWas(1));
   });
 });
+
+// needs to filter files
+// build the KataifyableList from the src+dest dir
