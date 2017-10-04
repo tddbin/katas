97f5862e4db9a68e92e14016ee717bc038a9c872
Wolfram Kriesing <wolfram+github@kriesing.de>
2017-10-04 22:07:14 +0200

    Don't forget what to do next.

--- src/kataifiables.spec.js
+++ src/kataifiables.spec.js
@@ -8,3 +8,15 @@ import {
 
 // needs to filter files
 // build the KataifyableList from the src+dest dir
+
+describe('Build list of kataifiable files', () => {
+  xit('exclude all JS files on root of srcDir', () => {
+    // TODO
+    const props = {
+      sourceDir: '/source/dir',
+      destinationDir: '/destination/dir',
+    };
+    const deps = {};
+    assertThat(buildKataifiables(props, deps), equalTo([]));
+  });
+});
