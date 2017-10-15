66676ab7acdea41f487d81ac2cdf4f9b0fe7eefb
Wolfram Kriesing <wolfram+github@kriesing.de>
2017-10-15 13:51:09 +0200

    Add typing to the deploy-util.

--- src/deploy-util.js
+++ src/deploy-util.js
@@ -1,4 +1,21 @@
-export const toSrcDestPairs = (files, {sourcePath, destinationPath}) =>
+// @flow
+type DirnameType = string;
+type FilenameType = string;
+type FilenameListType = Array<FilenameType>;
+type SrcDestPairType = {
+  sourceFileName: FilenameType,
+  destinationFilename: FilenameType,
+};
+type SrcDestPairListType = Array<SrcDestPairType>;
+
+type ToSrcDestPairsDepsType = {
+  sourcePath: DirnameType,
+  destinationPath: DirnameType,
+};
+export const toSrcDestPairs = (
+  files: FilenameListType,
+  {sourcePath, destinationPath}: ToSrcDestPairsDepsType
+): SrcDestPairListType =>
   files.map(file => ({
     sourceFileName: file,
     destinationFilename: file.replace(sourcePath, destinationPath)
