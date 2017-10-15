18a4927355facf589cc0ddd9a83d88b130f8522a
Wolfram Kriesing <wolfram+github@kriesing.de>
2017-10-15 12:47:41 +0200

    Convert a list of files.

--- src/deply.spec.js
+++ src/deply.spec.js
@@ -6,13 +6,27 @@ import {
   buildFunctionSpy, wasNotCalled, callCountWas, wasCalledWith,
 } from 'hamjest-spy';
 
-const toSrcDestPairs = (files) => {
-  return [];
-};
 
 describe('Build src-dest pairs from file names', () => {
+  const srcPath = '/src/path';
+  const destPath = '/dest/path';
+  const toSrcDestPairs = (files) =>
+    files.map(file => ({
+      sourceFileName: file,
+      destinationFilename: file.replace(srcPath, destPath)
+    }))
+  ;
+
   it('WHEN no file names given return empty pairs', () => {
     const noFiles = [];
     assertThat(toSrcDestPairs(noFiles), equalTo([]));
+  });
+  it('WHEN one file given, replace src path with dest path', () => {
+    const oneSrcFile = [`${srcPath}/file.js`];
+    const onePair = [{
+      sourceFileName: `${srcPath}/file.js`,
+      destinationFilename: `${destPath}/file.js`
+    }];
+    assertThat(toSrcDestPairs(oneSrcFile), equalTo(onePair));
   });
 });
