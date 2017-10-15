ce6da21958934ebce083cd203550128ad61e1792
Wolfram Kriesing <wolfram+github@kriesing.de>
2017-10-15 12:50:54 +0200

    Extract the function with its deps.

--- src/deply.spec.js
+++ src/deply.spec.js
@@ -2,19 +2,20 @@ import {describe, it} from 'mocha';
 import {
   assertThat, equalTo,
 } from 'hamjest';
-import {
-  buildFunctionSpy, wasNotCalled, callCountWas, wasCalledWith,
-} from 'hamjest-spy';
 
+const toSrcDestPairs = (files, {sourcePath, destinationPath}) =>
+  files.map(file => ({
+    sourceFileName: file,
+    destinationFilename: file.replace(sourcePath, destinationPath)
+  }))
+;
 
 describe('Build src-dest pairs from file names', () => {
-  const srcPath = '/src/path';
-  const destPath = '/dest/path';
+  const sourcePath = '/src/path';
+  const destinationPath = '/dest/path';
+  const deps = {sourcePath, destinationPath};
   const buildPairs = (files) =>
-    files.map(file => ({
-      sourceFileName: file,
-      destinationFilename: file.replace(srcPath, destPath)
-    }))
+    toSrcDestPairs(files, deps)
   ;
 
   it('WHEN no file names given return empty pairs', () => {
@@ -22,10 +23,10 @@ describe('Build src-dest pairs from file names', () => {
     assertThat(buildPairs(noFiles), equalTo([]));
   });
   it('WHEN one file given, replace src path with dest path', () => {
-    const oneSrcFile = [`${srcPath}/file.js`];
+    const oneSrcFile = [`${sourcePath}/file.js`];
     const onePair = [{
-      sourceFileName: `${srcPath}/file.js`,
-      destinationFilename: `${destPath}/file.js`
+      sourceFileName: `${sourcePath}/file.js`,
+      destinationFilename: `${destinationPath}/file.js`
     }];
     assertThat(buildPairs(oneSrcFile), equalTo(onePair));
   });
