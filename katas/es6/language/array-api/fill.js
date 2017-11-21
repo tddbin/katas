c626f77b2285e3aa1ac7f7f570a07b4438c69c98
Wolfram Kriesing <wolfram+github@kriesing.de>
2017-11-21 23:11:18 +0100

    Another kataified.

--- katas/es6/language/array-api/fill.js
+++ katas/es6/language/array-api/fill.js
@@ -4,19 +4,22 @@
 describe('`Array.prototype.fill` can fill up an array with one value', () => {
 
   it('`fill(0)` will populate `0` into each array element', function() {
-    const arr = new Array(3).fill();
+    //// const arr = new Array(3).fill();
+    const arr = new Array(3).fill(0);
     
     assert.deepEqual(arr, [0, 0, 0]);
   });
 
   it('fill only changes content, adds no new elements', function() {
-    const arr = [undefined].fill(0);
-    
+    //// const arr = [undefined].fill(0);
+    const arr = [].fill(0);
+
     assert.deepEqual(arr, []);
   });
 
   it('second parameter to `fill()` is the position where to start filling', function() {
-    const fillPosition = 0;
+    //// const fillPosition = 0;
+    const fillPosition = 2;
     const arr = [1,2,3].fill(42, fillPosition);
     
     assert.deepEqual(arr, [1, 2, 42]);
@@ -24,7 +27,8 @@ describe('`Array.prototype.fill` can fill up an array with one value', () => {
 
   it('third parameter is the position where filling stops', function() {
     const fillStartAt = 1;
-    const fillEndAt = 1;
+    //// const fillEndAt = 1;
+    const fillEndAt = 2;
     const arr = [1,2,3].fill(42, fillStartAt, fillEndAt);
     
     assert.deepEqual(arr, [1, 42, 3]);
