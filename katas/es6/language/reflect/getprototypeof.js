6902752a8c2de0a19e10649acaa06cb73dc0b112
wolframkriesing <wolfram+github@kriesing.de>
2018-09-08 17:41:18 +0200

    Kataify.

--- katas/es6/language/reflect/getprototypeof.js
+++ katas/es6/language/reflect/getprototypeof.js
@@ -2,43 +2,38 @@
 // To do: make all tests pass, leave the assert lines unchanged!
 
 describe('`Reflect.getPrototypeOf` returns the prototype', function() {
-
   it('works like `Object.getPrototypeOf`', function() {
     const viaObject = Object.getPrototypeOf({});
-    const viaReflect = Reflect.getPrototypeOf();
-    
+    //// const viaReflect = Reflect.getPrototypeOf();
+    const viaReflect = Reflect.getPrototypeOf({});
     assert.strictEqual(viaObject, viaReflect);
   });
-
   it('throws a TypeError for a non-object', function() {
-    let fn = () => { Reflect.getPrototypeOf({}) };
+    //// let fn = () => { Reflect.getPrototypeOf({}) };
+    let fn = () => { Reflect.getPrototypeOf(8) };
     assert.throws(fn, TypeError);
   });
-
   it('a `new Set()` has a prototype', function() {
-    const aSet = Set;
-    
+    //// const aSet = Set;
+    const aSet = new Set;
     assert.equal(Reflect.getPrototypeOf(aSet), Set.prototype);
   });
-
   it('for a class, it is `Klass.prototype`', function() {
     class Klass {}
-    const proto = new Klass();
-    
+    //// const proto = new Klass();
+    const proto = Reflect.getPrototypeOf(new Klass());
     assert.equal(proto, Klass.prototype);
   });
-
-  it('for a old-style class, works too', function() {
+  it('works also for an old-style "class"', function() {
     function Klass() {}
-    const proto = Reflect.getPrototypeOf();
-    
+    //// const proto = Reflect.getPrototypeOf();
+    const proto = Reflect.getPrototypeOf(new Klass);
     assert.equal(proto, Klass.prototype);
   });
-
   it('an array has a prototype too', function() {
     let arr = [];
-    const expectedProto = Array;
-    
+    //// const expectedProto = Array;
+    const expectedProto = Array.prototype;
     assert.equal(Reflect.getPrototypeOf(arr), expectedProto);
   });
 
