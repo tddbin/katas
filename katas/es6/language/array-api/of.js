08c7bb4f57b9c0f327e7204f0d37cedc9313c116
wolframkriesing <wolfram+github@kriesing.de>
2018-09-08 13:04:06 +0200

    Kataify `of`.

--- katas/es6/language/array-api/of.js
+++ katas/es6/language/array-api/of.js
@@ -4,19 +4,22 @@
 describe('`Array.of` creates an array with the given arguments as elements', () => {
   
   it('dont mix it up with `Array(10)`, where the argument is the array length', () => {
-    const arr = Array(10);
+    //// const arr = Array(10);
+    const arr = Array.of(10);
     
     assert.deepEqual(arr, [10]);
   });
   
   it('puts all arguments into array elements', () => {
-    const arr = Array.of();
-    
+    //// const arr = Array.of();
+    const arr = Array.of(1, 2);
+
     assert.deepEqual(arr, [1, 2]);
   });
   
   it('takes any kind and number of arguments', () => {
-    const starter = [1, 2];
+    //// const starter = [1, 2];
+    const starter = [[1, 2]];
     const end = [3, '4'];
     const arr = Array.of(...starter, ...end);
     
