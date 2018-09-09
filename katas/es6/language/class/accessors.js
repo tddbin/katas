dd8ffc500091f9989da013c8bfc689316c5b0490
wolframkriesing <wolfram+github@kriesing.de>
2018-09-09 13:39:33 +0200

    Kataify.

--- katas/es6/language/class/accessors.js
+++ katas/es6/language/class/accessors.js
@@ -1,49 +1,44 @@
 // 23: class - accessors
 // To do: make all tests pass, leave the assert lines unchanged!
 
-describe('class accessors (getter and setter)', () => {
-
-  it('only a getter is defined like a method prefixed with `get`', () => {
+describe('Class accessors (getter and setter)', () => {
+  it('a getter is defined like a method prefixed with `get`', () => {
     class MyAccount {
-      get money() { return Infinity; }
+      //// get money() { return Infinity; }
+      get balance() { return Infinity; }
     }
-    
     assert.equal(new MyAccount().balance, Infinity);
   });
-
   it('a setter has the prefix `set`', () => {
     class MyAccount {
       get balance() { return this.amount; }
       set balance(amount) { this.amount = amount; }
     }
-    
     const account = new MyAccount();
-    account.balance = 42;
+    //// account.balance = 42;
+    account.balance = 23;
     assert.equal(account.balance, 23);
   });
   
   describe('dynamic accessors', () => {
-    
-    it('a dynamic getter name is enclosed in [ and ]', function() {
-      const balance = 'yourMoney';
+    it('a dynamic getter name is enclosed in `[]`', function() {
+      //// const balance = 'yourMoney';
+      const getterName = 'yourMoney';
       class YourAccount {
         get [getterName]() { return -Infinity; }
       }
-      
       assert.equal(new YourAccount().yourMoney, -Infinity);
     });
-    
     it('a dynamic setter name as well', function() {
       const propertyName = 'balance';
       class MyAccount {
         get [propertyName]() { return this.amount; }
-        set propertyName(amount) { this.amount = 23; }
+        //// set propertyName(amount) { this.amount = 23; }
+        set [propertyName](amount) { this.amount = 23; }
       }
-      
       const account = new MyAccount();
       account.balance = 42;
       assert.equal(account.balance, 23);
     });
   });
-  
 });
