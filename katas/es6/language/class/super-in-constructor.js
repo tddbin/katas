91e5ee2acadd251f952d5daf2ca4a688477c31c2
wolframkriesing <wolfram+github@kriesing.de>
2018-09-09 22:46:54 +0200

    Kataify and improve descriptions.

--- katas/es6/language/class/super-in-constructor.js
+++ katas/es6/language/class/super-in-constructor.js
@@ -1,53 +1,53 @@
 // 28: class - super in constructor
 // To do: make all tests pass, leave the assert lines unchanged!
 
-describe('class', () => {
-
-  it('if you `extend` a class, use `super()` to call the parent constructor', () => {
+describe('Inside a class`s constructor `super()` can be used', () => {
+  it('`extend` a class and use `super()` to call the parent constructor', () => {
     class A {constructor() { this.levels = 1; }}
-    class B {
+    //// class B {
+    class B extends A {
       constructor() {
-        this.levels++; 
+        ////
+        super();
+        this.levels++;
       }
     }
-    
     assert.equal(new B().levels, 2);
   });
-
   it('`super()` may also take params', () => {
     class A {constructor(startValue=1, addTo=1) { this.counter = startValue + addTo; }}
     class B extends A {
       constructor(...args) { 
-        super();
+        //// super();
+        super(...args);
         this.counter++; 
       }
     }
-    
     assert.equal(new B(42, 2).counter, 45);
   });
-  
   it('it is important where you place your `super()` call!', () => {
     class A {inc() { this.countUp = 1; }}
     class B extends A {
-      inc() { 
+      inc() {
+        //// super.inc();
+
+        this.countUp = 2;
+        ////
         super.inc();
-        this.countUp = 2; 
         return this.countUp;
       }
     }
-    
     assert.equal(new B().inc(), 1);
   });
-
   it('use `super.constructor` to find out if there is a parent constructor', () => {
-    class A extends null {
+    class A {constructor() {"parent"}}
+    class B extends A {
       constructor() {
         super();
-        this.isTop = !!super.constructor;
+        //// this.isTop = '' + super.konstructer;
+        this.isTop = '' + super.constructor;
       }
     }
-
-    assert.equal(new A().isTop, false);
+    assert.equal(new B().isTop, 'class A {constructor() {"parent"}}');
   });
-  
 });
