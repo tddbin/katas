f09e83f1f9a6b51b6c9f8c7aec4fc44c27ba88af
wolframkriesing <wolfram+github@kriesing.de>
2018-09-10 23:30:27 +0200

    Kataify.

--- katas/es6/language/block-scoping/let.js
+++ katas/es6/language/block-scoping/let.js
@@ -2,40 +2,36 @@
 // To do: make all tests pass, leave the asserts unchanged!
 
 describe('`let` restricts the scope of the variable to the current block', () => {
-
   describe('`let` vs. `var`', () => {
-
-    it('`var` works as usual', () => {
+    it('`var` works as usual, it`s scope is the function', () => {
       if (true) {
-        let varX = true;
+        //// let varX = true;
+        var varX = true;
       }
       assert.equal(varX, true);
     });
-    
     it('`let` restricts scope to inside the block', () => {
       if (true) {
-        var letX = true;
+        //// var letX = true;
+        let letX = true;
       }
       assert.throws(() => console.log(letX));
     });
-    
   });
 
   describe('`let` usage', () => {
-    
     it('`let` use in `for` loops', () => {
       let obj = {x: 1};
-      for (var key in obj) {}
+      //// for (var key in obj) {}
+      for (let key in obj) {}
       assert.throws(() => console.log(key));
     });
-    
     it('create artifical scope, using curly braces', () => {
       {
-        var letX = true;
+        //// var letX = true;
+        let letX = true;
       }
       assert.throws(() => console.log(letX));
     });
-    
   });
-  
 });
