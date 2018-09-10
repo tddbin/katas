7231c3eecf9b80e88c2df469cd13093b87693453
wolframkriesing <wolfram+github@kriesing.de>
2018-09-10 22:41:11 +0200

    Kataify.

--- katas/es6/language/arrow-functions/binding.js
+++ katas/es6/language/arrow-functions/binding.js
@@ -2,42 +2,35 @@
 // To do: make all tests pass, leave the asserts unchanged!
 
 class LexicallyBound {
-  
   getFunction() {
     return () => {
-      return new LexicallyBound();
+      //// return new LexicallyBound();
+      return this;
     }
   }
-  
   getArgumentsFunction() {
-    return function() {return arguments}
+    //// return function() {return arguments}
+    return () => {return arguments}
   }
-  
 }
 
-describe('arrow functions have lexical `this`, no dynamic `this`', () => {
- 
-  it('bound at definition time, use `=>` ', function() {
+describe('Arrow functions have lexical `this`, no dynamic `this`', () => {
+  it('bound at definition time, use `=>`', function() {
     var bound = new LexicallyBound();
     var fn = bound.getFunction();
-    
     assert.strictEqual(fn(), bound);
   });
- 
   it('can NOT bind a different context', function() {
     var bound = new LexicallyBound();
     var fn = bound.getFunction();
-    var anotherObj = {};
+    //// var anotherObj = {};
+    var anotherObj = bound;
     var expected = anotherObj;
-    
     assert.strictEqual(fn.call(anotherObj), expected);
   });
-  
-  it('`arguments` doesnt work inside arrow functions', function() {
+  it('`arguments` does NOT work inside arrow functions', function() {
     var bound = new LexicallyBound();
     var fn = bound.getArgumentsFunction();
-    
     assert.equal(fn(1, 2).length, 0);
   });
-  
 });
