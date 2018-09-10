9addd8445ff5bed1b19d93967c7fa11d46a0b295
wolframkriesing <wolfram+github@kriesing.de>
2018-09-10 23:22:13 +0200

    Kataify.

--- katas/es6/language/block-scoping/const.js
+++ katas/es6/language/block-scoping/const.js
@@ -2,43 +2,36 @@
 // To do: make all tests pass, leave the asserts unchanged!
 
 describe('`const` is like `let` plus read-only', () => {
-
   describe('scalar values are read-only', () => {
-
-    it('number', () => {
+    it('e.g. a number', () => {
       const constNum = 0;
-      constNum = 1;
+      //// constNum = 1;
+
       assert.equal(constNum, 0);
     });
-
-    it('string', () => {
+    it('or a string', () => {
       const constString = 'I am a const';
-      constString = 'Cant change you?';
+      //// constString = 'Cant change you?';
+
       assert.equal(constString, 'I am a const');
     });
-
   });
-  
   const notChangeable = 23;
-
   it('const scope leaks too', () => {
     assert.equal(notChangeable, 23);
   });
-  
   describe('complex types are NOT fully read-only', () => {
-
-    it('array', () => {
-      const arr = [42, 23];
-      arr[0] = 0;
+    it('array`s items can be changed', () => {
+      const arr = [];
+      //// arr[0] = 0;
+      arr[0] = 42;
       assert.equal(arr[0], 42);
     });
-    
-    it('object', () => {
+    it('object`s can be modified', () => {
       const obj = {x: 1};
-      obj.x = 2;
+      //// obj.x = 2;
+      obj.x = 3;
       assert.equal(obj.x, 3);
     });
-    
   });
-
 });
