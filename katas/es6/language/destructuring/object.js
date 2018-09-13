9610d843f30324424ee8b3ea4d9ebc4e9b2f898a
wolframkriesing <wolfram+github@kriesing.de>
2018-09-13 21:35:34 +0200

    Kataify.

--- katas/es6/language/destructuring/object.js
+++ katas/es6/language/destructuring/object.js
@@ -1,39 +1,40 @@
 // 12: destructuring - object
 // To do: make all tests pass, leave the assert lines unchanged!
 
-describe('destructuring objects', () => {
-
-  it('is simple', () => {
-    const x = {x: 1};
+describe('Destructure objects', () => {
+  it('by surrounding the left-hand variable with `{}`', () => {
+    //// const x = {x: 1};
+    const {x} = {x: 1};
     assert.equal(x, 1);
   });
-
   describe('nested', () => {
     it('multiple objects', () => {
       const magic = {first: 23, second: 42};
-      const {magic: [second]} = {magic};
+      //// const {magic: [second]} = {magic};
+      const {magic: {second}} = {magic};
       assert.equal(second, 42);
     });
     it('object and array', () => {
-      const {z:[x]} = {z: [23, 42]};
+      //// const {z:[x]} = {z: [23, 42]};
+      const {z:[,x]} = {z: [23, 42]};
       assert.equal(x, 42);
     });
     it('array and object', () => {
-      const [,{lang}] = [null, [{env: 'browser', lang: 'ES6'}]];
+      //// const [,{lang}] = [null, [{env: 'browser', lang: 'ES6'}]];
+      const [,[{lang}]] = [null, [{env: 'browser', lang: 'ES6'}]];
       assert.equal(lang, 'ES6');
     });
   });
-  
   describe('interesting', () => {
     it('missing refs become undefined', () => {
-      const {z} = {x: 1, z: 2};
+      //// const {z} = {x: 1, z: 2};
+      const {z} = {x: 1};
       assert.equal(z, void 0);
     });
-  
     it('destructure from builtins (string)', () => {
-      const {substr} = 1;
+      //// const {substr} = 1;
+      const {substr} = '1';
       assert.equal(substr, String.prototype.substr);
     });
   });
-
 });
--- katas/es6/language/destructuring/string.js
+++ katas/es6/language/destructuring/string.js
@@ -1,22 +1,20 @@
 // 11: destructuring - string
 // To do: make all tests pass, leave the assert lines unchanged!
 
-describe('destructuring also works on strings', () => {
-
-  
-  it('destructure every character', () => {
-    let a, b, c = 'abc';
+describe('Destructuring also works on strings', () => {
+  it('destructure every character, just as if the string was an array', () => {
+    //// let a, b, c = 'abc';
+    let [a, b, c] = 'abc';
     assert.deepEqual([a, b, c], ['a', 'b', 'c']);
   });
-  
   it('missing characters are undefined', () => {
-    const [a, c] = 'ab';
+    //// const [a, c] = 'ab';
+    const [a,, c] = 'ab';
     assert.equal(c, void 0);
   });
-  
   it('unicode character work too', () => {
-    const [space, coffee] = 'a ☕';
+    //// const [space, coffee] = 'a ☕';
+    const [,space, coffee] = 'a ☕';
     assert.equal(coffee, '\u{2615}');
   });
-  
 });
