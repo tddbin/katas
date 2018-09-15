21303ea339b1cfda3107faa291099779c60058e6
wolframkriesing <wolfram+github@kriesing.de>
2018-09-15 13:32:50 +0200

    Kataify.

--- katas/es6/language/generator/send-function.js
+++ katas/es6/language/generator/send-function.js
@@ -1,8 +1,7 @@
 // 56: Generator - Send function to a generator
 // To do: make all tests pass, leave the assert lines unchanged!
 
-describe('pass a function to a generator', () => {
-
+describe('Pass a function to a generator', () => {
   it('the generator can receive a function as a value', function() {
     let fn = function() {};
     function* generatorFunction() {
@@ -10,24 +9,27 @@ describe('pass a function to a generator', () => {
     }
     let iterator = generatorFunction();
     iterator.next();
-    iterator.next();
+    //// iterator.next();
+    iterator.next(fn);
   });
-
   it('pass a function to the iterator, which calls it', function() {
     function* generatorFunction() {
       yield (yield 1)();
     }
     var iterator = generatorFunction();
-    var iteratedOver = [iterator.next().value, iterator.next().value];
+    //// var iteratedOver = [iterator.next().value, iterator.next().value];
+    var iteratedOver = [iterator.next().value, iterator.next(() => 2).value];
     assert.deepEqual([1, 2], iteratedOver);
   });
-
   it('nesting yielded function calls', function() {
     function* generatorFunction() {
       yield (yield (yield 1)());
     }
-    var iteratedOver = [];
+    //// 
+    var iterator = generatorFunction();
+    //// var iteratedOver = [];
+    var iteratedOver = [iterator.next().value, iterator.next(()=>2).value, iterator.next(3).value];
     assert.deepEqual([1, 2, 3], iteratedOver);
   });
-
 });
+
