594a4e9d16ed0e471a83a5eecfc600badef2abc3
wolframkriesing <wolfram+github@kriesing.de>
2018-09-15 18:24:23 +0200

    Kataify.

--- katas/es6/language/generator/send-value.js
+++ katas/es6/language/generator/send-value.js
@@ -1,8 +1,7 @@
 // 52: Generator - Send value to a generator
 // To do: make all tests pass, leave the assert lines unchanged!
 
-describe('pass a value to a generator', () => {
-
+describe('Pass a value to a generator', () => {
   it('basics: get the values from a generator in two ways', function() {
     function* generatorFunction() {
       yield 1;
@@ -12,23 +11,26 @@ describe('pass a value to a generator', () => {
     var convertedToAnArray = Array.from(generatorFunction());
     // way #2
     var iterator = generatorFunction();
-    var iteratedOver = [iterator.next().___, iterator.___];
+    //// var iteratedOver = [iterator.next().___, iterator.___];
+    var iteratedOver = [iterator.next().value, iterator.next().value];
     assert.deepEqual(convertedToAnArray, iteratedOver);
   });
-
   it('pass a value to the iterator', function() {
-    function* generatorFunction() {
+    //// function* generatorFunction() {
+    function* generatorFunction(param) {
       yield 1;
       yield param;
     }
-    var iterator = generatorFunction();
+    //// var iterator = generatorFunction();
+    var iterator = generatorFunction(2);
     var iteratedOver = [iterator.next().value, iterator.next(2).value];
     assert.deepEqual([1, 2], iteratedOver);
   });
-
   it('a value passed to the 1st `next()` call is ignored', function() {
     function* generatorFunction() {
       yield 1;
+      ////
+      yield 2;
     }
     let iterator = generatorFunction();
     const values = [
@@ -37,5 +39,5 @@ describe('pass a value to a generator', () => {
     ];
     assert.deepEqual(values, [1, 2]);
   });
-
 });
+
