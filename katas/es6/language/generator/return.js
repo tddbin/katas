0ea385634bb9abd7e897facb72b0046a76def286
wolframkriesing <wolfram+github@kriesing.de>
2018-09-15 13:08:13 +0200

    Fixed the number of slashes.

--- katas/es6/language/generator/return.js
+++ katas/es6/language/generator/return.js
@@ -17,19 +17,19 @@ describe('`return` in a generator function is special', function() {
       assert.equal(value, 23);
     });
     it('the property `done` is true', function() {
-      /// function* generatorFunction() { yield 42; }
+      //// function* generatorFunction() { yield 42; }
       function* generatorFunction() { return 42; }
       const {done} = generatorFunction().next();
       assert.equal(done, true);
     });
     it('NOTE: `yield` does not return `done=true` but `done=false`!', function() {
-      /// function* generatorFunction() { return 1; }
+      //// function* generatorFunction() { return 1; }
       function* generatorFunction() { yield 1; }
       const returned = generatorFunction().next();
       assert.deepEqual(returned, {value: 1, done: false});
     });
     it('a missing `return` returns `{value: undefined, done: true}`', function() {
-      /// function* generatorFunction() { yield; }	
+      //// function* generatorFunction() { yield; }	
       function* generatorFunction() { }
       const returned = generatorFunction().next();
       assert.deepEqual(returned, {value: void 0, done: true});
@@ -52,7 +52,7 @@ describe('`return` in a generator function is special', function() {
     });
     it('the mix behaves different to two `yield`s', function() {
       const iterator = generatorFunctionWithYieldAndReturn();
-      /// const values = [1, 2];
+      //// const values = [1, 2];
       const values = [1];
       assert.deepEqual(Array.from(iterator), values);
     });
@@ -67,7 +67,7 @@ describe('`return` in a generator function is special', function() {
     });
     it('return a yielded value by "chaining" `return` and `yield`', function() {
       function* generatorFunction() {
-        /// return 1;
+        //// return 1;
         return yield 1;
       }
       const iterator = generatorFunction();
