64d769312aca338b3f4d0caa2969ae2a04adb9f3
wolframkriesing <wolfram+github@kriesing.de>
2018-10-03 22:11:48 +0200

    Map katafied.

--- katas/es6/language/map/get.js
+++ katas/es6/language/map/get.js
@@ -3,45 +3,39 @@
 // Follow the hints of the failure messages!
 
 describe('`Map.prototype.get` returns the element from the map for a key', function(){
-
   it('`get(key)` returns the value stored for this key', function() {
     let map = new Map();
     map.set('key', 'value');
-    
-    const value = map.get;
+    //// const value = map.get;
+    const value = map.get('key');
     assert.equal(value, 'value');
   });
-
   it('multiple calls still return the same value', function() {
     let map = new Map();
     map.set('value', 'value');
-    
-    var value = map.get(map.get(map.get()));
+    //// var value = map.get(map.get(map.get()));
+    var value = map.get(map.get(map.get('value')));
     assert.equal(value, 'value');
   });
-
   it('requires exactly the value as passed to `set()`', function() {
     let map = new Map();
     const obj = {};
-    map.set({}, 'object is key');
-    
+    //// map.set({}, 'object is key');
+    map.set(obj, 'object is key');
     assert.equal(map.get(obj), 'object is key');
   });
-
   it('leave out the key, and you get the value set for the key `undefined` (void 0)', function() {
     let map = new Map();
     map.set(void 0, 'yo');
-    
-    const value = map.get(___);
+    //// const value = map.get(___);
+    const value = map.get();
     assert.equal(value, 'yo');
   });
-
   it('returns undefined for an unknown key', function() {
     let map = new Map();
     map.set(void 0, 1);
-    
-    const value = map.get();
+    //// const value = map.get();
+    const value = map.get('unknown key');
     assert.equal(value, void 0);
   });
-  
 });
--- katas/es6/language/map/has.js
+++ katas/es6/language/map/has.js
@@ -3,39 +3,40 @@
 // Follow the hints of the failure messages!
 
 describe('`map.has()` indicates whether an element with a key exists', function() {
-  
   it('finds nothing in an empty map', function() {
     let map = new Map();
-    const hasKey = map.hazz(void 0);
+    //// const hasKey = map.hazz(void 0);
+    const hasKey = map.has(void 0);
     assert.equal(hasKey, false);
   });
-
   it('finds an element by it`s key', function() {
     let map = new Map([['key', 'VALUE']]);
-    const hasKey = map.has();
+    //// const hasKey = map.has();
+    const hasKey = map.has('key');
     assert.equal(hasKey, true);
   });
-
   it('finds `undefined` as key too', function() {
     let map = new Map([[void 0, 'not defined key']]);
-    const hasUndefinedAsKey = map;
+    //// const hasUndefinedAsKey = map;
+    const hasUndefinedAsKey = map.has();
     assert.equal(hasUndefinedAsKey, true);
   });
-
   it('does not coerce keys', function() {
     let map = new Map([[1, 'one']]);
-    const findsStringOne = true;
+    //// const findsStringOne = true;
+    const findsStringOne = false;
     assert.equal(map.has('1'), findsStringOne);
   });
-
   it('after removal (using `map.delete(<key>)`) it doesnt find the element anymore', function() {
     let map = new Map([[1, 'one']]);
+    ////
+    map.delete(1);
     assert.equal(map.has(1), false);
   });
-
   it('adding an item (using `map.set(key, value)`) later will make `has()` return true', function() {
     let map = new Map();
+    ////
+    map.set(void 0, '');
     assert.equal(map.has(void 0), true);
   });
-  
 });
