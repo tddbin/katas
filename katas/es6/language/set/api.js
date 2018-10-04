0cb322a03efa7db1bf31b1bd48055bbf4900bd97
wolframkriesing <wolfram+github@kriesing.de>
2018-10-04 17:47:07 +0200

    Katafy all set tests.

--- katas/es6/language/set/api.js
+++ katas/es6/language/set/api.js
@@ -3,70 +3,72 @@
 // Follow the hints of the failure messages!
 
 describe('`Set` API overview', function(){
-
   const api = ['size', 'add', 'clear', 'delete', 'entries', 'forEach', 'has', 'keys', 'values'];
   let set;
   beforeEach(function() {
     set = new Set(api);
   });
-  
   it('a Set can be created from an array', function() {
-    let set = new Set([]);
+    //// let set = new Set([]);
+    let set = new Set(api);
     assert.deepEqual(Array.from(set), api);
   });
-
   it('`size` is the number of values', function() {
-    const theSize = set.count;
+    //// const theSize = set.count;
+    const theSize = set.size;
     assert.equal(theSize, api.length);
   });
-
   it('`add()` appends the given value', function() {
-    // hint: to make the example consistent you can add the `Symbol.iterator` to `set`
-    // strongly speaking it is missing in the API.
+    // Hint: To make the content of `api` and `set` consistent you can add the
+    // `Symbol.iterator` to the `set`. Strongly speaking it is missing in the API.
+    ////
+    set.add(Symbol.iterator);
     assert.equal(set.size, api.length + 1);
   });
-
   it('`clear()` removes all elements', function() {
+    ////
+    set.clear();
     assert.equal(set.size, 0);
   });
-
   it('`delete()` removes the given value', function() {
+    ////
+    set.delete('size');
     assert.equal(set.size, api.length - 1);
   });
-  
   it('`entries()` returns an iterator for all values', function() {
     const expectedEntries = api.map(entry => [entry, entry]);
-    const actualEntries = set.entry;
+    //// const actualEntries = set.entry;
+    const actualEntries = set.entries();
     assert.deepEqual([...actualEntries], expectedEntries);
   });
-  
   it('`forEach()` calls a callback for each value', function() {
     let values = [];
-    set.map(value => { values.push(value); });
+    //// set.map(value => { values.push(value); });
+    set.forEach(value => { values.push(value); });
     assert.deepEqual(values, api);
   });
-  
   it('`has()` returns true if the given value is in the set', function() {
     const propertyName = '';
+    ////
+    set.add(propertyName);
     assert.equal(set.has(propertyName), true);
   });
-
   describe('returns an iterator that contains all values', function() {
-    // in order to be alike to `Map` `keys()` and `values()` are essentially the same thing for a `Set`. 
+    // In order to be alike to `Map`, `keys()` and `values()` are essentially the same thing for a `Set`.
     it('`keys()`', function() {
-      const allKeys = Object.keys(set);
+      //// const allKeys = Object.keys(set);
+      const allKeys = set.keys();
       assert.deepEqual([...allKeys], api);
     });
-    
     it('`values()`', function() {
-      const allValues = set.value();
+      //// const allValues = set.value();
+      const allValues = set.values();
       assert.deepEqual([...allValues], api);
     });
-    
     it('`[Symbol.iterator]()`', function() {
-      const iteratorKey = '???';
+      //// const iteratorKey = '???';
+      const iteratorKey = Symbol.iterator;
       assert.deepEqual([...set[iteratorKey]()], api);
     });
   });
-  
 });
