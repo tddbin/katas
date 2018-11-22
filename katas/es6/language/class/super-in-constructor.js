abed5b22fb1a8a60d4f9fedd682f381282d7594f
Jizu Sun <jizu.sun@sap.com>
2018-11-22 14:38:46 +0800

    es5 compatible

--- katas/es6/language/class/super-in-constructor.js
+++ katas/es6/language/class/super-in-constructor.js
@@ -41,14 +41,14 @@ describe('Inside a class`s constructor `super()` can be used', () => {
     assert.equal(new B().inc(), 1);
   });
   it('use `super.constructor` to find out if there is a parent constructor', () => {
-    class A {constructor() {"parent"}}
-    class B extends A {
+    class ParentClassA {constructor() {"parent"}}
+    class B extends ParentClassA {
       constructor() {
         super();
         //// this.isTop = '' + super.konstructer;
         this.isTop = '' + super.constructor;
       }
     }
-    assert(new B().isTop.startsWith('class A'));
+    assert(new B().isTop.includes('ParentClassA'), new B().isTop);
   });
 });
