b10b6d69eb14579605c5863c54b03dd3341f075a
wolframkriesing <wolfram+github@kriesing.de>
2018-12-29 18:39:26 +0100

    Ensure the katafied code is valid JS.

--- katas/es6/language/template-strings/multiline.js
+++ katas/es6/language/template-strings/multiline.js
@@ -4,16 +4,17 @@
 
 describe('Template string, can contain multiline content', function() {
   it('wrap it in backticks (`) and add a newline, to span across two lines', function() {
-    //// var normalString = `line1 line2`;
+    //// var normalString = `line1 //// line3`;
     var normalString = `line1
-line2`;
-    assert.equal(normalString, 'line1\nline2');
+////
+line3`;
+    assert.equal(normalString, 'line1\n////\nline3');
   });
   it('even over more than two lines', function() {
     //// var multiline = ``;
     var multiline = `
 	    
-	    
+	  ////  
 	    `;
     assert.equal(multiline.split('\n').length, 4);
   });
@@ -22,15 +23,16 @@ line2`;
     it('like simple variables', function() {
       //// var multiline = `line 1 $ {x}`;
       var multiline = `line 1
+////
       ${x}`;
-      assert.equal(multiline, 'line 1\n      42');
+      assert.equal(multiline, 'line 1\n////\n      42');
     });
     it('also here spaces matter', function() {
       //// var multiline = ``;
       var multiline = `
+////
 ${x}`;
-      assert.equal(multiline, '\n42');
+      assert.equal(multiline, '\n////\n42');
     });
   });
 });
-
