253ce8d54188feafb5aa67ef76df4f16f16cf8f5
wolframkriesing <wolfram+github@kriesing.de>
2018-12-30 18:31:41 +0100

    Adhere to ADR#1.

--- katas/es6/language/template-strings/tagged.js
+++ katas/es6/language/template-strings/tagged.js
@@ -3,7 +3,6 @@
 // Follow the hints of the failure messages!
 
 describe('tagged template strings, are an advanced form of template strings', function() {
-  
   it('syntax: prefix the template string with a function to call (without "()" around it)', function() {
     function tagFunction(s) {
       return s.toString();
@@ -11,29 +10,21 @@ describe('tagged template strings, are an advanced form of template strings', fu
     var evaluated = tagFunc `template string`;
     assert.equal(evaluated, 'template string');
   });
-  
   describe('the function can access each part of the template', function() {
-
     describe('the 1st parameter - receives only the pure strings of the template string', function() {
-
       function tagFunction(strings) {
         return strings;
       }
-
       it('the strings are an array', function() {
         var result = 'template string';
         assert.deepEqual(tagFunction`template string`, result);
       });
-
       it('expressions are NOT passed to it', function() {
         var tagged = tagFunction`one${23}`; 
         assert.deepEqual(tagged, ['one', 'two']);
       });
-
     });
-
     describe('the 2nd and following parameters - contain the values of the processed substitution', function() {
-
       var one = 1;
       var two = 2;
       var three = 3;
@@ -43,22 +34,18 @@ describe('tagged template strings, are an advanced form of template strings', fu
         }
         assert.equal(firstValueOnly`uno ${one}, dos ${two}`, 1);
       });
-      
       it('the 3rd parameter contains the second expression`s value', function() {
         function firstValueOnly(strings, firstValue, ____) { 
           return secondValue;
         }
         assert.equal(firstValueOnly`uno ${one}, dos ${two}`, 2);
       });
-      
       it('using ES6 rest syntax, all values can be accessed via one variable', function() {
         function valuesOnly(stringsArray, ...allValues) { // using the new ES6 rest syntax
           return;
         }
         assert.deepEqual(valuesOnly`uno=${one}, dos=${two}, tres=${three}`, [1, 2, 3]);
       });
-      
-    });     
+    });
   });
-
 });
