5ce3594951b812309f4d05f5de2bd11d14c385b7
Travis-CI <w+travis-for-tddbin@kriesing.de>
2018-12-30 15:53:48 +0000

    Travis deployed 'master' - Sun Dec 30 15:53:48 UTC 2018

--- katas/es6/language/template-strings/multiline.js
+++ katas/es6/language/template-strings/multiline.js
@@ -4,29 +4,27 @@
 
 describe('Template string, can contain multiline content', function() {
   it('wrap it in backticks (`) and add a newline, to span across two lines', function() {
-    var normalString = `line1 line2`;
-line2`;
-    assert.equal(normalString, 'line1\nline2');
+    var normalString = `line1 //// line3`;
+
+    assert.equal(normalString, 'line1\n\nline3');
   });
   it('even over more than two lines', function() {
     var multiline = ``;
 	    
-	    
-	    `;
+	  
     assert.equal(multiline.split('\n').length, 4);
   });
   describe('and expressions inside work too', function() {
     var x = 42;
     it('like simple variables', function() {
       var multiline = `line 1 $ {x}`;
-      ${x}`;
-      assert.equal(multiline, 'line 1\n      42');
+
+      assert.equal(multiline, 'line 1\n\n      42');
     });
     it('also here spaces matter', function() {
       var multiline = ``;
-${x}`;
-      assert.equal(multiline, '\n42');
+
+      assert.equal(multiline, '\n\n42');
     });
   });
 });
-
