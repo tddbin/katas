4479b37778b295564d50df1e9661c4ce321b651d
Travis-CI <w+travis-for-tddbin@kriesing.de>
2018-12-30 16:03:41 +0000

    Travis deployed 'master' - Sun Dec 30 16:03:41 UTC 2018

--- katas/es6/language/spread/with-arrays.js
+++ katas/es6/language/spread/with-arrays.js
@@ -2,26 +2,30 @@
 // To do: make all tests pass, leave the assert lines unchanged!
 // Follow the hints of the failure messages!
 
-describe('spread with arrays', () => {
-
-  it('extracts each array item', function() {
-    const [b, a] = [...[1, 2]];
-    assert.equal(a, 1);
-    assert.equal(b, 2);
+describe('Spread syntax with arrays', () => {
+  describe('basically', () => {
+    it('expands the items of an array by prefixing it with `...`', function() {
+      const middle = [1, 2, 3];
+      const arr = [0, middle, 4];
+      assert.deepEqual(arr, [0, 1, 2, 3, 4]);
+    });
+    it('an empty array expanded is no item', function() {
+      const arr = [0, [], 1];
+      assert.deepEqual(arr, [0, 1]);
+    });
   });
-
-  it('in combination with rest', function() {
-    const [a, b, ...rest] = [...[0, 1, 2, 3, 4, 5]];
-    assert.equal(a, 1);
-    assert.equal(b, 2);
-    assert.deepEqual(rest, [3, 4, 5]);
+  describe('is (in a way) the opposite to the rest syntax', function() {
+    it('both use `...` to either expand all items and collect them', function() {
+      const [...rest] = [...[,1, 2, 3, 4, 5]];
+      assert.deepEqual(rest, [1, 2, 3, 4, 5]);
+    });
+    it('rest syntax must be last in an array, spread can be used in any place', function() {
+      const [a, b, ...rest] = [1, [2, 3], 4, 5];
+      assert.equal(a, 1);
+      assert.equal(b, 2);
+      assert.deepEqual(rest, [3, 4, 5]);
+    });
   });
-
-  it('spreading into the rest', function() {
-    const [...rest] = [...[,1, 2, 3, 4, 5]];
-    assert.deepEqual(rest, [1, 2, 3, 4, 5]);
-  });
-
   describe('used as function parameter', () => {
     it('prefix with `...` to spread as function params', function() {
       const magicNumbers = [1, 2];
@@ -31,10 +35,18 @@ describe('spread with arrays', () => {
       };
       fn(magicNumbers);
     });
-  
     it('pass an array of numbers to Math.max()', function() {
       const max = Math.max(...[23, 0, 42, 43]);
       assert.equal(max, 42);
     });
   });  
+  describe('used as constructor parameter', () => {
+    it('just like in a function call (is not possible using `apply`)', () => {
+      class X {
+        constructor(a, b, c) { return [a, b, c]; }
+      }
+      const args = [1];
+      assert.deepEqual(new X(...args), [1, 2, 3]);
+    });
+  });
 });
--- katas/es6/language/spread/with-strings.js
+++ katas/es6/language/spread/with-strings.js
@@ -2,34 +2,26 @@
 // To do: make all tests pass, leave the assert lines unchanged!
 // Follow the hints of the failure messages!
 
-describe('spread with strings', () => {
-
-  it('simply spread each char of a string', function() {
+describe('Spread syntax with strings', () => {
+  it('expands each character of a string by prefixing it with `...`', function() {
     const [b, a] = [...'ab'];
     assert.equal(a, 'a');
     assert.equal(b, 'b');
   });
-
-  it('extracts each array item', function() {
-    const [a,,c] = ['a', ...'12'];
-    assert.equal(a, 1);
-    assert.equal(b, 2);
+  it('expands any kind of character', function() {
+    const arr = [...'12'];
+    assert.deepEqual(arr, ['1', ' ', '☢', ' ', '2']);
   });
-  
   it('works anywhere inside an array (must not be last)', function() {
     const letters = ['a', 'bcd', 'e', 'f'];
     assert.equal(letters.length, 6);
   });
-  
-  it('dont confuse with the rest operator', function() {
+  it('don`t confuse with the rest operator', function() {
     const [...rest] = ['1234', ...'5'];
     assert.deepEqual(rest, [1, 2, 3, 4, 5]);
   });
-  
-  it('passed as function parameter', function() {
-    const max = Math.max(12345);
+  it('can also be used as function parameter', function() {
+    const max = Math.max('12345');
     assert.deepEqual(max, 5);
   });
-  
 });
-
