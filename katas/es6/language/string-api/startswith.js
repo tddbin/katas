65adacd3fcb7ba2234440a27288b2f3d31de7594
wolframkriesing <wolfram+github@kriesing.de>
2018-12-30 18:19:48 +0100

    Katafy String#startsWith.

--- katas/es6/language/string-api/startswith.js
+++ katas/es6/language/string-api/startswith.js
@@ -4,55 +4,65 @@
 
 describe('`str.startsWith(searchString)` determines whether `str` begins with `searchString`.', function() {
   const s = 'the string s';
-  describe('1st parameter, the string to search for', function() {
-    it('works with just a character', function() {
-      const actual = s.starts_with('t');
+  describe('the 1st parameter, the string to search for', function() {
+    it('can be just a character', function() {
+      //// const actual = s.starts_with('t');
+      const actual = s.startsWith('t');
       assert.equal(actual, true);
     });
-    it('works with a string', function() {
-      const expected = '???';
+    it('can be a string', function() {
+      //// const expected = '???';
+      const expected = true;
       assert.equal(s.startsWith('the'), expected);
     });
-    it('works with unicode characters', function() {
-      const nuclear = 'NO ☢ NO';
+    it('can contain unicode characters', function() {
+      //// const nuclear = 'NO ☢ NO';
+      const nuclear = '☢ NO';
       assert.equal(nuclear.startsWith('☢'), true);
     });
     it('a regular expression throws a TypeError', function() {
-      const aRegExp = 'the';
+      //// const aRegExp = 'the';
+      const aRegExp = /the/;
       assert.throws(() => {''.startsWith(aRegExp)}, TypeError);
     });
   });
-  describe('2nd parameter, the position where to start searching from', function() {
-    it('find "str" at position 4', function() {
-      const position = 3;
+  describe('the 2nd parameter, the position where to start searching from', function() {
+    it('e.g. find "str" at position 4', function() {
+      //// const position = 3;
+      const position = 4;
       assert.equal(s.startsWith('str', position), true);
     });
-    it('`undefined` is the same as 0', function() {
-      const _undefined_ = '1';
+    it('for `undefined` is the same as 0', function() {
+      //// const _undefined_ = '1';
+      const _undefined_ = undefined;
       assert.equal(s.startsWith('the', _undefined_), true);
     });
-    it('the parameter gets coerced to an int', function() {
-      const position = 'four';
+    it('the parameter gets converted to an int', function() {
+      //// const position = 'four';
+      const position = '4';
       assert.equal(s.startsWith('str', position), true);
     });
     it('a value larger than the string`s length, returns false', function() {
-      const expected = true;
+      //// const expected = true;
+      const expected = false;
       assert.equal(s.startsWith(' ', s.length + 1), expected);
     });
   });
-  describe('transfer the functionality to other objects', function() {
-    const startsWith = (...args) => String.prototype.startsWith.call(...args);
+  describe('this functionality can be used on non-strings too', function() {
     it('e.g. a boolean', function() {
-      let aBool;
-      assert.equal(startsWith(!aBool, 'false'), true);
+      //// let aBool;
+      let aBool = false;
+      assert.equal(String.prototype.startsWith.call(aBool, 'false'), true);
     });
     it('e.g. a number', function() {
-      let aNumber = 19;
-      assert.equal(startsWith(aNumber + 84, '1984'), true);
+      //// let aNumber = 19;
+      let aNumber = 1900;
+      assert.equal(String.prototype.startsWith.call(aNumber + 84, '1984'), true);
     });
     it('also using the position works', function() {
-      const position = 0;
-      assert.equal(startsWith(1994, '99', position), true);
+      //// const position = 0;
+      const position = 1;
+      assert.equal(String.prototype.startsWith.call(1994, '99', position), true);
     });
   });
 });
