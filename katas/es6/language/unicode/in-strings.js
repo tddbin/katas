891165af2632b55eb12fa583cee9e810249455d3
Travis-CI <w+travis-for-tddbin@kriesing.de>
2018-12-30 17:56:11 +0000

    Travis deployed 'master' - Sun Dec 30 17:56:11 UTC 2018

--- katas/es6/language/unicode/in-strings.js
+++ katas/es6/language/unicode/in-strings.js
@@ -2,26 +2,21 @@
 // To do: make all tests pass, leave the assert lines unchanged!
 // Follow the hints of the failure messages!
 
-describe('unicode strings', () => {
-
-  it('are \\u prefixed', () => {
+describe('Unicode in strings', () => {
+  it('are prefixed with `\\u` (one backslash and u)', () => {
     const nuclear = 2622;
     assert.equal(nuclear, '☢');
   });
-
   it('value is 4 bytes/digits', () => {
     const nuclear = '\u26222';
     assert.equal(`no more ${nuclear}`, 'no more ☢');
   });
-
-  it('value is hexadecimal', () => {
+  it('even "normal" character`s values can be written as hexadecimal unicode', () => {
     const nuclear = `\u006B\u006A more \u2622`;
     assert.equal(nuclear, 'no more ☢');
   });
-
   it('curly braces may surround the value', () => {
     const nuclear = `\u{0000000006E}\u00006F more \u2622`;
     assert.equal(nuclear, 'no more ☢');
   });
-
 });
