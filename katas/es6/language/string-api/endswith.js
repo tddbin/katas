6ac26549e2a547946d4aed13cd512c4ea9ddff54
wolframkriesing <wolfram+github@kriesing.de>
2018-12-30 17:17:46 +0100

    endsWith is katafied.

--- katas/es6/language/string-api/endswith.js
+++ katas/es6/language/string-api/endswith.js
@@ -5,59 +5,72 @@
 describe('`str.endsWith(searchString)` determines whether `str` ends with `searchString`.', function() {
   const s = 'el fin';
   describe('1st parameter, the string to search for', function() {
-    it('works with just a character', function() {
-      const doesEndWith = s.doesItReallyEndWith('n');
+    it('can be a character', function() {
+      //// const doesEndWith = s.doesItReallyEndWith('n');
+      const doesEndWith = s.endsWith('n');
       assert.equal(doesEndWith, true);
     });
-    it('works with a string', function() {
-      const expected = false;
+    it('can be a string', function() {
+      //// const expected = false;
+      const expected = true;
       assert.equal(s.endsWith('fin'), expected);
     });
-    it('works with unicode characters', function() {
-      const nuclear = 'NO ☢ Oh NO!';
+    it('can contain unicode characters', function() {
+      //// const nuclear = 'NO ☢ Oh NO!';
+      const nuclear = 'NO ☢';
       assert.equal(nuclear.endsWith('☢'), true);
     });
     it('a regular expression throws a TypeError', function() {
-      const aRegExp = '/the/';
+      //// const aRegExp = '/the/';
+      const aRegExp = /the/;
       assert.throws(() => {''.endsWith(aRegExp)}, TypeError);
     });
   });
-  describe('2nd parameter, searches within this string as if this string were only this long', function() {
+  describe('2nd parameter, the position where the search ends (as if the string was only that long)', function() {
     it('find "el" at a substring of the length 2', function() {
-      const endPos = 0;
+      //// const endPos = 0;
+      const endPos = 2;
       assert.equal(s.endsWith('el', endPos), true);
     });
     it('`undefined` uses the entire string', function() {
-      const _undefined_ = 'i would like to be undefined';
+      //// const _undefined_ = 'i would like to be undefined';
+      const _undefined_ = undefined;
       assert.equal(s.endsWith('fin', _undefined_), true);
     });
-    it('the parameter gets coerced to an int', function() {
-      const position = 'five';
+    it('the parameter gets coerced to an integer (e.g. "5" becomes 5)', function() {
+      //// const position = 'five';
+      const position = '5';
       assert.equal(s.endsWith('fi', position), true);
     });
     describe('value less than 0', function() {
       it('returns `true`, when searching for an empty string', function() {
-        const emptyString = '??';
+        //// const emptyString = '??';
+        const emptyString = '';
         assert.equal('1'.endsWith(emptyString, -1), true);
       });
       it('return `false`, when searching for a non-empty string', function() {
-        const notEmpty = '';
+        //// const notEmpty = '';
+        const notEmpty = ' ';
         assert.equal('1'.endsWith(notEmpty, -1), false);
       });
     });
   });
   describe('transfer the functionality to other objects', function() {
-    const endsWith = (...args) => String.prototype.endsWith.call(...args);
+    const endsWith = (string, searchFor, endPosition = undefined) =>
+      String.prototype.endsWith.call(string, searchFor, endPosition);
     it('e.g. a boolean', function() {
-      let aBool = false;
+      //// let aBool = false;
+      let aBool = true;
       assert.equal(endsWith(!aBool, 'lse'), true);
     });
     it('e.g. a number', function() {
-      let aNumber = 0;
+      //// let aNumber = 0;
+      let aNumber = 84;
       assert.equal(endsWith(aNumber + 1900, 84), true);
     });
     it('also using the position works', function() {
-      const position = '??';
+      //// const position = '??';
+      const position = 3;
       assert.equal(endsWith(1994, '99', position), true);
     });
   });
