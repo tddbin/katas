4b6d859da94eecd997953b3f028df5406f395e6e
wkriesing <wolfram.kriesing@holidaycheck.com>
2019-06-15 16:06:28 +0200

    The corner cases for return values true+false.

--- katas/es6/language/number-api/isnan.js
+++ katas/es6/language/number-api/isnan.js
@@ -8,4 +8,39 @@ describe('`Number.isNaN()` determines if a value is `NaN`', function(){
     const whatType = 'function';
     assert.equal(typeof Number.isNaN, whatType);
   });
+  describe('returns false', () => {
+    describe('for any not-Number type', () => {
+      it('like null', () => {
+        assert.equal(Number.isNaN(null), false);
+      });
+      it('like a string', () => {
+        assert.equal(Number.isNaN('a string'), false);
+      });
+      it('like an object', () => {
+        assert.equal(Number.isNaN({}), false);
+      });
+      it('as opposed to the global function `isNaN`', () => {
+        //// const fn = Number.isNaN;
+        const fn = isNaN;
+        assert.equal(fn({}), true);
+      });
+    });
+    describe('for Numbers', () => {
+      it('like 0', () => {
+        assert.equal(Number.isNaN(NaN), true);
+      });
+    });
+  });
+  describe('returns true for', () => {
+    it('exactly `NaN`', () => {
+      //// const notANumber = 1;
+      const notANumber = NaN;
+      assert.equal(Number.isNaN(notANumber), true);
+    });
+    it('zero divided by zero', () => {
+      //// const zeroDividedByZero = 1 / 2;
+      const zeroDividedByZero = 0 / 0;
+      assert.equal(Number.isNaN(zeroDividedByZero), true);
+    });
+  });
 });
