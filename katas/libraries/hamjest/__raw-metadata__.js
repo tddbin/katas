0322bd095fc606aa17105cd4deadbea7ff7f4d5f
wolframkriesing <wolfram+github@kriesing.de>
2019-06-17 00:28:17 +0200

    Add some more katas, that are already there.

--- katas/libraries/hamjest/__raw-metadata__.js
+++ katas/libraries/hamjest/__raw-metadata__.js
@@ -1,9 +1,13 @@
 import {SKILL_LEVEL} from '../../skill-levels.js';
+import {toUtcDate}  from '../../date';
 import * as date from '../../date';
 
 const ASSERRT_THAT = 1;
 const MATCHER_EQUAL_TO = 2;
 const MATCHER_STRICTLY_EQUAL_TO = 3;
+const MATCHER_ANYTHING = 4;
+const MATCHER_DEFINED = 5;
+const MATCHER_EQUALTO = 6;
 
 export const all = {
   name: 'Hamjest assertion library',
@@ -16,7 +20,7 @@ export const all = {
           path: 'assertThat',
           level: SKILL_LEVEL.BEGINNER,
           requiresKnowledgeFrom: [],
-          publishDateUTC: date.toUtcDate(2017, date.AUGUST, 21, 13, 0),
+          publishDateUTC: toUtcDate(2017, date.AUGUST, 21, 13, 0),
           links: [],
         },
         [MATCHER_EQUAL_TO]: {
@@ -25,7 +29,7 @@ export const all = {
           path: 'matchers/equalTo',
           level: SKILL_LEVEL.BEGINNER,
           requiresKnowledgeFrom: [],
-          publishDateUTC: date.toUtcDate(2017, date.AUGUST, 21, 15, 54),
+          publishDateUTC: toUtcDate(2017, date.AUGUST, 21, 15, 54),
           links: [],
         },
         [MATCHER_STRICTLY_EQUAL_TO]: {
@@ -34,7 +38,34 @@ export const all = {
           path: 'matchers/strictlyEqualTo',
           level: SKILL_LEVEL.BEGINNER,
           requiresKnowledgeFrom: [],
-          publishDateUTC: date.toUtcDate(2017, date.AUGUST, 21, 16, 30),
+          publishDateUTC: toUtcDate(2017, date.AUGUST, 21, 16, 30),
+          links: [],
+        },
+        [MATCHER_ANYTHING]: {
+          name: '`anything()`',
+          description: 'The matcher `anything()`, matches any value and never fails.',
+          path: 'matchers/anything',
+          level: SKILL_LEVEL.BEGINNER,
+          requiresKnowledgeFrom: [],
+          publishDateUTC: toUtcDate(2017, date.AUGUST, 22, 16, 30),
+          links: [],
+        },
+        [MATCHER_DEFINED]: {
+          name: '`defined()`',
+          description: 'The matcher `defined()`, matches everything but `undefined`.',
+          path: 'matchers/defined',
+          level: SKILL_LEVEL.BEGINNER,
+          requiresKnowledgeFrom: [],
+          publishDateUTC: toUtcDate(2017, date.AUGUST, 22, 16, 30),
+          links: [],
+        },
+        [MATCHER_EQUALTO]: {
+          name: '`equalTo()`',
+          description: 'The matcher `equalTo()`, matches equality.',
+          path: 'matchers/equalTo',
+          level: SKILL_LEVEL.BEGINNER,
+          requiresKnowledgeFrom: [],
+          publishDateUTC: toUtcDate(2017, date.AUGUST, 22, 16, 30),
           links: [],
         },
       }
