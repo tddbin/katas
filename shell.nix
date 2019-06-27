615d3e7899d6ccc2c811aeaedbabdd12307758e6
wolframkriesing <wolfram+github@kriesing.de>
2019-06-27 09:24:58 +0200

    Use node 12, which is needed for Object.fromEntries.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
-with import (fetchTarball https://github.com/nixos/nixpkgs/tarball/f52505fac8c82716872a616c501ad9eff188f97f) { };
+with import (fetchTarball https://github.com/nixos/nixpkgs/tarball/525f0b043652cb62192874054d7aca5843d45e8f) { };
 
 stdenv.mkDerivation {
     name = "dev-shell";
     src = null;
-    buildInputs = [ nodejs-11_x ];
+    buildInputs = [ nodejs-12_x ];
 }
