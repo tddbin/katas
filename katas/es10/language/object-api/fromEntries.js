34e54225e4374ebcba3dfc59dd88b3a7f18d1e3c
wolframkriesing <wolfram+github@kriesing.de>
2019-07-13 21:47:33 +0200

    Even more material for the kata.

--- katas/es10/language/object-api/fromEntries.js
+++ katas/es10/language/object-api/fromEntries.js
@@ -21,8 +21,11 @@ describe('`Object.fromEntries()` converts key-value pairs into an object', () =>
     it('a Symbol throws a TypeError', () => {
       assert.throws(() => { Object.fromEntries(Symbol.for('some')); }, TypeError);
     });
-    it('a string does not throw', () => {
+    it('an empty string does NOT throw', () => {
       assert.doesNotThrow(() => { Object.fromEntries(''); });
+    });
+    it('an non-empty string does throw', () => {
+      assert.throws(() => { Object.fromEntries(' '); });
     });
   });
 
@@ -46,6 +49,33 @@ describe('`Object.fromEntries()` converts key-value pairs into an object', () =>
     it('array of arrays #3', () => {
       assert.doesNotThrow(() => { Object.fromEntries([[], []]); });
       assert.deepEqual(Object.fromEntries([[1,2], [3,4]]), {1:2, 3:4});
+    });
+  });
+
+  describe('use cases', () => {
+    it('from JSON to object', () => {
+      // the
+    });
+    it('mapping data', () => {
+      const people = [{name: 'Alex', age: 21}, {name: 'Anna', age: 21}];
+      const peoplesAge = Object.fromEntries(people.map(({name, age}) => [name, age]));
+      assert.deepEqual({Alex: 21, Anna:21}, peoplesAge);
+    });
+    it('toString-ables can be keys', () => {
+      const s = new class { toString() { return 'key'; }  };
+      assert.deepEqual(Object.fromEntries([[s, 42]]), {key: 42});
+    });
+  });
+  describe('relates to `Object.entries()`', () => {
+    it('is almost the reverse of Object.entries()', () => {
+
+    });
+    it('allows Symbols as keys, while `Object.entries()` does not report them', () => {
+      const sym = Symbol();
+      const fromEntries = Object.fromEntries([[sym, 42]]);
+      assert.deepEqual(fromEntries, {[sym]: 42});
+      // while
+      assert.deepEqual(Object.entries(fromEntries), []);
     });
   });
 });
