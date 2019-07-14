8e155d99cc2eca9daaf7d3ed89a6fcda582a0115
wolframkriesing <wolfram+github@kriesing.de>
2019-07-15 00:03:38 +0200

    The start of this ...

---
+++ katas/es7/language/array-api/includes.js
@@ -0,0 +1,11 @@
+// ES7 - 1: Array - includes
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('`Object.fromEntries()` converts key-value pairs into an object', () => {
+  it('it is a static function defined on `Object`', () => {
+    //// const fn = Object.entries;
+    const fn = Object.fromEntries;
+    assert(Object.is(Object.fromEntries, fn));
+  });
+});
