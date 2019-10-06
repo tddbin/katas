51b8452e61e24035c9f389b8be5549218a993893
wolframkriesing <wolfram+github@kriesing.de>
2019-10-06 19:53:21 +0200

    Katafy.

--- katas/es1/language/global-api/parseInt.js
+++ katas/es1/language/global-api/parseInt.js
@@ -4,48 +4,80 @@
 
 describe('`parseInt()` parses a string and returns an integer.', () => {
   it('it is a global function', () => {
-    //// const whatType = 'global function';
-    const whatType = 'function';
+    //// var whatType = 'global function';
+    var whatType = 'function';
     assert.equal(typeof parseInt, whatType);
   });
   describe('WHEN given a numeric string', () => {
     it('THEN converts it to an integer', () => {
-      assert.equal(parseInt('1'), 1);
+      //// var numericString = '';
+      var numericString = '1';
+      assert.equal(parseInt(numericString), 1);
     });
     it('AND a radix=16 THEN converts to an integer applying the radix (like hexadecimal)', () => {
-      assert.equal(parseInt('10', 16), 16);
+      //// var radix = 6;
+      var radix = 16;
+      assert.equal(parseInt('10', radix), 16);
     });
     it('AND a radix=9 THEN converts to an integer applying the radix', () => {
-      assert.equal(parseInt('10', 9), 9);
+      //// var expected = 10;
+      var expected = 9;
+      assert.equal(parseInt('10', 9), expected);
     });
     it('AND a radix=0 THEN converts to an integer assuming radix=10', () => {
-      assert.equal(parseInt('10', 0), 10);
+      //// var radix = 8;
+      var radix = 0;
+      assert.equal(parseInt('10', radix), 10);
     });
     it('AND the string starts with `0x` THEN converts to an integer assuming radix=16', () => {
-      assert.equal(parseInt('0x10'), 16);
+      //// var parsedToInt = '10';
+      var parsedToInt = '0x10';
+      assert.equal(parseInt(parsedToInt), 16);
     });
     it('AND the string starts with `0X` THEN converts to an integer assuming radix=16', () => {
-      assert.equal(parseInt('0X10'), 16);
+      //// var hex = '1X10';
+      var hex = '0X10';
+      assert.equal(parseInt(hex), 16);
     });
   });
   describe('WHEN given a NOT-numeric string (it will be tried to be converted to a string)', () => {
     it('a string starting with numbers THEN converts containing the leading numbers only', () => {
-      assert.equal(parseInt(' 12AB34'), 12)
+      //// var s = ' AB34';
+      var s = ' 12AB34';
+      assert.equal(parseInt(s), 12)
     });
     it('a string starting with `-F` (a hexadecimal number) AND radix=16 THEN returns -15', () => {
-      assert.equal(parseInt('-F', 16), -15)
+      //// var hex = '-';
+      var hex = '-F';
+      assert.equal(parseInt(hex, 16), -15)
     });
     it('a string, a word made of letters only THEN returns `NaN` (not a number)', () => {
-      assert(isNaN(parseInt('word')));
+      //// var word = '1 word';
+      var word = 'word';
+      assert.equalToNaN(parseInt(word));
     });
     it('an empty object literal THEN returns `NaN`', () => {
-      assert(isNaN(parseInt({})));
+      //// var emptyObject = 1+{};
+      var emptyObject = {};
+      assert.equalToNaN(parseInt(emptyObject));
     });
     it('an empty array THEN returns `NaN`', () => {
-      assert(isNaN(parseInt([])));
+      //// var emptyArray = [9];
+      var emptyArray = [];
+      assert.equalToNaN(parseInt(emptyArray));
     });
     it('an array with `[123,456]` THEN converts it to a string and then to an integer', () => {
-      assert.equal(parseInt([123,456]), 123);
+      //// var filledArray = [123456];
+      var filledArray = [123,456];
+      assert.equal(parseInt(filledArray), 123);
     });
   });
 });
+
+assert.equalToNaN = actual => {
+  assert(isNaN(actual), new assert.AssertionError({
+    message: 'Expected `parseInt() to return `NaN`.',
+    actual,
+    expected: NaN,
+  }));
+};
