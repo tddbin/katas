62d696be771ddcbccf457ddb14364182e6c69dfc
wolframkriesing <wolfram+github@kriesing.de>
2019-10-06 19:18:21 +0200

    Move out one kata, into the in-depth one. And add a couple non-numeric-string cases.

--- katas/es1/language/global-api/parseInt.js
+++ katas/es1/language/global-api/parseInt.js
@@ -8,23 +8,44 @@ describe('`parseInt()` parses a string and returns an integer.', () => {
     const whatType = 'function';
     assert.equal(typeof parseInt, whatType);
   });
-  it('WHEN given a numeric string THEN converts it to an integer', () => {
-    assert.equal(parseInt('1'), 1);
+  describe('WHEN given a numeric string', () => {
+    it('THEN converts it to an integer', () => {
+      assert.equal(parseInt('1'), 1);
+    });
+    it('AND a radix=16 THEN converts to an integer applying the radix (like hexadecimal)', () => {
+      assert.equal(parseInt('10', 16), 16);
+    });
+    it('AND a radix=9 THEN converts to an integer applying the radix', () => {
+      assert.equal(parseInt('10', 9), 9);
+    });
+    it('AND a radix=0 THEN converts to an integer assuming radix=10', () => {
+      assert.equal(parseInt('10', 0), 10);
+    });
+    it('AND the string starts with `0x` THEN converts to an integer assuming radix=16', () => {
+      assert.equal(parseInt('0x10'), 16);
+    });
+    it('AND the string starts with `0X` THEN converts to an integer assuming radix=16', () => {
+      assert.equal(parseInt('0X10'), 16);
+    });
   });
-  it('WHEN given a numeric string AND a radix THEN converts to an integer applying the radix', () => {
-    assert.equal(parseInt('10', 16), 16);
-  });
-  it('WHEN given a numeric string AND a radix=0 THEN converts to an integer assuming radix=10', () => {
-    assert.equal(parseInt('10', 0), 10);
-  });
-  it('WHEN given a numeric string starting with `0x` THEN converts to an integer assuming radix=16', () => {
-    assert.equal(parseInt('0x10'), 16);
-  });
-  it('WHEN given a numeric string starting with `0X` THEN converts to an integer assuming radix=16', () => {
-    assert.equal(parseInt('0X10'), 16);
-  });
-
-  it('ignores any surrounding white spaces for a given string', () => {
-    assert.equal(parseInt('     123   '), 123);
+  describe('WHEN given a NOT-numeric string, it will be converted to a string first', () => {
+    it('a string starting with numbers', () => {
+      assert.equal(parseInt(' 12AB34'), 12)
+    });
+    it('a string starting with numbers, as hex', () => {
+      assert.equal(parseInt('-F', 16), -15)
+    });
+    it('a word made of letters', () => {
+      assert(isNaN(parseInt('word')));
+    });
+    it('an empty object literal RETURNS `NaN`', () => {
+      assert(isNaN(parseInt({})));
+    });
+    it('an empty array', () => {
+      assert(isNaN(parseInt([])));
+    });
+    it('a array [123]', () => {
+      assert.equal(parseInt([123]), 123);
+    });
   });
 });
