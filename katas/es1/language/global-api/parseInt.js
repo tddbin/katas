1061eaa5d5a127a6b1c2ae28fab5c23e72ab882c
wolframkriesing <wolfram+github@kriesing.de>
2019-10-06 18:57:13 +0200

    Just reading the first paragraph of the spec text produces quite a number of tests already, make those the default katas for this function, there will be an in-depth one coming up, I can already see it :).

--- katas/es1/language/global-api/parseInt.js
+++ katas/es1/language/global-api/parseInt.js
@@ -8,4 +8,23 @@ describe('`parseInt()` parses a string and returns an integer.', () => {
     const whatType = 'function';
     assert.equal(typeof parseInt, whatType);
   });
+  it('WHEN given a numeric string THEN converts it to an integer', () => {
+    assert.equal(parseInt('1'), 1);
+  });
+  it('WHEN given a numeric string AND a radix THEN converts to an integer applying the radix', () => {
+    assert.equal(parseInt('10', 16), 16);
+  });
+  it('WHEN given a numeric string AND a radix=0 THEN converts to an integer assuming radix=10', () => {
+    assert.equal(parseInt('10', 0), 10);
+  });
+  it('WHEN given a numeric string starting with `0x` THEN converts to an integer assuming radix=16', () => {
+    assert.equal(parseInt('0x10'), 16);
+  });
+  it('WHEN given a numeric string starting with `0X` THEN converts to an integer assuming radix=16', () => {
+    assert.equal(parseInt('0X10'), 16);
+  });
+
+  it('ignores any surrounding white spaces for a given string', () => {
+    assert.equal(parseInt('     123   '), 123);
+  });
 });
