26bec20014ca6752e9e7bf993c4ebfe214f6fa7b
wolframkriesing <wolfram+github@kriesing.de>
2019-10-06 18:38:39 +0200

    Describe and start with parseInt kata.

--- katas/es1/language/__raw-metadata__.js
+++ katas/es1/language/__raw-metadata__.js
@@ -67,6 +67,40 @@ export const all = {
           ]
         }
       }
-    }
+    },
+    'Global Object API': {
+      items: {
+        [es1.GLOBAL_PARSEINT.id]: {
+          name: '`parseInt()`',
+          description: '',
+          path: 'global-api/parseInt',
+          level: SKILL_LEVEL.BEGINNER,
+          requiresKnowledgeFrom: [],
+          // publishDateUTC: new Date(Date.UTC(2015, date.OCTOBER, 22, 9, 11)),
+          links: [
+            {
+              url: 'https://www.ecma-international.org/publications/files/ECMA-ST-ARCH/ECMA-262,%201st%20edition,%20June%201997.pdf',
+              comment: 'The very first version of the spec defining `parseInt`, the ES1 spec.',
+              tags: [tag.SPECIFICATION, tag.DOCS]
+            },
+            {
+              url: 'https://www.ecma-international.org/ecma-262/10.0/index.html#sec-parseint-string-radix',
+              comment: 'A later, newer version of the spec text for `parseInt`, from ES10.',
+              tags: [tag.SPECIFICATION, tag.DOCS]
+            },
+            {
+              url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt',
+              comment: 'The description of `parseInt()` on MDN, probably best to read.',
+              tags: [tag.MDN, tag.DOCS]
+            },
+            // {
+            //   url: 'https://twitter.com/wolframkriesing/status/657161540525826048',
+            //   comment: 'Announcement of this kata on twitter.',
+            //   tags: [tag.ANNOUNCEMENT]
+            // },
+          ],
+        },
+      },
+    },
   }
 };
---
+++ katas/es1/language/global-api/parseInt.js
@@ -0,0 +1,11 @@
+// ES1 - 3: parseInt
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('`parseInt()` parses a string and returns an integer.', () => {
+  it('it is a global function', () => {
+    //// const whatType = 'global function';
+    const whatType = 'function';
+    assert.equal(typeof parseInt, whatType);
+  });
+});
