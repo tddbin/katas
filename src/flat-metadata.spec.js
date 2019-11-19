0d9408c4937519fa5323c534ad14705109d9a086
wolframkriesing <wolfram+github@kriesing.de>
2019-11-19 17:10:20 +0100

    Improve test description and use of deprecated assert function.

--- src/flat-metadata.spec.js
+++ src/flat-metadata.spec.js
@@ -3,7 +3,7 @@ import FlatMetaData from './flat-metadata';
 import {forGroupName as slugForGroupName} from './slug';
 import {all} from '../katas/es6/language/__raw-metadata__';
 
-describe('build the meta data from the all.js file structure', function() {
+describe('Build the "all meta data" (from the `__raw-metadata__.js` file)', function() {
   
   const groupName = 'Template strings';
   const firstGroup = all.groups[groupName];
@@ -16,9 +16,9 @@ describe('build the meta data from the all.js file structure', function() {
   it('items are sorted by original object`s key (which is the ID)', function() {
     const items = converted().items;
     // use `path` to compare, since not all data stay the same
-    assert.equal(items[0].path, firstGroupItems[1].path);
-    assert.equal(items[1].path, firstGroupItems[2].path);
-    assert.equal(items[2].path, firstGroupItems[3].path);
+    assert.strictEqual(items[0].path, firstGroupItems[1].path);
+    assert.strictEqual(items[1].path, firstGroupItems[2].path);
+    assert.strictEqual(items[2].path, firstGroupItems[3].path);
   });
   
   it('each item gets an `id` property, the original object`s key', function() {
@@ -37,10 +37,10 @@ describe('build the meta data from the all.js file structure', function() {
     });
     it('the `publishDate`, if set, is as UTC string, with GMT+0', function() {
       var expected = new Date(Date.UTC(2015, 2, 13, 7, 55));
-      assert.equal(converted().items[0].publishDateRfc822, expected.toUTCString());
+      assert.strictEqual(converted().items[0].publishDateRfc822, expected.toUTCString());
     });
     it('the `publishDateUTC` is not in the destination data', function() {
-      assert.equal(converted().items[0].publishDateUTC, void 0);
+      assert.strictEqual(converted().items[0].publishDateUTC, void 0);
     });
   });
   
@@ -50,10 +50,10 @@ describe('build the meta data from the all.js file structure', function() {
   });
   
   it('the group name is added in each item', function() {
-    assert.equal(converted().items[0].groupName, groupName);
+    assert.strictEqual(converted().items[0].groupName, groupName);
   });
   
   it('a group slug is provided (a string usable in a URL)', function() {
-    assert.equal(converted().items[0].groupNameSlug, slugForGroupName(groupName));
+    assert.strictEqual(converted().items[0].groupNameSlug, slugForGroupName(groupName));
   });
 });
