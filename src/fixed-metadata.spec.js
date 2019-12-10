dfcf22e4ff47a8d9142f5f009fbea46bc972023f
wolframkriesing <wolfram+github@kriesing.de>
2019-12-10 20:47:23 +0100

    Use hamjest, for better error messages.

--- src/fixed-metadata.spec.js
+++ src/fixed-metadata.spec.js
@@ -1,4 +1,4 @@
-import assert from 'assert';
+import {assertThat, equalTo} from 'hamjest';
 import FlatMetaData from './flat-metadata';
 import {all as es1Metadata} from '../katas/es1/language/__raw-metadata__';
 import {all as es1RewriteMetadata} from '../katas/es1/learn-by-rewriting/__raw-metadata__.js';
@@ -10,24 +10,24 @@ import {all as hamjestMetadata} from '../katas/libraries/hamjest/__raw-metadata_
 
 describe('Ensure metadata that must NEVER change (because they are used for builing URLs, etc.)', () => {
   it('ES1 katas slug must stay the same', () => {
-    assert.strictEqual(FlatMetaData.to(es1Metadata).nameSlug, 'es1-katas');
+    assertThat(FlatMetaData.to(es1Metadata).nameSlug, equalTo('es1-katas'));
   });
   it('ES1-rewrite katas slug must stay the same', () => {
-    assert.strictEqual(FlatMetaData.to(es1RewriteMetadata).nameSlug, 'es1-learn-by-rewriting');
+    assertThat(FlatMetaData.to(es1RewriteMetadata).nameSlug, equalTo('es1-learn-by-rewriting'));
   });
   it('ES6 katas slug must stay the same', () => {
-    assert.strictEqual(FlatMetaData.to(es6Metadata).nameSlug, 'es6-katas');
+    assertThat(FlatMetaData.to(es6Metadata).nameSlug, equalTo('es6-katas'));
   });
   it('ES7 katas slug must stay the same', () => {
-    assert.strictEqual(FlatMetaData.to(es7Metadata).nameSlug, 'es7-katas');
+    assertThat(FlatMetaData.to(es7Metadata).nameSlug, equalTo('es7-katas'));
   });
   it('ES8 katas slug must stay the same', () => {
-    assert.strictEqual(FlatMetaData.to(es8Metadata).nameSlug, 'es8-katas');
+    assertThat(FlatMetaData.to(es8Metadata).nameSlug, equalTo('es8-katas'));
   });
   it('ES10 katas slug must stay the same', () => {
-    assert.strictEqual(FlatMetaData.to(es10Metadata).nameSlug, 'es10-katas');
+    assertThat(FlatMetaData.to(es10Metadata).nameSlug, equalTo('es10-katas'));
   });
   it('Hamjest katas slug must stay the same', () => {
-    assert.strictEqual(FlatMetaData.to(hamjestMetadata).nameSlug, 'hamjest-assertion-library');
+    assertThat(FlatMetaData.to(hamjestMetadata).nameSlug, equalTo('hamjest-assertion-library'));
   });
 });
