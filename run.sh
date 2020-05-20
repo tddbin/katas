718238d1293d245c98caf4ca7407911c3a2b4413
wolframkriesing <wolfram+github@kriesing.de>
2020-05-20 22:21:40 +0200

    Improve the docker setup, allow multiple shells to run it and document it.

--- run.sh
+++ run.sh
@@ -1,20 +1,33 @@
 #!/usr/bin/env bash
 
-set -e
+set -o errexit  # fail on simple (non-piped) error
+set -o pipefail # also fail on piped commands (e.g. cat myfile.txt | grep timo)
+set -o nounset  # fail when accessing unset vars
 
 DOCKERFILE_HASH=$(md5 -q ./Dockerfile)
-CONTAINER_NAME=katas
+CONTAINER_NAME=the-katas
 IMAGE_NAME=${CONTAINER_NAME}:${DOCKERFILE_HASH}
+
+function is_container_running() {
+  if [[ -n $(docker ps --quiet --filter "name=$CONTAINER_NAME") ]]; then
+    return 0
+  fi;
+  return 1
+}
 
 if [[ $(docker inspect --format . ${IMAGE_NAME} 2>&1) != "." ]]; then
   echo "--- BUILDING image '${IMAGE_NAME}'---"
-  docker build -t ${IMAGE_NAME} -f Dockerfile .
+  docker build -t ${IMAGE_NAME} - < Dockerfile
 fi
 
 
-echo "--- RUNNING container '${CONTAINER_NAME}'---"
-docker run --rm \
-	--name ${CONTAINER_NAME} \
-	--volume $(pwd):/home/node/app \
-	${IMAGE_NAME} \
-	$1
+if is_container_running; then
+  echo "--- ENTER running container '${CONTAINER_NAME}'---"
+  docker exec -it ${CONTAINER_NAME} $@
+else
+  echo "--- RUNNING container '${CONTAINER_NAME}'---"
+  docker run --rm -it \
+    --name ${CONTAINER_NAME} \
+    --volume $(pwd):/app \
+    ${IMAGE_NAME} $@
+fi
