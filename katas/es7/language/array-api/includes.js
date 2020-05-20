06ab754db069541cdc6c0baf10645fee2d2a7a1d
wolframkriesing <wolfram+github@kriesing.de>
2020-05-21 00:09:34 +0200

    Made it a kata.

--- katas/es7/language/array-api/includes.js
+++ katas/es7/language/array-api/includes.js
@@ -4,6 +4,7 @@
 
 describe('`Array.prototype.includes()` determines whether an array includes a certain value', () => {
   it('`includes()` method is defined on the prototype', () => {
+    //// const fn = Array.includes;
     const fn = [].includes;
     assert.equal(typeof fn, 'function');
   });
@@ -17,15 +18,27 @@ describe('`Array.prototype.includes()` determines whether an array includes a ce
       assert.equal(emptyArray.includes(0), false);
     });
     it('WHEN searching for +0 or -0 THEN they are equal', () => {
-      assert.equal([-0].includes(+0), true);
+      //// const numbers = [-1];
+      const numbers = [-0];
+      assert.equal(numbers.includes(+0), true);
     });
     it('WHEN searching for a number THEN they are compared by value (not type)', () => {
-      assert.equal([1.0000].includes(1), true);
-      assert.equal([42e10].includes(420000000000.000), true);
+      //// const one = 1.00001;
+      const one = 1.0000;
+      assert.equal([one].includes(1), true);
+
+      //// const fourtyTwoALot = 42e9;
+      const fourtyTwoALot = 42e10;
+      assert.equal([fourtyTwoALot].includes(420000000000.000), true);
     });
     it('WHEN searching for a boolean THEN works as expected', () => {
-      assert.equal([1, 0, 2].includes(true), false);
-      assert.equal([1, 0, 2, true].includes(true), true);
+      //// const noBooleans = [1, 0, 2, true];
+      const noBooleans = [1, 0, 2];
+      assert.equal(noBooleans.includes(true), false);
+
+      //// const containsOneBoolean = [1, 0, 2];
+      const containsOneBoolean = [1, 0, 2, true];
+      assert.equal(containsOneBoolean.includes(true), true);
     });
   });
   describe('GIVEN we search (more) complex values', () => {
@@ -42,16 +55,21 @@ describe('`Array.prototype.includes()` determines whether an array includes a ce
     });
     describe('types must always match, for example', () => {
       it('WHEN searching a numeric string THEN types must match', () => {
-        assert.equal(['1'].includes('1'), true);
+        //// const searchFor = 1;
+        const searchFor = '1';
+        assert.equal(['1'].includes(searchFor), true);
       });
       it('WHEN searching a number THEN types must match', () => {
-        assert.equal([1].includes(1), true);
+        //// const searchFor = '1';
+        const searchFor = 1;
+        assert.equal([1].includes(searchFor), true);
       });
     });
     it('WHEN searching for an object THEN the same object (not just the same content) must be given', () => {
       const obj = {};
-      assert.equal([{}].includes(obj), false);
-      assert.equal([obj].includes(obj), true);
+      //// const searchFor = {};
+      const searchFor = obj;
+      assert.equal([obj].includes(searchFor), true);
     });
     it('WHEN searching for a Symbol THEN only the same symbol is found', () => {
       //// const symbol = Symbol('oha');
@@ -61,11 +79,14 @@ describe('`Array.prototype.includes()` determines whether an array includes a ce
   });
   describe('the `fromIndex` parameter, GIVEN we start the search at a certain position in the array', () => {
     it('WHEN searching from 3rd position THEN find only everything beyond', () => {
+      //// const fromIndex = 5;
       const fromIndex = 3;
       assert.equal(['zero', 'one', 'two', 'three'].includes('three', fromIndex), true);
     });
     it('WHEN searching at index beyond the array`s end THEN find nothing', () => {
-      assert.equal([42, 23].includes(23, 10), false);
+      //// const fromIndex = 0;
+      const fromIndex = 10;
+      assert.equal([42, 23].includes(23, fromIndex), false);
     });
     describe('WHEN searching at a negative index', () => {
       it('THEN use it as index from the end', () => {
@@ -75,7 +96,9 @@ describe('`Array.prototype.includes()` determines whether an array includes a ce
         assert.equal([1, 2, 42, 23].includes(42, fromIndex), true);
       });
       it('THEN use it as index from the end', () => {
-        assert.equal([1, 2, 42, 23].includes(42, -100), true);
+        //// const fromIndex = 100;
+        const fromIndex = -100;
+        assert.equal([1, 2, 42, 23].includes(42, fromIndex), true);
       });
     });
   });
