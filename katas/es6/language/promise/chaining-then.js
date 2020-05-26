2ea2f8c1c512f1d09a28588deaec790271eb94f3
Travis-CI <w+travis-for-tddbin@kriesing.de>
2020-05-26 20:27:20 +0000

    Travis deployed 'master' - Tue May 26 20:27:20 UTC 2020

--- katas/es6/language/promise/chaining-then.js
+++ katas/es6/language/promise/chaining-then.js
@@ -19,8 +19,7 @@ describe('chaining multiple promises can enhance readability', () => {
     it('`then()` receives the result of the promise it was called on', function() {
       const wordsPromise = Promise.resolve('one   space     between each     word');
       return wordsPromise
-        // .then(s => removeMultipleSpaces())
-        .then(s => removeMultipleSpaces(s))
+        .then(s => removeMultipleSpaces())
         .then(actual => {assert.equal(actual, 'one space between each word')})
       ;
     });
