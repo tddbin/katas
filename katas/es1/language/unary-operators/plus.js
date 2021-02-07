2d0e1e478d7ce1e13ed4bc2561335ab70c180253
Travis-CI <w+travis-for-tddbin@kriesing.de>
2021-02-07 16:38:35 +0000

    Travis deployed 'master' - Sun Feb  7 16:38:35 UTC 2021

--- katas/es1/language/unary-operators/plus.js
+++ katas/es1/language/unary-operators/plus.js
@@ -2,7 +2,11 @@
 // To do: make all tests pass, leave the assert lines unchanged!
 // Follow the hints of the failure messages!
 
-describe('Unary "+" operator', () => {
+describe('The unary "+" operator', () => {
+  it('is the "+" that preceeds an operand', () => {
+    const x = -'23';
+    assert.strictEqual(x, +'23');
+  });
   it('converts its operand to the Number type', () => {
     const converted = +'fourty two';
     assert.strictEqual(converted, Number(42));
