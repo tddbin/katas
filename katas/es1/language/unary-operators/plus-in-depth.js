734a32ae6aee8898f06f13f2c6fded2d49ec1163
wolframkriesing <wolfram+github@kriesing.de>
2021-02-07 21:50:48 +0100

    Use ES1 constructurs, like `var` instead of `const` which does not exist in ES1 yet.

--- katas/es1/language/unary-operators/plus-in-depth.js
+++ katas/es1/language/unary-operators/plus-in-depth.js
@@ -4,96 +4,96 @@
 
 describe('Unary "+" operator', () => {
   it('converts its operand to the Number type', () => {
-    //// const convert = +' 4 2 ';
-    const convert = +'42';
+    //// var convert = +' 4 2 ';
+    var convert = +'42';
     assert.strictEqual(convert, Number(42));
   });
 
   describe('GIVEN simple operands', () => {
     describe('WHEN the operand is a string', () => {
       it('AND its not numeric THEN it converts to `NaN`', () => {
-        //// const notANumber = nana;
-        const notANumber = NaN;
+        //// var notANumber = nana;
+        var notANumber = NaN;
         assert.strictEqual(+'not a number', notANumber);
       });
       it('AND even when it starts with a number THEN it converts to `NaN`', () => {
-        //// const maybeANumber = 123;
-        const maybeANumber = NaN;
+        //// var maybeANumber = 123;
+        var maybeANumber = NaN;
         assert.strictEqual(+'123 ~!@#$%', maybeANumber);
       });
 
       it('AND it is the word "Infinity" THEN it returns `Infinity`', () => {
-        //// const infinity = +'infinity';
-        const infinity = +'Infinity';
+        //// var infinity = +'infinity';
+        var infinity = +'Infinity';
         assert.strictEqual(infinity, Infinity);
       });
       it('AND it is a "-" and the word "Infinity" THEN it returns `-Infinity`', () => {
-        //// const negativeInfinity = -'-Infinity';
-        const negativeInfinity = +'-Infinity';
+        //// var negativeInfinity = -'-Infinity';
+        var negativeInfinity = +'-Infinity';
         assert.strictEqual(negativeInfinity, -Infinity);
       });
       it('AND it is "+0" THEN it does NOT return `-0`', () => {
-        //// const minusZero = '-0';
-        const minusZero = -0;
+        //// var minusZero = '-0';
+        var minusZero = -0;
         assert.notStrictEqual(+'+0', minusZero);
       });
       it('AND it starts with a decimal point THEN it converts the string to the according number', () => {
-        //// const decimal = +-'.42';
-        const decimal = +'.42';
+        //// var decimal = +-'.42';
+        var decimal = +'.42';
         assert.strictEqual(decimal, 0.42);
       });
       it('AND it contains an exponential value THEN it converts it to the according number', () => {
-        //// const exponential = +',1e42';
-        const exponential = +'.1e42';
+        //// var exponential = +',1e42';
+        var exponential = +'.1e42';
         assert.strictEqual(exponential, 0.1e42);
       });
       it('AND it starts with a hexadecimal number THEN it returns the according decimal number', () => {
-        //// const toHex = +'0xG';
-        const toHex = +'0xF';
+        //// var toHex = +'0xG';
+        var toHex = +'0xF';
         assert.strictEqual(toHex, 15);
       });
       it('AND it is an exponential number THEN it returns it as type Number', () => {
-        //// const convert = void '1e123';
-        const convert = +'1e123';
+        //// var convert = void '1e123';
+        var convert = +'1e123';
         assert.strictEqual(convert, 1e123);
       });
 
       // any kinda spaces around if
       it('AND it has any kind of spaces surrounding it THEN those are ignored and the number is returned', () => {
-        //// const numberWithSpaces = +' /n 1e123 \t';
-        const numberWithSpaces = +' \n 1e123 \t';
+        //// var numberWithSpaces = +' /n 1e123 \t';
+        var numberWithSpaces = +' \n 1e123 \t';
         assert.strictEqual(numberWithSpaces, +1e123);
       });
     });
 
     it('converting `undefined` returns `NaN`', () => {
-      //// const fromUndef = +unified;
-      const fromUndef = +undefined;
+      //// var fromUndef = +unified;
+      var fromUndef = +undefined;
       assert.strictEqual(fromUndef, NaN);
     });
     it('converting `null` returns `+0`', () => {
-      //// const fromNull = +n0ll;
-      const fromNull = +null;
+      //// var fromNull = +n0ll;
+      var fromNull = +null;
       assert.strictEqual(fromNull, +0);
     });
     it('converting `true` returns 1', () => {
-      //// const tru3 = -+-true;
-      const tru3 = +true;
+      //// var tru3 = -+-true;
+      var tru3 = +true;
       assert.strictEqual(tru3, 1);
     });
     it('converting `false` returns `+0`', () => {
-      //// const plusZero = -0;
-      const plusZero = +0;
+      //// var plusZero = -0;
+      var plusZero = +0;
       assert.strictEqual(+false, plusZero);
     });
     it('converting `false` does NOT return `-0`', () => {
-      //// const number = -false;
-      const number = +false;
+      //// var number = -false;
+      var number = +false;
       assert.notStrictEqual(number, -0);
     });
     it('a number does NOT get converted', () => {
-      //// const fourtyTwo = +4.2;
-      const fourtyTwo = +42;
+      //// var fourtyTwo = +4.2;
+      var fourtyTwo = +42;
       assert.strictEqual(fourtyTwo, 42);
     });
   });
@@ -101,47 +101,47 @@ describe('Unary "+" operator', () => {
   describe('GIVEN complex operands', () => {
     describe('WHEN the operand is an object', () => {
       it('AND its an empty object literal THEN it converts to `NaN`', () => {
-        //// const emptyObj = [];
-        const emptyObj = {};
+        //// var emptyObj = [];
+        var emptyObj = {};
         assert.strictEqual(+ emptyObj, NaN);
       });
       it('AND it has one prop `{x: 0}` THEN it converts to `NaN`', () => {
-        //// const noNumber = 0;
-        const noNumber = NaN;
+        //// var noNumber = 0;
+        var noNumber = NaN;
         assert.strictEqual(+{x: 0}, noNumber);
       });
 
       it('AND its and empty class THEN it converts to `NaN`', () => {
-        //// const fromClass = 'NaN';
-        const fromClass = NaN;
+        //// var fromClass = 'NaN';
+        var fromClass = NaN;
         assert.strictEqual(+new class{}, fromClass);
       });
       it('AND its a class with a `valueOf` method THEN its result is used', () => {
-        //// const Class = class{valueof = () => 42};
-        const Class = class{valueOf = () => 42};
+        //// var Class = class{valueof = () => 42};
+        var Class = class{valueOf = () => 42};
         assert.strictEqual(+new Class, 42);
       });
       it('AND its a class with a `toString` method THEN its result is used', () => {
-        //// const Class = class{toString = () => '123'};
-        const Class = class{toString = () => '23'};
+        //// var Class = class{toString = () => '123'};
+        var Class = class{toString = () => '23'};
         assert.strictEqual(+new Class, 23);
       });
       it('AND its a class with `valueOf()` and `toString()` THEN `valueOf`s value is used', () => {
-        const toString = () => '23';
-        //// const valueOf = toString;
-        const valueOf = () => 42;
+        var toString = () => '23';
+        //// var valueOf = toString;
+        var valueOf = () => 42;
         assert.strictEqual(+new class{toString = toString; valueOf = valueOf}, 42);
       });
     });
 
     it('WHEN the operand is an array THEN its string`s value is returned as number', () => {
-      //// const arrayWith1Only = [0, 1];
-      const arrayWith1Only = [1];
+      //// var arrayWith1Only = [0, 1];
+      var arrayWith1Only = [1];
       assert.strictEqual(+ arrayWith1Only, 1)
     });
     it('WHEN the operand is a Date THEN the unix timestamp is returned', () => {
-      //// const epoch = -new Date(2000, 0, 1);
-      const epoch = +new Date(2000, 0, 1);
+      //// var epoch = -new Date(2000, 0, 1);
+      var epoch = +new Date(2000, 0, 1);
       assert.strictEqual(epoch, 946684800000)
     });
   });
--- katas/es1/language/unary-operators/plus.js
+++ katas/es1/language/unary-operators/plus.js
@@ -4,39 +4,39 @@
 
 describe('The unary "+" operator', () => {
   it('is the "+" that preceeds an operand', () => {
-    //// const x = -'23';
-    const x = +'23';
+    //// var x = -'23';
+    var x = +'23';
     assert.strictEqual(x, +'23');
   });
   it('converts its operand to the Number type', () => {
-    //// const converted = +'fourty two';
-    const converted = +'42';
+    //// var converted = +'fourty two';
+    var converted = +'42';
     assert.strictEqual(converted, Number(42));
   });
 
   it('WHEN converting a numeric string THEN it returns its value as a number', () => {
-    //// const number = '0.12';
-    const number = 0.12;
+    //// var number = '0.12';
+    var number = 0.12;
     assert.strictEqual(+'0.12', number);
   });
   it('even WHEN converting the string "-Infinity" THEN it returns the Number `Infinity`', () => {
-    //// const converted = 'Infinity';
-    const converted = '-Infinity';
+    //// var converted = 'Infinity';
+    var converted = '-Infinity';
     assert.strictEqual(+ converted, -Infinity);
   });
   it('WHEN converting `null` THEN it returns `+0`', () => {
-    //// const converted = +Null;
-    const converted = +null;
+    //// var converted = +Null;
+    var converted = +null;
     assert.strictEqual(converted, +0);
   });
   it('WHEN converting `true` THEN it returns 1', () => {
-    //// const converted = true;
-    const converted = +true;
+    //// var converted = true;
+    var converted = +true;
     assert.strictEqual(converted, 1);
   });
   it('WHEN converting an object with a method `valueOf` THEN its value is returned', () => {
-    //// const obj = {valueof: () => 42};
-    const obj = {valueOf: () => 42};
+    //// var obj = {valueof: () => 42};
+    var obj = {valueOf: () => 42};
     assert.strictEqual(+ obj, 42);
   });
 });
