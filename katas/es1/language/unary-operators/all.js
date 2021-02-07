231460d75e75398d1803c554a0fd2f576b7dd418
wolframkriesing <wolfram+github@kriesing.de>
2021-02-07 22:46:52 +0100

    Now the "all" is a kata too. A simple but nice one, I think.

--- katas/es1/language/unary-operators/all.js
+++ katas/es1/language/unary-operators/all.js
@@ -3,42 +3,58 @@
 // Follow the hints of the failure messages!
 
 describe('All unary operators', () => {
-  it('a unary operator is preceeding or following its operand', () => {
+  it('a unary operator is preceeding (or following) its operand', () => {
+    //// var convertWithPlusBefore = -'1';
     var convertWithPlusBefore = +'1';
     assert.strictEqual(convertWithPlusBefore, +'1');
   });
 
   describe('WHEN the operator is before the operand', () => {
     it('AND it is `+` THEN a number-like string converts it to a number', () => {
-      assert.strictEqual(+'23', 23);
+      //// var toNumber = +' 2 3 ';
+      var toNumber = +'23';
+      assert.strictEqual(toNumber, 23);
     });
     it('AND it is `-` THEN a number-like string converts it to the negated value of this number', () => {
-      assert.strictEqual(-'-23', 23);
+      //// var toNumber = '+23';
+      var toNumber = '-23';
+      assert.strictEqual(-toNumber, 23);
     });
     it('AND it is `~` THEN a number-like string converts it to a number and return the bitwise complement', () => {
-      assert.strictEqual(~'0', -1);
+      //// var bitwise = ~~'0';
+      var bitwise = ~'0';
+      assert.strictEqual(bitwise, -1);
     });
     it('AND it is `!` THEN a boolean-like string returns the negated boolean', () => {
-      assert.strictEqual(!'true', false);
+      //// var trueString = !'true';
+      var trueString = 'true';
+      assert.strictEqual(!trueString, false);
     });
     it('AND it is `delete` before an object`s property THEN the property is removed', () => {
       var obj = {x: 1};
+      //// delete obj.axe;
       delete obj.x;
       assert.strictEqual(obj.x, undefined);
     });
     it('AND it is `void` THEN it returns `undefined`', () => {
-      assert.strictEqual(void {}, undefined);
+      //// var toUndefined = delete {any: 'thing'};
+      var toUndefined = void {any: 'thing'};
+      assert.strictEqual(toUndefined, undefined);
     });
     it('AND it is `typeof` THEN it returns the operand`s type', () => {
-      assert.strictEqual(typeof 42, 'number');
+      //// var whatsTheType = typeof '42';
+      var whatsTheType = typeof 42;
+      assert.strictEqual(whatsTheType, 'number');
     });
     it('AND it is `++` THEN it increments the operand', () => {
       var number = 1;
+      //// +(+number);
       ++number;
       assert.strictEqual(number, 2);
     });
     it('AND it is a `--` THEN it decrements the operand', () => {
       var number = 1;
+      //// -_-number;
       --number;
       assert.strictEqual(number, 0);
     });
@@ -46,15 +62,16 @@ describe('All unary operators', () => {
 
   describe('WHEN the operator is after the operand', () => {
     it('AND it is `++` THEN the operand is incremented', () => {
+      //// var number = 11;
       var number = 1;
       assert.strictEqual(number++, 1);
       assert.strictEqual(number, 2);
     });
     it('AND it is `--` THEN the operand is decremented', () => {
+      //// var number = '2';
       var number = 1;
       assert.strictEqual(number--, 1);
       assert.strictEqual(number, 0);
     });
   });
 });
-
