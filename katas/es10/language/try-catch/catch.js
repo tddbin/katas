1f17bdcbd2de00116a0095d223aa5ac79ce1dc96
Wolfram Kriesing <wolfram+github@kriesing.de>
2021-07-19 21:02:03 +0200

    Built

---
+++ katas/es10/language/try-catch/catch.js
@@ -0,0 +1,7 @@
+// ES10 - 12: unbinded `catch`
+// To do: make all tests pass, leave the assert lines unchanged!
+// Follow the hints of the failure messages!
+
+describe('`catch` can now be used without a binding', () => {
+  // to do
+});
