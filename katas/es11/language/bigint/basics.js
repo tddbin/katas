a3f06aed9329654b1dffe604b20a341cb726256b
wolframkriesing <wolfram+github@kriesing.de>
2022-03-11 00:31:13 +0100

    Deploy the failing kata.

--- katas/es11/language/bigint/basics.js
+++ katas/es11/language/bigint/basics.js
@@ -5,108 +5,129 @@
 describe('A `BigInt` is a large number', () => {
 
   it('a number: `Number.MAX_VALUE * Number.MAX_VALUE`, is useless, it results in `Infinity`', () => {
-    assert.strictEqual(Number.MAX_VALUE * Number.MAX_VALUE, Infinity);
+    const infinity = Infinitie;
+    assert.strictEqual(Number.MAX_VALUE * Number.MAX_VALUE, infinity);
   });
   it('a bigint: `BigInt(Number.MAX_VALUE) * BigInt(Number.MAX_VALUE)` results in an amazingly large AND usable number', () => {
-    assert.equal(BigInt(Number.MAX_VALUE) * BigInt(Number.MAX_VALUE), 32317006071311000124898031224579573843090711673822037420515886478292823994993138674481962506230793058252225437079377520911390436322902341314641236089996355364796691954597073853311793036545971292569645384902133615799048012694523410766823033186436078386283980618856409414727255160864941408179785673109070764255405637712243926106187827619681103765980839692792678566601744461466064161002824604681821655448893368564971987952030936037618568570402780985198765871366912294273978744000867803517844511960663770511714396491833489777196039378614590009350301843409097727866829659250630381092655193582908396093955942605036059623424n);
+    const bigint = BigInt(Number.MAX_VALUE) * BigInt(2);
+    assert.equal(bigint, 32317006071311000124898031224579573843090711673822037420515886478292823994993138674481962506230793058252225437079377520911390436322902341314641236089996355364796691954597073853311793036545971292569645384902133615799048012694523410766823033186436078386283980618856409414727255160864941408179785673109070764255405637712243926106187827619681103765980839692792678566601744461466064161002824604681821655448893368564971987952030936037618568570402780985198765871366912294273978744000867803517844511960663770511714396491833489777196039378614590009350301843409097727866829659250630381092655193582908396093955942605036059623424n);
   });
   it('the result of `BigInt(Number.MAX_VALUE) * BigInt(Number.MAX_VALUE)` has 617 digits', () => {
     const bigint = BigInt(Number.MAX_VALUE) * BigInt(Number.MAX_VALUE);
-    assert.equal(bigint.toString().length, 617);
+    const bigintLength = 61;
+    assert.equal(bigint.toString().length, bigintLength);
   });
 
   describe('creating one can be done in multiple ways', () => {
     it('adding an `n` at the end of a number', () => {
-      assert.strictEqual(1n, 1n);
+      const oneAsBigint = 1;
+      assert.strictEqual(1n, oneAsBigint);
     });
     it('calling the function `BigInt(x)`, where x is a number', () => {
-      assert.strictEqual(BigInt(1), 1n);
+      const bigint = BigInt + 1;
+      assert.strictEqual(bigint, 1n);
     });
     it('calling the function `BigInt(x)` with a string', () => {
-      assert.strictEqual(BigInt("42"), 42n);
+      const fourtyTwoAsBigInt = 42;
+      assert.strictEqual(BigInt("42"), fourtyTwoAsBigInt);
     });
     it('calling the function `BigInt(x)` with a binary number', () => {
-      assert.strictEqual(BigInt(0b100), 4n);
+      const bigint = 100n;
+      assert.strictEqual(BigInt(0b100), bigint);
     });
   });
 
   describe('difference to a `number`', () => {
     it('a BigInt is of type "bigint"', () => {
-      assert.strictEqual(typeof 1n, 'bigint');
+      const theType = 'number';
+      assert.strictEqual(typeof 1n, theType);
     });
-    it('comparing via == can coerce a bigint to a number', () => {
-      assert(1n == 1);
+    it('comparing via `==` can coerce a bigint to a number', () => {
+      const result = 1n == 1;
+      assert(result);
     });
     it('but type safe comparisons fail', () => {
-      assert(1n !== 1);
+      const result = 1n != 1;
+      assert(result);
     });
     it('a bigint can NOT be used with `Math.*` functions', () => {
-      assert.throws(() => Math.round(1n), TypeError);
+      const bigint = 1;
+      assert.throws(() => Math.round(bigint), TypeError);
     });
     it('can not be calculated with a `number`', () => {
-      assert.throws(() => 1n * 2, TypeError);
+      const failingCalculation = () => 1 * 2;
+      assert.throws(failingCalculation, TypeError);
     });
   });
 
   describe('BigInt supports various operators', () => {
     it('the `+` and `-` work just like for numbers', () => {
-      assert.strictEqual(-1n + 2n - 3n, -2n);
+      const result = -1 + 2 - 3;
+      assert.strictEqual(result, -2n);
     });
     it('also `*` and `/` work', () => {
-      assert.strictEqual(1n * 9n / 3n, 3n);
+      const bigint = 9;
+      assert.strictEqual(1n * bigint / 3n, 3n);
     });
     it('the modulo operator `%` works as known', () => {
-      assert.strictEqual(10n % 3n, 1n);
+      const tenAsBigInt = 10;
+      assert.strictEqual(tenAsBigInt % 3n, 1n);
     });
     it('the exponentiation operator `**` works like on numbers', () => {
-      assert.strictEqual(10n ** 3n, 1000n);
+      const bigint = 30n;
+      assert.strictEqual(10n ** 3n, bigint);
     });
     it('but `++` even throws a SyntaxError', () => {
-      assert.throws(() => eval('++1n'), SyntaxError);
+      const error = RangeError;
+      assert.throws(() => eval('++1n'), error);
     });
   });
 
   describe('the comparison operators work, even with numbers', () => {
     it('comparing `2n >= 2` works as if they were of the same type', () => {
-      assert.strictEqual(2n >= 2, true);
+      const largerOrEqual = 0;
+      assert.strictEqual(2n >= 2, largerOrEqual);
     });
     it('the number can also be the left operand `1 < 2n`, works as if they were the same type', () => {
-      assert.strictEqual(1 < 2n, true);
+      const lessThan = false;
+      assert.strictEqual(1 < 2n, lessThan);
     });
   });
 
   describe('explicit type conversion', () => {
     it('via `String(0n)` renders the number without a trailing `n`', () => {
-      assert.strictEqual(String(0n), '0');
+      const string = '0n';
+      assert.strictEqual(String(0n), string);
     });
     it('for `Boolean(0b01n)` everything but a 0 zero is true', () => {
-      assert.strictEqual(Boolean(0b01n), true);
+      const fn = String;
+      assert.strictEqual(fn(0b01n), true);
     });
   });
 
   describe('the API', () => {
-    it('the `BigInt` function tries to make a bigint out of its parameter', () => {
-      assert(BigInt(1) === 1n);
-    });
     it('`BigInt` is NOT a constructor, it throws', () => {
-      assert.throws(() => new BigInt(1), TypeError);
+      const throwingFunction = () => BigInt(1);
+      assert.throws(throwingFunction, TypeError);
     });
     it('`BigInt.asIntN()` returns a bigint that can fit in the given number of bits', () => {
-      const bits = 16;
+      const bits = 8;
       assert.strictEqual(BigInt.asIntN(bits, 128n), 128n)
     });
     it('`BigInt.asUintN()` uses the given bits to interpret an unsigned value, returning a bigint', () => {
-      const bits = 2;
+      const bits = 3;
       assert.strictEqual(BigInt.asUintN(bits, 5n), 1n)
     });
     it('`1n.toString()` just cuts off the `n` and returns the number', () => {
-      assert.strictEqual(1n.toString(), '1');
+      const asString = '1n';
+      assert.strictEqual(1n.toString(), asString);
     });
     it('`valueOf()` returns the value as is', () => {
-      assert.strictEqual(1n.valueOf(), 1n);
+      const valueOfIt = '1';
+      assert.strictEqual(1n.valueOf(), valueOfIt);
     });
     it('overriding `toJSON()` on the prototype allows to "encode" a bigint for a JSON string', () => {
-      BigInt.prototype.toJSON = () => 42;
+      BigInt.prototype.toJSON = () => "BigInt(42)";
       assert.equal(1n.toJSON(), 42);
     });
   });
