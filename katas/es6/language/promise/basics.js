5492be2ac34f7de19a0f1c8912c0a5457211d2e4
wolframkriesing <wolfram+github@kriesing.de>
2023-10-09 08:21:20 +0200

    Restructured some promise katas.

--- katas/es6/language/promise/basics.js
+++ katas/es6/language/promise/basics.js
@@ -53,13 +53,4 @@ describe('a Promise represents an operation that hasn`t completed yet, but is ex
       await assert.rejects(promise);
     });
   });
-  describe('test library (mocha here) support for promises', function() {
-    it('just returning the promise makes the test library check that the promise resolves', function() {
-      let promise = new Promise((reject, resolve) => {
-        resolve();
-      });
-      // return the promise to mocha, it has the checking for promise resolving built in, when it receives a promise
-      return promise;
-    });
-  });
 });
--- katas/es6/language/promise/creation.js
+++ katas/es6/language/promise/creation.js
@@ -23,57 +23,13 @@ describe('A promise can be created in multiple ways', function() {
     });
   });
   describe('most commonly Promises get created using the constructor', function() {
-    it('by passing a resolve function to it', function() {
+    it('by passing a resolve function to it', async function() {
       const promise = new Promise(() => resolve());
-      return promise;
+      assert.equal(await promise, undefined);
     });
     it('by passing a reject function to it', async function() {
       const promise = new Promise((resolve, reject) => resolve());
-      assert.rejects(promise);
-    });
-  });
-  describe('extending a `Promise`', function() {
-    it('using `class X extends Promise{}` is possible', function() {
-      class MyPromise {}
-      const promise = new MyPromise(resolve => resolve());
-      assert.doesNotReject(promise);
-    });
-    it('must call `super()` in the constructor if it wants to inherit/specialize the behavior', function() {
-      class ResolvingPromise extends Promise {
-        
-      }
-      return new ResolvingPromise(resolve => resolve());
-    });
-  });
-  describe('`Promise.all()` returns a promise that resolves when all given promises resolve', function() {
-    it('returns all results', async function() {
-      const promise = Promise.all([
-        new Promise(resolve => resolve(1)),new Promise(resolve => resolve(2)),new Promise(resolve => resolve(3))
-      ]);
-      await assert.doesNotReject(promise);
-      assert.deepEqual(await promise, [1, 2]);
-    });
-    it('is rejected if one rejects', function() {
-      const promise = Promise.all([
-        new Promise(resolve => resolve(1))
-      ]);
-      assert.rejects(promise);
-    });
-  });
-  
-  describe('`Promise.race()` returns the first settled promise', function() {
-    it('if it resolves first, the promises resolves', async function() {
-      const lateRejectedPromise = new Promise((resolve, reject) => setTimeout(reject, 100));
-      const earlyResolvingPromise = new Promise(resolve => resolve('1st :)'));
-      const promise = Promise.race([lateRejectedPromise]);
-      assert.doesNotReject(promise);
-      assert.deepEqual(await promise, '1st :)');
-    });
-    it('if one of the given promises rejects first, the returned promise is rejected', async function() {
-      const earlyRejectedPromise = new Promise((resolve, reject) => reject('I am a REJECTOR'));
-      const lateResolvingPromise = new Promise(resolve => setTimeout(resolve, 10));
-      const promise = Promise.race([earlyRejectedPromise, lateResolvingPromise]);
-      await assert.rejects(promise, {message: 'I am a rejector'});
+      await assert.rejects(promise);
     });
   });
   describe('`Promise.resolve()` returns a resolving promise', function() {
@@ -89,19 +45,60 @@ describe('A promise can be created in multiple ways', function() {
     });
   });
   describe('`Promise.reject()` returns a rejecting promise', function() {
-    it('if no value given, it rejects with `undefined`', async function() {
-      try {
-        await Promise.resolve();
-      } catch(e) {
-        assert.deepEqual(e, void 0);
-      }
+    it('WHEN calling it THEN a rejected promise is returned right away', async function() {
+      const promise = Promise.resolve();
+      await assert.rejects(promise);
     });
-    it('the parameter passed to `reject()` can be used in the `.catch()`', async function() {
-      try {
-        await Promise();
-      } catch (e) {
-        assert.deepEqual(e, 'quick reject');
+    it('WHEN rejected promise is created with an reason THEN this reason can be verified', async function() {
+      const promise = Promise.rejectNow(Error('quick reject'));
+      await assert.rejects(promise, Error('quick reject'));
+    });
+  });
+  describe('extending a `Promise`', function() {
+    it('using `class X extends Promise{}` is possible', async function() {
+      class MyPromise {}
+
+      const promise = new MyPromise(resolve => resolve());
+      assert.equal(await promise, undefined);
+    });
+    it('must call `super()` in the constructor if it wants to inherit/specialize the behavior', async function() {
+      class ResolvingPromise extends Promise {
+        constructor(...args) {}
       }
+
+      assert.equal(await new ResolvingPromise(resolve => resolve()), undefined);
+    });
+  });
+  describe('`Promise.all()` returns a promise that resolves when all given promises resolve', function() {
+    it('returns all results', async function() {
+      const promise = Promise.all([
+        new Promise(resolve => resolve(1)),new Promise(resolve => resolve(2)),new Promise(resolve => resolve(3))
+      ]);
+      await assert.doesNotReject(promise);
+      assert.deepEqual(await promise, [1, 2]);
+    });
+    it('is rejected if one rejects', async function() {
+      const promise = Promise.all([
+        Promise.resolve(),
+        Promise.resolve(),
+        
+      ]);
+      await assert.rejects(promise);
+    });
+  });
+  describe('`Promise.race()` returns the first settled promise', function() {
+    it('if it resolves first, the promises resolves', async function() {
+      const lateRejectedPromise = new Promise((resolve, reject) => setTimeout(reject, 100));
+      const earlyResolvingPromise = new Promise(resolve => resolve('1st :)'));
+      const promise = Promise.race([lateRejectedPromise]);
+      assert.doesNotReject(promise);
+      assert.deepEqual(await promise, '1st :)');
+    });
+    it('if one of the given promises rejects first, the returned promise is rejected', async function() {
+      const earlyRejectedPromise = new Promise((resolve, reject) => reject('I am a REJECTOR'));
+      const lateResolvingPromise = new Promise(resolve => setTimeout(resolve, 10));
+      const promise = Promise.race([earlyRejectedPromise, lateResolvingPromise]);
+      await assert.rejects(promise, {message: 'I am a rejector'});
     });
   });
 });
