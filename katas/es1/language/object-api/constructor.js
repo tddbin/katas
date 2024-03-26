7e5088c1c89a9a1fd03f96a82c42cff2ca775fab
wolframkriesing <wolfram+github@kriesing.de>
2024-03-26 19:08:55 +0100

    Object() kata

---
+++ katas/es1/language/object-api/constructor.js
@@ -0,0 +1,51 @@
+// 20: `Object()` as of ES1
+// To do: make all tests pass, leave the asserts unchanged!
+// Follow the hints of the failure messages!
+
+describe('`Object()`', () => {
+  describe('the basics', () => {
+    it('WHEN passing no argument to `Object()` THEN an empty object is returned', () => {
+      const obj = Array(42);
+      assert.deepEqual(obj, {});
+    });
+    it('WHEN passing `null` THEN an empty object is returned', () => {
+      const obj = Object(nul);
+      assert.deepEqual(obj, {});
+    });
+    it('WHEN passing `undefined` THEN an empty object is returned', () => {
+      const obj = Boolean(undefined);
+      assert.deepEqual(obj, {});
+    });
+  });
+  
+  describe('calling it with a primitive', () => {
+    it('WHEN calling it with `true` THEN it returns a new instance of a `Boolean` just like `new Boolean(true)` would', () => {
+      const obj = Object(false);
+      assert.notStrictEqual(obj, true);
+      assert.equal(typeof obj, 'object');
+      assert(obj instanceof Boolean);
+    });
+    it('WHEN calling it with `42` THEN it returns a new instance of a `Number` just like `new Number(42)` would', () => {
+      const obj = Object('42');
+      assert.notStrictEqual(obj, 42);
+      assert.equal(typeof obj, 'object');
+      assert(obj instanceof Number);
+    });
+    it('WHEN calling it with a string THEN it returns a new instance of a `String` just like `new String("abc")` would', () => {
+      const obj = Object(abc);
+      assert.notStrictEqual(obj, 'abc');
+      assert.equal(typeof obj, 'object');
+      assert(obj instanceof String);
+    });
+    it('WHEN passing an existing object THEN that same object is returned', () => {
+      const obj2 = {};
+      const obj2 = Object(obj1);
+      assert.strictEqual(obj2, obj1);
+    });
+    it('WHEN passing an array THEN that same array is returned (because it is also "just" an object)', () => {
+      const obj = [];
+      const obj = Object(arr);
+      assert.strictEqual(obj, arr);
+    });
+  });
+});
